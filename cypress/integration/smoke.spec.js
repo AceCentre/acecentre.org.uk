e6d4cf2addbb28afb2d591d1268c28690f41352d
Gavin Henderson <github@gavinhenderson.co.uk>
2021-04-01 17:07:50 +0100

    Make cypress use the preview url

--- cypress/integration/smoke.spec.js
+++ cypress/integration/smoke.spec.js
@@ -1,6 +1,6 @@
 context("Smoke", () => {
   it("loads the webpage without any issues", () => {
-    cy.visit("https://deploy-preview-2--acecentreorguk.netlify.app/");
+    cy.visit("/");
     cy.get("#__next").should("exist");
   });
 });
