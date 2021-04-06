e91a97ca838470c8aa43336e50995e045ece9f1a
Gavin Henderson <github@gavinhenderson.co.uk>
2021-04-06 09:34:06 +0100

    Set lighthouse levels

--- lighthouserc.js
+++ lighthouserc.js
@@ -14,5 +14,13 @@ module.exports = {
     upload: {
       target: "temporary-public-storage",
     },
+    assert: {
+      assertions: {
+        "categories:performance": ["error", { minScore: 0.95 }],
+        "categories:accessibility": ["error", { minScore: 0.95 }],
+        "categories:best-practices": ["error", { minScore: 0.95 }],
+        "categories:seo": ["error", { minScore: 0.95 }],
+      },
+    },
   },
 };
