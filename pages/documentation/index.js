f9974955d351a8e59050977aea478e0c78b1d235
Gavin Henderson <github@gavinhenderson.co.uk>
2021-04-08 11:48:01 +0100

    Add some landing page text

--- pages/documentation/index.js
+++ pages/documentation/index.js
@@ -3,6 +3,10 @@ import { SOURCES } from "../../lib/auto-docs/config";
 export default function DocsHome({ sources }) {
   return (
     <>
+      <p>
+        Imagine this is a nice landing page and all these bullet points are
+        fancy cards. The bullet points are clickable
+      </p>
       <ul>
         {/* TODO: make this a next link  */}
         {sources.map((source) => (
