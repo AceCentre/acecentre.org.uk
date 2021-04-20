07471313f01185e8f757123e2e54343d4b99f495
Gavin Henderson <github@gavinhenderson.co.uk>
2021-04-20 14:57:15 +0100

    Add landmark components

--- pages/staff/index.js
+++ pages/staff/index.js
@@ -5,18 +5,22 @@ import styles from "../../styles/staff.module.css";
 export default function AllStaffPage({ allStaff }) {
   return (
     <>
-      <p>
-        Imagine this is a nice landing page and all these bullet points are
-        fancy cards. The bullet points are clickable
-      </p>
-      <ul>
-        {/* TODO: make this a next link  */}
-        {allStaff.map((employee) => (
-          <li key={employee.slug} className={styles.listItem}>
-            <Link href={`/staff/${employee.slug}`}>{employee.name}</Link>
-          </li>
-        ))}
-      </ul>
+      <header>
+        <p>
+          Imagine this is a nice landing page and all these bullet points are
+          fancy cards. The bullet points are clickable
+        </p>
+      </header>
+      <main>
+        <ul>
+          {/* TODO: make this a next link  */}
+          {allStaff.map((employee) => (
+            <li key={employee.slug} className={styles.listItem}>
+              <Link href={`/staff/${employee.slug}`}>{employee.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </main>
     </>
   );
 }
