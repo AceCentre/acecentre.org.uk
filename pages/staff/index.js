d4c24ce973f777c8e98d98e62771e57c3a45d285
Gavin Henderson <github@gavinhenderson.co.uk>
2021-04-20 14:45:55 +0100

    Increase touch target size

--- pages/staff/index.js
+++ pages/staff/index.js
@@ -1,5 +1,6 @@
 import { getAllStaff } from "../../lib/staff/get-staff";
 import Link from "next/link";
+import styles from "../../styles/staff.module.css";
 
 export default function AllStaffPage({ allStaff }) {
   return (
@@ -11,7 +12,7 @@ export default function AllStaffPage({ allStaff }) {
       <ul>
         {/* TODO: make this a next link  */}
         {allStaff.map((employee) => (
-          <li key={employee.slug}>
+          <li key={employee.slug} className={styles.listItem}>
             <Link href={`/staff/${employee.slug}`}>{employee.name}</Link>
           </li>
         ))}
