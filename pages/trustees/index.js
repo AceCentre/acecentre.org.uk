898701531b5b328869621dcc9117052ed80ed8ef
Gavin Henderson <github@gavinhenderson.co.uk>
2021-04-21 09:58:55 +0100

    Add trustee page

---
+++ pages/trustees/index.js
@@ -0,0 +1,30 @@
+import styles from "../../styles/staff.module.css";
+import { getAllTrustees } from "../../lib/trustees/get-trustees";
+
+export default function AllTrusteesPage({ allTrustees }) {
+  return (
+    <>
+      <header>
+        <p>
+          Imagine this is a nice landing page and all these bullet points are
+          fancy cards. The bullet points are clickable
+        </p>
+      </header>
+      <main>
+        <ul>
+          {allTrustees.map((trustee) => (
+            <li key={trustee.slug} className={styles.listItem}>
+              <pre>{JSON.stringify(trustee, null, 2)}</pre>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const allTrustees = await getAllTrustees();
+
+  return { props: { allTrustees } };
+}
