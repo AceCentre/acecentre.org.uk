60cb932161a0de20b24fd2dee3d931521f366ed7
Gavin Henderson <github@gavinhenderson.co.uk>
2021-04-23 11:14:42 +0100

    Show trustee urls

--- pages/trustees/index.js
+++ pages/trustees/index.js
@@ -1,5 +1,6 @@
 import styles from "../../styles/staff.module.css";
 import { getAllTrustees } from "../../lib/trustees/get-trustees";
+import Image from "next/image";
 
 export default function AllTrusteesPage({ allTrustees }) {
   return (
@@ -14,6 +15,9 @@ export default function AllTrusteesPage({ allTrustees }) {
         <ul>
           {allTrustees.map((trustee) => (
             <li key={trustee.slug} className={styles.listItem}>
+              {trustee.imageUrl && (
+                <Image src={trustee.imageUrl} width={40} height={40} />
+              )}
               <pre>{JSON.stringify(trustee, null, 2)}</pre>
             </li>
           ))}
