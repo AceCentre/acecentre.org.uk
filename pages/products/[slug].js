04b85de4745ca45f271c54cb1f81adc99ea70a8f
Gavin Henderson <github@gavinhenderson.co.uk>
2021-04-26 14:59:02 +0100

    Product page now outputs product json

---
+++ pages/products/[slug].js
@@ -0,0 +1,40 @@
+import { getAllProducts } from "../../lib/products/get-products";
+import { readFromStaticCache } from "../../lib/static-caching/read";
+import { writeToStaticCache } from "../../lib/static-caching/write";
+
+const PRODUCT_CACHE_KEY = "products";
+
+export default function ProductPage({ product }) {
+  return (
+    <>
+      <h1>A product page</h1>
+      <pre>{JSON.stringify(product, null, 2)}</pre>
+    </>
+  );
+}
+
+export async function getStaticPaths() {
+  const allProducts = await getAllProducts();
+
+  writeToStaticCache(PRODUCT_CACHE_KEY, allProducts);
+
+  const paths = allProducts.map((product) => ({
+    params: { slug: product.slug },
+  }));
+
+  return {
+    paths,
+    fallback: false, // See the "fallback" section below
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const allProducts = readFromStaticCache(PRODUCT_CACHE_KEY);
+  const product = allProducts.find(({ slug }) => slug === params.slug);
+
+  if (!product) {
+    throw new Error("Could not find product with slug: ", params.slug);
+  }
+
+  return { props: { product } };
+}
