d613f73ea0c030da41a9262afa01161ebc351ef7
Gavin Henderson <github@gavinhenderson.co.uk>
2021-04-27 15:20:28 +0100

    Fix import path

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import config from "../config";
+import config from "../../lib/config";
 
 const URL = `${config.baseUrl}/graphql`;
 
