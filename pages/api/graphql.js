7b273090720643dc3a28c8c0b05906287fb14a1a
Gavin Henderson <github@gavinhenderson.co.uk>
2021-04-27 15:18:26 +0100

    Put base URL into config

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,10 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import config from "../config";
+
+const URL = `${config.baseUrl}/graphql`;
+
 const expressPlayground = require("graphql-playground-middleware-express")
   .default;
 
 const playgroundFunc = expressPlayground({
-  endpoint: "https://acecentre.org.uk/graphql",
+  endpoint: URL,
 });
 
 export default playgroundFunc;
