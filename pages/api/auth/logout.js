afb378bfd5fe6e9de30dd639938cb808ae0b0e7b
Gavin Henderson <github@gavinhenderson.co.uk>
2021-04-29 10:14:04 +0100

    Consolodate session management

--- pages/api/auth/logout.js
+++ pages/api/auth/logout.js
@@ -1,15 +1,8 @@
-import { withIronSession } from "next-iron-session";
+import withSession from "../../../lib/auth/with-session";
 
 function handler(req, res) {
   req.session.destroy();
   res.send("Logged out");
 }
 
-export default withIronSession(handler, {
-  cookieName: "next_auth",
-  password: process.env.SESSION_SECRET,
-  // if your localhost is served on http:// then disable the secure flag
-  cookieOptions: {
-    secure: process.env.NODE_ENV === "production",
-  },
-});
+export default withSession(handler);
--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,5 @@
 import { useLogin } from "../lib/auth/hooks";
+import withSession from "../lib/auth/with-session";
 
 export default function LoginPage() {
   const { loginFormSubmit, readyForLogin, loginError } = useLogin();
@@ -18,3 +19,19 @@ export default function LoginPage() {
     </>
   );
 }
+
+// Redirect if you are signed in
+export const getServerSideProps = withSession(async function ({ req, res }) {
+  const user = req.session.get("user");
+
+  if (user && user.authToken) {
+    res.setHeader("location", "/user");
+    res.statusCode = 302;
+    res.end();
+    return { props: {} };
+  }
+
+  return {
+    props: {},
+  };
+});
