a2ff1caa31a4917fe849dfd4e36b571e13620996
Gavin Henderson <github@gavinhenderson.co.uk>
2021-04-30 12:20:08 +0100

    Show the cart on the get cart page

--- pages/cart.js
+++ pages/cart.js
@@ -1,18 +1,19 @@
-import { refreshToken } from "../lib/auth/refresh-token";
 import withSession from "../lib/auth/with-session";
+import { getCart } from "../lib/cart/get";
 
-export default function CartPage() {
+export default function CartPage({ rawCart }) {
   return (
     <div>
       <h1>Cart</h1>
+      <pre>{JSON.stringify(rawCart, null, 2)}</pre>
     </div>
   );
 }
 
 export const getServerSideProps = withSession(async function ({ req }) {
-  await refreshToken(req);
+  const rawCart = await getCart(req);
 
   return {
-    props: {},
+    props: { rawCart },
   };
 });
