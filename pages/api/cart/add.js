f1d8cd3061e2f8ed7f4cb6874e8fbc488fdb9adb
Gavin Henderson <github@gavinhenderson.co.uk>
2021-04-30 14:00:49 +0100

    Create add to cart endpoint

---
+++ pages/api/cart/add.js
@@ -0,0 +1,18 @@
+import withSession from "../../../lib/auth/with-session";
+import { addToCart } from "../../../lib/cart/add";
+
+async function handler(req, res) {
+  const body = JSON.parse(req.body);
+  const productId = body.productId;
+  const variationId = body.variationId || null;
+
+  if (!productId) {
+    return res.send({ success: false, error: "No product ID supplied" });
+  }
+
+  const result = await addToCart(req, { productId, variationId });
+
+  res.send({ success: true, result });
+}
+
+export default withSession(handler);
