b74ddeb074092e7a34a5bb4124289c6fe646e49c
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-05 15:12:13 +0100

    Basic alignment

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -1,6 +1,58 @@
+import {
+  faSearch,
+  faShoppingCart,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import styles from "./nav.module.css";
 
 export const Nav = () => {
-  console.log(styles);
-  return <p>Nav</p>;
+  return (
+    <div className={styles.header}>
+      <HomeImage />
+      <div>
+        <NavList>
+          <NavItem>About</NavItem>
+          <NavItem>Blog</NavItem>
+          <NavItem>Contact</NavItem>
+          <NavItem>
+            <NavIcon icon={faUser} />
+            My AceCentre
+          </NavItem>
+          <NavItem>
+            <NavIcon icon={faSearch} />
+            Search
+          </NavItem>
+          <NavItem>
+            <NavIcon icon={faShoppingCart} />
+            Checkout
+          </NavItem>
+          <NavItem>
+            <DonateButton />
+          </NavItem>
+        </NavList>
+      </div>
+    </div>
+  );
+};
+
+const NavIcon = ({ icon }) => {
+  return <FontAwesomeIcon className={styles.navIcon} icon={icon} />;
+};
+
+const HomeImage = () => {
+  return <span className={styles.homeImage}>Home</span>;
+};
+
+const DonateButton = () => {
+  return <button>Donate</button>;
+};
+
+const NavList = ({ children }) => {
+  return <ul className={styles.list}>{children}</ul>;
+};
+
+const NavItem = ({ children }) => {
+  return <li className={styles.listItem}>{children}</li>;
 };
