a6ce5386184a48b3b10caa3d5b85d52c5c9aad96
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-05 10:24:01 +0100

    Use new endpoint for checkout

--- pages/checkout.js
+++ pages/checkout.js
@@ -30,18 +30,20 @@ const CheckoutForm = () => {
   async function handleSubmit(event) {
     event.preventDefault();
 
-    try {
-      const source = await handleStripe();
+    const source = await handleStripe();
 
-      const result = await fetch("/api/cart/checkout", {
-        method: "POST",
-        body: JSON.stringify({ stripeSourceId: source.id }),
-      });
+    const value = event.target["free-input"].value;
+    let parsed = JSON.parse(value);
+    parsed.billingDetails.stripeSourceId = source.id;
 
-      console.log(result);
-    } catch (error) {
-      console.error(error);
-    }
+    const result = await fetch("/api/cart/checkout", {
+      method: "POST",
+      body: JSON.stringify(parsed),
+    });
+
+    const resultParsed = await result.json();
+
+    console.log({ parsed, result, resultParsed });
   }
 
   async function handleStripe() {
@@ -75,14 +77,40 @@ const CheckoutForm = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      {/* CardElement will load up the necessary CC fields */}
-      <CardElement
-        options={{
-          hidePostalCode: true, // We'll be sending up the postal ourselves
-        }}
-      />
-      <button disabled={!stripe}>Pay</button>
-    </form>
+    <>
+      <style jsx>{`
+        form {
+          display: grid;
+        }
+      `}</style>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="free-input">Free Input</label>
+        <textarea
+          name="free-input"
+          defaultValue={`
+        {
+            "billingDetails": {
+              "firstName": "Gavin",
+              "lastName": "Henderson",
+              "address1": "1 Real Street",
+              "city": "dundee",
+              "postcode": "DD1 5PT",
+              "phoneNo": "07898565478",
+              "email": "myemail@email.com",
+              "country": "GB"
+            }
+          }
+          
+        `}
+        ></textarea>
+
+        <CardElement
+          options={{
+            hidePostalCode: true, // We'll be sending up the postal ourselves
+          }}
+        />
+        <button disabled={!stripe}>Pay</button>
+      </form>
+    </>
   );
 };
