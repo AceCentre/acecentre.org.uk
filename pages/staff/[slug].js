76062aaec937c325db69835e29ae3ecdf03eb487
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-06 13:54:08 +0100

    Add subnav to staff page

--- pages/staff/[slug].js
+++ pages/staff/[slug].js
@@ -1,17 +1,26 @@
 import { Image } from "../../components/image";
+import { Nav } from "../../components/nav/nav";
+import { defaultNavItems, SubNav } from "../../components/sub-nav/sub-nav";
 import { getAllStaff, getEmployee } from "../../lib/staff/get-staff";
 
 export default function EmployeePage({ employee }) {
   return (
     <>
-      {employee.image && (
-        <Image
-          {...employee.image}
-          maxWidth={100}
-          alt={`A head shot of ${employee.name}`}
-        />
-      )}
-      <pre>{JSON.stringify(employee, null, 2)}</pre>
+      <header>
+        <Nav />
+        <SubNav navItems={defaultNavItems} />
+      </header>
+
+      <main>
+        {employee.image && (
+          <Image
+            {...employee.image}
+            maxWidth={100}
+            alt={`A head shot of ${employee.name}`}
+          />
+        )}
+        <pre>{JSON.stringify(employee, null, 2)}</pre>
+      </main>
     </>
   );
 }
