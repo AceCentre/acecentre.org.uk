e855dfe898dc3d0e5961250fd5e4d6d8d56fbc79
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-06 16:46:16 +0100

    Add global props to context

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,14 @@
 import "../styles/globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { DefaultHead } from "../components/default-head";
+import { GlobalsContext } from "../lib/global-props/context";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { globalProps = {}, ...pageProps } }) {
   return (
-    <>
+    <GlobalsContext.Provider value={globalProps}>
       <DefaultHead />
       <Component {...pageProps} />
-    </>
+    </GlobalsContext.Provider>
   );
 }
 
