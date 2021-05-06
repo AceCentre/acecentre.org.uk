777d44b402944e32a2145d11737747406bf3d428
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-06 11:35:35 +0100

    Setup basic subnav

--- components/sub-nav/sub-nav.js
+++ components/sub-nav/sub-nav.js
@@ -1,3 +1,42 @@
-export const SubNav = () => {
-  return <p>Nav</p>;
+import Link from "next/link";
+
+export const SubNav = ({ navItems }) => {
+  console.log(navItems);
+
+  return (
+    <nav>
+      <ul>
+        {navItems.map((item) => {
+          return (
+            <li key={item.href}>
+              <Link href={item.href}>{item.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
 };
+
+export const defaultNavItems = [
+  {
+    title: "Getting started",
+    href: "/getting-started",
+  },
+  {
+    title: "Resources",
+    href: "/resources",
+  },
+  {
+    title: "Services",
+    href: "/services",
+  },
+  {
+    title: "AceCentre Learning",
+    href: "/acecentre-learning",
+  },
+  {
+    title: "Get involved",
+    href: "/get-involved",
+  },
+];
