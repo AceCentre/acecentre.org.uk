7e2eb886079f5a570ec85aba6e8f456919e4fcbc
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-06 15:58:22 +0100

    Add cart count to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import { Nav } from "../components/nav/nav";
 import { defaultNavItems, SubNav } from "../components/sub-nav/sub-nav";
+import { useCartCount } from "../lib/cart/use-cart-count";
 
 export default function Home() {
+  const cartCount = useCartCount();
+
   return (
     <>
       <header>
-        <Nav />
+        <Nav numberOfItemsInCart={cartCount} />
         <SubNav navItems={defaultNavItems} />
       </header>
       <main></main>
