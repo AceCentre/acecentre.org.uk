39d8617cc50a1ea47723d6d76549a6ee912c436b
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-06 11:41:07 +0100

    Display all raw content

--- components/sub-nav/sub-nav.js
+++ components/sub-nav/sub-nav.js
@@ -10,6 +10,17 @@ export const SubNav = ({ navItems }) => {
           return (
             <li key={item.href}>
               <Link href={item.href}>{item.title}</Link>
+              <nav>
+                <ul>
+                  {item.subItems.map((subItem) => {
+                    return (
+                      <li key={subItem.href}>
+                        <Link href={subItem.href}>{subItem.title}</Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </nav>
             </li>
           );
         })}
@@ -18,25 +29,42 @@ export const SubNav = ({ navItems }) => {
   );
 };
 
+const subItems = [
+  { title: "Getting started", href: "/getting-started" },
+  { title: "Supporting language", href: "/supporting-language" },
+  { title: "Supporting access", href: "/supporting-access" },
+  { title: "Working in schools", href: "/working-in-schools" },
+  { title: "Family and friends", href: "/family-and-friends" },
+  { title: "Software", href: "/software" },
+  { title: "Alphabet resources", href: "/alphabet-resources" },
+  { title: "Symbol resources", href: "/symbol-resources" },
+  { title: "All resources", href: "/all-resources" },
+];
+
 export const defaultNavItems = [
   {
     title: "Getting started",
     href: "/getting-started",
+    subItems,
   },
   {
     title: "Resources",
     href: "/resources",
+    subItems,
   },
   {
     title: "Services",
     href: "/services",
+    subItems,
   },
   {
     title: "AceCentre Learning",
     href: "/acecentre-learning",
+    subItems,
   },
   {
     title: "Get involved",
     href: "/get-involved",
+    subItems,
   },
 ];
