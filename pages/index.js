779b8048d658b02b6d7778cb372ee193b08e294d
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-06 10:54:29 +0100

    Setup default head

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,8 @@
-import Head from "next/head";
 import { Nav } from "../components/nav/nav";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="Description" content="Put your description here." />
-      </Head>
       <header>
         <Nav />
       </header>
