7cadbb3bf19895100e691b6c2729adf9bfca0000
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-06 16:51:08 +0100

    Add global props to home

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
+import { Footer } from "../components/footer/footer";
 import { Nav } from "../components/nav/nav";
 import { defaultNavItems, SubNav } from "../components/sub-nav/sub-nav";
 import { useCartCount } from "../lib/cart/use-cart-count";
+import { useGlobalProps } from "../lib/global-props/hook";
+import { withGlobalProps } from "../lib/global-props/inject";
 
 export default function Home() {
   const cartCount = useCartCount();
+  const { currentYear } = useGlobalProps();
 
   return (
     <>
@@ -12,6 +16,9 @@ export default function Home() {
         <SubNav navItems={defaultNavItems} />
       </header>
       <main></main>
+      <Footer currentYear={currentYear} />
     </>
   );
 }
+
+export const getStaticProps = withGlobalProps();
