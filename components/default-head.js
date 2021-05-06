5746ffc6366ccc27ac6d44c14fa7ad142a6b58e9
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-06 10:54:29 +0100

    Setup default head

---
+++ components/default-head.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export const DefaultHead = () => {
+  return (
+    // TODO Fill this out
+    <Head>
+      <title>AceCentre</title>
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="Description" content="AceCentre" />
+    </Head>
+  );
+};
