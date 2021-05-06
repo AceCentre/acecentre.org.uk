d11ecb440c33fb0fea02f87b40c98a2d260c05a0
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-06 16:51:18 +0100

    Basic styling and content for footer

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,3 +1,73 @@
-export const Footer = () => {
-  return <p>Footer</p>;
+import Link from "next/link";
+import { Image } from "../image";
+import styles from "./footer.module.css";
+
+export const Footer = ({ currentYear }) => {
+  return (
+    <footer className={styles.container}>
+      <div className={styles.innerContainer}>
+        <div className={styles.topArea}>
+          <Image height={152} width={290} maxHeight={100} src="/nav-logo.png" />
+          <div>
+            <p>0800 048 7642</p>
+            <p>Office hours, Monday - Friday</p>
+            <p>
+              Call the helpline on <span>0800 080 3115</span>
+            </p>
+          </div>
+          <ul className={styles.list}>
+            {sitemapList.map((item) => (
+              <li key={item.href}>
+                <Link href={item.href}>{item.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li>Newsletter</li>
+            <li>Sitemap</li>
+            <li>Privacy Policy</li>
+            <li>Safeguarding</li>
+          </ul>
+          <p>© Copyright AceCentre {currentYear}</p>
+        </div>
+      </div>
+    </footer>
+  );
 };
+
+const sitemapList = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "About",
+    href: "/about",
+  },
+  {
+    title: "Getting started",
+    href: "/getting-started",
+  },
+  {
+    title: "Resources",
+    href: "/resources",
+  },
+  {
+    title: "Services",
+    href: "/services",
+  },
+  {
+    title: "Ace Centre Learning",
+    href: "/acecentre-learning",
+  },
+  {
+    title: "Get involved",
+    href: "/get-involved",
+  },
+  {
+    title: "My AceCentre",
+    href: "/my-acecentre",
+  },
+];
