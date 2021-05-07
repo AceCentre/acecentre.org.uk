1b07bab916025ddfbbdf1bebc4d5a3432a804e89
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-07 16:07:50 +0100

    Finish get involved

--- components/get-involved/get-involved.js
+++ components/get-involved/get-involved.js
@@ -1,5 +1,24 @@
+import Link from "next/link";
 import styles from "./get-involved.module.css";
 
 export const GetInvolved = () => {
-  return <p>Get Invovled</p>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.innerContainer}>
+        <h1>Get involved</h1>
+        <p>
+          Your support is vital to our ongoing work to ensure that children and
+          adults with the most challenging communication difficulties have free
+          access to the advice, information and support they need to fulfil
+          their potential
+        </p>
+        <div className={styles.buttonContainer}>
+          <div className={styles.getInvolved}>
+            <Link href="/get-involved">Ways to get involved</Link>
+          </div>
+          <Link href="/donate">Make a donation</Link>
+        </div>
+      </div>
+    </div>
+  );
 };
