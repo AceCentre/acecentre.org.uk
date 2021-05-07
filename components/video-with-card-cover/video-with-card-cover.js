5b29c445d5439ab62c706dcb608098d010a16fab
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-07 10:36:03 +0100

    Basic card video layout

--- components/video-with-card-cover/video-with-card-cover.js
+++ components/video-with-card-cover/video-with-card-cover.js
@@ -1,3 +1,25 @@
+import Link from "next/link";
+import { ImageSelector } from "../image";
+
+import styles from "./video-with-card-cover.module.css";
+
 export const VideoWithCardCover = () => {
-  return <p>Test</p>;
+  return (
+    <>
+      <div className={styles.imageContainer}>
+        <ImageSelector height="400px" width="100%" src="/placeholder.jpeg" />
+      </div>
+      <div className={styles.cardContainer}>
+        <div className={styles.cardInnerContainer}>
+          <div className={styles.card}>
+            <p>
+              Working with people of all ages to overcome communication
+              challenges
+            </p>
+            <Link href="/about">Learn about us</Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
