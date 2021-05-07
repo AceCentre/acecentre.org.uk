32d2a49a9b4e6a572fbc3fe3daf996dc2e8a97d6
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-07 15:48:43 +0100

    Scaffold get invovled

---
+++ components/get-involved/get-involved.stories.js
@@ -0,0 +1,11 @@
+import { GetInvolved } from "./get-involved";
+
+export default {
+  title: "Home/GetInvolved",
+  component: GetInvolved,
+};
+
+const Template = (args) => <GetInvolved {...args} />;
+
+export const Standard = Template.bind({});
+Standard.args = {};
