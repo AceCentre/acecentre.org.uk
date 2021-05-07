2dc1137f036de982e02b78609012135b1fa429ae
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-07 15:35:24 +0100

    Finish featured story

--- components/featured-story/featured-story.js
+++ components/featured-story/featured-story.js
@@ -1,5 +1,29 @@
 import styles from "./featured-story.module.css";
 
-export const FeaturedStory = ({ youtubeVideo, summary, title }) => {
-  return <p>Test</p>;
+import YouTube from "react-youtube";
+import Link from "next/link";
+
+export const FeaturedStory = ({ youtubeVideo, summary, title, slug }) => {
+  const videoId = urlToVideoId(youtubeVideo);
+
+  return (
+    <div className={styles.container}>
+      <YouTube className={styles.video} videoId={videoId} />
+      <div>
+        <h1>{title}</h1>
+        {/* We are dangerously setting inner HTML here because the summary comes from wordpress */}
+        <div dangerouslySetInnerHTML={{ __html: summary }}></div>
+        <div className={styles.buttonContainer}>
+          <div className={styles.findOutMore}>
+            <Link href={`/stories/${slug}`}>Full story</Link>
+          </div>
+          <Link href="/stories">Read more stories</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const urlToVideoId = (url) => {
+  return new URL(url).searchParams.get("v");
 };
