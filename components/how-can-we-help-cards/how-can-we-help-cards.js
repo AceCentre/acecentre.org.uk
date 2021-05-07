99dec22b32ade7dea8cb8114be249fa794525ba9
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-07 11:40:33 +0100

    Finish how can we help section

--- components/how-can-we-help-cards/how-can-we-help-cards.js
+++ components/how-can-we-help-cards/how-can-we-help-cards.js
@@ -1,7 +1,39 @@
 import styles from "./how-can-we-help-cards.module.css";
 
 export const HowCanWeHelpCards = () => {
-  console.log(styles);
+  return (
+    <div className={styles.container}>
+      <h1>How can we help</h1>
+      <div className={styles.topCards}>
+        <div>
+          <img width="450px" height="250px" src="/placeholder.jpeg" />
+          <h2>Getting started</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+        </div>
+        <div>
+          <img width="450px" height="250px" src="/placeholder.jpeg" />
+          <h2>Resources</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+        </div>
+      </div>
 
-  return <p>Test</p>;
+      <div className={styles.bottomCards}>
+        <div>
+          <img width="300px" height="250px" src="/placeholder.jpeg" />
+          <h2>Ace Centre Learning</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+        </div>
+        <div>
+          <img width="300px" height="250px" src="/placeholder.jpeg" />
+          <h2>Assessment</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+        </div>
+        <div>
+          <img width="300px" height="250px" src="/placeholder.jpeg" />
+          <h2>Helpline</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+        </div>
+      </div>
+    </div>
+  );
 };
