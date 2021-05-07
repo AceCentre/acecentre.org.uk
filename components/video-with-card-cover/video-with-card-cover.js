7ad1aaa114a6143767d54e497ffce6b6b7230548
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-07 10:44:09 +0100

    Finish basic component

--- components/video-with-card-cover/video-with-card-cover.js
+++ components/video-with-card-cover/video-with-card-cover.js
@@ -1,13 +1,12 @@
 import Link from "next/link";
-import { ImageSelector } from "../image";
 
 import styles from "./video-with-card-cover.module.css";
 
 export const VideoWithCardCover = () => {
   return (
-    <>
+    <div className={styles.container}>
       <div className={styles.imageContainer}>
-        <ImageSelector height="400px" width="100%" src="/placeholder.jpeg" />
+        <img height="400px" width="100%" src="/placeholder.jpeg" />
       </div>
       <div className={styles.cardContainer}>
         <div className={styles.cardInnerContainer}>
@@ -16,10 +15,12 @@ export const VideoWithCardCover = () => {
               Working with people of all ages to overcome communication
               challenges
             </p>
-            <Link href="/about">Learn about us</Link>
+            <div className={styles.aboutUs}>
+              <Link href="/donate">Learn about us</Link>
+            </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
