de4a576736b29bbb02650a2adbfd0775a58438f0
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-10 16:21:02 +0100

    Refactor VideoWithCard

--- components/video-with-card-cover/video-with-card-cover.js
+++ components/video-with-card-cover/video-with-card-cover.js
@@ -1,21 +1,11 @@
-import Link from "next/link";
-
 import styles from "./video-with-card-cover.module.css";
 
-export const VideoWithCardCover = () => {
+export const VideoWithCardCover = ({ children }) => {
   return (
     <div className={styles.container}>
       <div className={styles.cardContainer}>
         <div className={styles.cardInnerContainer}>
-          <div className={styles.card}>
-            <p>
-              Working with people of all ages to overcome communication
-              challenges
-            </p>
-            <div className={styles.aboutUs}>
-              <Link href="/donate">Learn about us</Link>
-            </div>
-          </div>
+          <div className={styles.card}>{children}</div>
         </div>
       </div>
     </div>
