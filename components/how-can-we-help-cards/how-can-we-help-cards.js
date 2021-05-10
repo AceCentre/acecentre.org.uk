e6441b28b84655031c203e0ce4f47ebbc8591c07
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-10 13:46:32 +0100

    Make how can we help responsive

--- components/how-can-we-help-cards/how-can-we-help-cards.js
+++ components/how-can-we-help-cards/how-can-we-help-cards.js
@@ -4,55 +4,53 @@ export const HowCanWeHelpCards = () => {
   return (
     <div className={styles.container}>
       <h1>How can we help</h1>
-      <div className={styles.topCards}>
-        <div>
+      <div className={styles.allCards}>
+        <div className={styles.gettingStarted}>
           <img
             alt="placeholder"
-            width="450px"
-            height="250px"
+            width="100%"
+            // height="250px"
             src="/placeholder.jpeg"
           />
           <h2>Getting started</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
         </div>
-        <div>
+        <div className={styles.resources}>
           <img
             alt="placeholder"
-            width="450px"
-            height="250px"
+            width="100%"
+            // height="250px"
             src="/placeholder.jpeg"
           />
           <h2>Resources</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
         </div>
-      </div>
 
-      <div className={styles.bottomCards}>
-        <div>
+        <div className={styles.aceCentreLearning}>
           <img
             alt="placeholder"
-            width="300px"
-            height="250px"
+            width="100%"
+            // height="250px"
             src="/placeholder.jpeg"
           />
           <h2>Ace Centre Learning</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
         </div>
-        <div>
+        <div className={styles.assessment}>
           <img
             alt="placeholder"
-            width="300px"
-            height="250px"
+            width="100%"
+            // height="250px"
             src="/placeholder.jpeg"
           />
           <h2>Assessment</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
         </div>
-        <div>
+        <div className={styles.helpline}>
           <img
             alt="placeholder"
-            width="300px"
-            height="250px"
+            width="100%"
+            // height="250px"
             src="/placeholder.jpeg"
           />
           <h2>Helpline</h2>
