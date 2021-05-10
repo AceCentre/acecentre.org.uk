883702e35769c176186fe84fc218518de7d84895
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-10 16:21:13 +0100

    Use new component

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,6 +1,7 @@
 import { Footer } from "../../components/footer/footer";
 import { Nav } from "../../components/nav/nav";
 import { defaultNavItems, SubNav } from "../../components/sub-nav/sub-nav";
+import { VideoWithCardCover } from "../../components/video-with-card-cover/video-with-card-cover";
 import { useCartCount } from "../../lib/cart/use-cart-count";
 import { useGlobalProps } from "../../lib/global-props/hook";
 import { withGlobalProps } from "../../lib/global-props/inject";
@@ -15,7 +16,15 @@ export default function Home() {
         <Nav numberOfItemsInCart={cartCount} />
         <SubNav navItems={defaultNavItems} />
       </header>
-      <main></main>
+      <main>
+        <VideoWithCardCover>
+          <h1>About us</h1>
+          <p>
+            Dedicated to supporting people with complex communication
+            difficulties
+          </p>
+        </VideoWithCardCover>
+      </main>
       <Footer currentYear={currentYear} />
     </>
   );
