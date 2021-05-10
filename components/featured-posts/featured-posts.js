d986c66799baf51857193bb557d4364d56cfb570
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-10 10:46:51 +0100

    Display all content

--- components/featured-posts/featured-posts.js
+++ components/featured-posts/featured-posts.js
@@ -1,3 +1,20 @@
-export const FeaturedPosts = () => {
-  return <p>Posts</p>;
+import Link from "next/link";
+
+export const FeaturedPosts = ({ title, viewAllLink, posts }) => {
+  return (
+    <div>
+      {title && <h1>{title}</h1>}
+      {viewAllLink && <Link href="viewAllLink">View All</Link>}
+      {posts.map((post) => {
+        return (
+          <Link key={post.slug} href={`/blog/${post.slug}`}>
+            <a>
+              <img src="/placeholder.jpeg" />
+              <h2>{post.title}</h2>
+            </a>
+          </Link>
+        );
+      })}
+    </div>
+  );
 };
--- components/featured-posts/featured-posts.stories.js
+++ components/featured-posts/featured-posts.stories.js
@@ -8,4 +8,46 @@ export default {
 const Template = (args) => <FeaturedPosts {...args} />;
 
 export const Standard = Template.bind({});
-Standard.args = {};
+Standard.args = {
+  title: "Latest from the blog",
+  viewAllLink: "/blog",
+  posts: [
+    {
+      title: "Ace Centre Festive opening times Christmas and New Year 2020",
+      slug: "ace-centre-festive-opening-times-christmas-and-new-year-2020",
+      featuredImage: null,
+    },
+    {
+      title: "COVID-19 Update – clients at the centre of our COVID-19 strategy",
+      slug: "covid-19-update-clients-at-the-centre-of-our-covid-19-strategy",
+      featuredImage: null,
+    },
+    {
+      title: "Ace Centre COVID-19 Position Statement",
+      slug: "ace-centre-covid-19-position-statement",
+      featuredImage: null,
+    },
+  ],
+};
+
+export const NoTitle = Template.bind({});
+NoTitle.args = {
+  viewAllLink: "/blog",
+  posts: [
+    {
+      title: "Ace Centre Festive opening times Christmas and New Year 2020",
+      slug: "ace-centre-festive-opening-times-christmas-and-new-year-2020",
+      featuredImage: null,
+    },
+    {
+      title: "COVID-19 Update – clients at the centre of our COVID-19 strategy",
+      slug: "covid-19-update-clients-at-the-centre-of-our-covid-19-strategy",
+      featuredImage: null,
+    },
+    {
+      title: "Ace Centre COVID-19 Position Statement",
+      slug: "ace-centre-covid-19-position-statement",
+      featuredImage: null,
+    },
+  ],
+};
