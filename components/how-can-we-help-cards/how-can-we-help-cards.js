20105eb6a22b3c51ccd65e12d4950e1efe8c4b2f
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-10 12:12:31 +0100

    Add alt tag

--- components/how-can-we-help-cards/how-can-we-help-cards.js
+++ components/how-can-we-help-cards/how-can-we-help-cards.js
@@ -6,12 +6,22 @@ export const HowCanWeHelpCards = () => {
       <h1>How can we help</h1>
       <div className={styles.topCards}>
         <div>
-          <img width="450px" height="250px" src="/placeholder.jpeg" />
+          <img
+            alt="placeholder"
+            width="450px"
+            height="250px"
+            src="/placeholder.jpeg"
+          />
           <h2>Getting started</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
         </div>
         <div>
-          <img width="450px" height="250px" src="/placeholder.jpeg" />
+          <img
+            alt="placeholder"
+            width="450px"
+            height="250px"
+            src="/placeholder.jpeg"
+          />
           <h2>Resources</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
         </div>
@@ -19,17 +29,32 @@ export const HowCanWeHelpCards = () => {
 
       <div className={styles.bottomCards}>
         <div>
-          <img width="300px" height="250px" src="/placeholder.jpeg" />
+          <img
+            alt="placeholder"
+            width="300px"
+            height="250px"
+            src="/placeholder.jpeg"
+          />
           <h2>Ace Centre Learning</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
         </div>
         <div>
-          <img width="300px" height="250px" src="/placeholder.jpeg" />
+          <img
+            alt="placeholder"
+            width="300px"
+            height="250px"
+            src="/placeholder.jpeg"
+          />
           <h2>Assessment</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
         </div>
         <div>
-          <img width="300px" height="250px" src="/placeholder.jpeg" />
+          <img
+            alt="placeholder"
+            width="300px"
+            height="250px"
+            src="/placeholder.jpeg"
+          />
           <h2>Helpline</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
         </div>
