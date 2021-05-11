012fba42b6e27bc89733772ff193765a59ff4647
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-11 16:15:48 +0100

    Setup plop

---
+++ plopfile.js
@@ -0,0 +1,51 @@
+module.exports = function (plop) {
+  // create your generators here
+  plop.setGenerator("Component", {
+    description:
+      "Generate a new component along with the CSS and storybook Story",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message:
+          "The name of the files eg (my-component). This will also be used to name the component",
+      },
+      {
+        type: "input",
+        name: "storyName",
+        message: "The name of the story eg (/About/Staff/MyComponent)",
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "components/{{name}}/{{name}}.module.css",
+        templateFile: "plop-templates/component/module.css",
+      },
+      {
+        type: "add",
+        path: "components/{{name}}/{{name}}.stories.js",
+        templateFile: "plop-templates/component/story.stories.js.hbs",
+      },
+      {
+        type: "add",
+        path: "components/{{name}}/{{name}}.js",
+        templateFile: "plop-templates/component/component.js.hbs",
+      },
+    ],
+  });
+
+  plop.setHelper("componentName", (txt) => makeCamelCase(txt));
+};
+
+// this-is-an-example => ThisIsAnExample
+function makeCamelCase(str) {
+  const arr = str.split("-");
+  const capital = arr.map(
+    (item) => item.charAt(0).toUpperCase() + item.slice(1).toLowerCase()
+  );
+  // ^-- change here.
+  const capitalString = capital.join("");
+
+  return capitalString;
+}
