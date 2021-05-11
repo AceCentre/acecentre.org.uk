f07a14381cbc2333e96c9c2c86e31fad97135d81
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-11 15:47:52 +0100

    Add page title

---
+++ components/page-title/page-title.js
@@ -0,0 +1,10 @@
+import styles from "./page-title.module.css";
+
+export const PageTitle = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Our people</h1>
+      <p>Our people are our greatest strength</p>
+    </div>
+  );
+};
