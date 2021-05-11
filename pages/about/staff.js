469ec81fb1e2fa9e8894b9f18bfbe20d1a68ffc3
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-11 15:37:18 +0100

    Add all staff to props

--- pages/about/staff.js
+++ pages/about/staff.js
@@ -4,8 +4,9 @@ import { defaultNavItems, SubNav } from "../../components/sub-nav/sub-nav";
 import { useCartCount } from "../../lib/cart/use-cart-count";
 import { useGlobalProps } from "../../lib/global-props/hook";
 import { withGlobalProps } from "../../lib/global-props/inject";
+import { getAllStaff } from "../../lib/staff/get-staff";
 
-export default function StaffPage() {
+export default function StaffPage({ allStaff }) {
   const cartCount = useCartCount();
   const { currentYear } = useGlobalProps();
 
@@ -22,5 +23,7 @@ export default function StaffPage() {
 }
 
 export const getStaticProps = withGlobalProps(async () => {
-  return { props: {} };
+  const allStaff = await getAllStaff();
+
+  return { props: { allStaff } };
 });
