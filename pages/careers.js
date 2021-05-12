3601cd50700bf964bbf8df6028c8e87905815b84
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-12 15:19:17 +0100

    Add a card

--- pages/careers.js
+++ pages/careers.js
@@ -1,6 +1,7 @@
 import { Footer } from "../components/footer/footer";
 import { Nav } from "../components/nav/nav";
 import { defaultNavItems, SubNav } from "../components/sub-nav/sub-nav";
+import { VideoWithCardCover } from "../components/video-with-card-cover/video-with-card-cover";
 import { useCartCount } from "../lib/cart/use-cart-count";
 import { useGlobalProps } from "../lib/global-props/hook";
 import { withGlobalProps } from "../lib/global-props/inject";
@@ -15,7 +16,15 @@ export default function Careers() {
         <Nav numberOfItemsInCart={cartCount} />
         <SubNav navItems={defaultNavItems} />
       </header>
-      <main></main>
+      <main>
+        <VideoWithCardCover>
+          <h1>Work with us</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Pellentesque gravida rutrum mattis.
+          </p>
+        </VideoWithCardCover>
+      </main>
       <Footer currentYear={currentYear} />
     </>
   );
