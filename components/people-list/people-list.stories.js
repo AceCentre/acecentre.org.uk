89ea15c9a24645452304092118fd1315c171b47c
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-12 14:03:25 +0100

    Create people list

--- components/people-list/people-list.stories.js
+++ components/people-list/people-list.stories.js
@@ -1,3 +1,5 @@
+import { Image } from "../image";
+import { STAFF_LIST_DEMO } from "../staff-list/static-staff-list";
 import { PeopleList } from "./people-list";
 
 export default {
@@ -7,5 +9,25 @@ export default {
 
 const Template = (args) => <PeopleList {...args} />;
 
-export const Standard = Template.bind({});
-Standard.args = {};
+const StaffCard = ({ person }) => {
+  const staffMember = person;
+
+  return (
+    <>
+      <Image
+        alt={`Head shot of ${staffMember.name}`}
+        {...staffMember.image}
+        maxWidth={200}
+      />
+      <p>{staffMember.name}</p>
+      <p>{staffMember.role.trim()}</p>
+      <p>{staffMember.location.trim()}</p>
+    </>
+  );
+};
+
+export const StaffExample = Template.bind({});
+StaffExample.args = {
+  peopleList: STAFF_LIST_DEMO,
+  renderCardContent: StaffCard,
+};
