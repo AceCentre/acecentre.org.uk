69081365c237896887fe763984b1d2c8b1315dec
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-12 15:17:44 +0100

    Add careers page

---
+++ pages/careers.js
@@ -0,0 +1,24 @@
+import { Footer } from "../components/footer/footer";
+import { Nav } from "../components/nav/nav";
+import { defaultNavItems, SubNav } from "../components/sub-nav/sub-nav";
+import { useCartCount } from "../lib/cart/use-cart-count";
+import { useGlobalProps } from "../lib/global-props/hook";
+import { withGlobalProps } from "../lib/global-props/inject";
+
+export default function Careers() {
+  const cartCount = useCartCount();
+  const { currentYear } = useGlobalProps();
+
+  return (
+    <>
+      <header>
+        <Nav numberOfItemsInCart={cartCount} />
+        <SubNav navItems={defaultNavItems} />
+      </header>
+      <main></main>
+      <Footer currentYear={currentYear} />
+    </>
+  );
+}
+
+export const getStaticProps = withGlobalProps();
