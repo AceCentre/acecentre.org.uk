1e6d7f7ea142cc1f16b75fd734bb3fb2d0ea3d7f
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-12 14:13:42 +0100

    Finish trustee page

--- pages/about/trustees.js
+++ pages/about/trustees.js
@@ -2,10 +2,12 @@ import { Footer } from "../../components/footer/footer";
 import { Nav } from "../../components/nav/nav";
 import { PageTitle } from "../../components/page-title/page-title";
 import { defaultNavItems, SubNav } from "../../components/sub-nav/sub-nav";
+import { TrusteeList } from "../../components/trustee-list/trustee-list";
 import { useCartCount } from "../../lib/cart/use-cart-count";
 import { useGlobalProps } from "../../lib/global-props/hook";
 import { withGlobalProps } from "../../lib/global-props/inject";
 import { getAllTrustees } from "../../lib/trustees/get-trustees";
+import styles from "../../styles/trustees.module.css";
 
 export default function AllTrusteesPage({ allTrustees }) {
   const cartCount = useCartCount();
@@ -22,6 +24,10 @@ export default function AllTrusteesPage({ allTrustees }) {
           heading="Our trustees"
           description="Meet the AceCentre trustees"
         />
+        <div className={styles.container}>
+          <h2>Meet our trustees</h2>
+        </div>
+        <TrusteeList trusteeList={allTrustees} />
       </main>
       <Footer currentYear={currentYear} />
     </>
