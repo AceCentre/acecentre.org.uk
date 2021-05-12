6c048032023b3bbf4453bef056bba6ca2bb23b07
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-12 10:56:42 +0100

    Setup filtering

--- pages/about/staff.js
+++ pages/about/staff.js
@@ -1,8 +1,13 @@
-import { FilterPeople } from "../../components/filter-people/filter-people";
+import { useState } from "react";
+import {
+  FilterPeople,
+  OPTIONS,
+} from "../../components/filter-people/filter-people";
 import { Footer } from "../../components/footer/footer";
 import { MeetOurPeople } from "../../components/meet-our-people/meet-our-people";
 import { Nav } from "../../components/nav/nav";
 import { PageTitle } from "../../components/page-title/page-title";
+import { StaffList } from "../../components/staff-list/staff-list";
 import { defaultNavItems, SubNav } from "../../components/sub-nav/sub-nav";
 import { useCartCount } from "../../lib/cart/use-cart-count";
 import { useGlobalProps } from "../../lib/global-props/hook";
@@ -12,6 +17,8 @@ import { getAllStaff } from "../../lib/staff/get-staff";
 export default function StaffPage({ allStaff }) {
   const cartCount = useCartCount();
   const { currentYear } = useGlobalProps();
+  const [currentFilter, setCurrentFilter] = useState(OPTIONS.ALL);
+  const filteredStaff = allStaff.filter(staffFilters[currentFilter]);
 
   return (
     <>
@@ -22,7 +29,8 @@ export default function StaffPage({ allStaff }) {
       <main>
         <PageTitle />
         <MeetOurPeople />
-        <FilterPeople />
+        <FilterPeople onChange={setCurrentFilter} />
+        <StaffList staffList={filteredStaff} />
       </main>
       <Footer currentYear={currentYear} />
     </>
@@ -34,3 +42,27 @@ export const getStaticProps = withGlobalProps(async () => {
 
   return { props: { allStaff } };
 });
+
+const staffFilters = {
+  // Everyone is always included in all
+  [OPTIONS.ALL]: () => {
+    return true;
+  },
+
+  // Only staff members within the leadership team group
+  [OPTIONS.LEADERSHIP]: (currentStaff) => {
+    return currentStaff.groups.includes("Leadership Team");
+  },
+
+  // Only staff members with the location set to Oldham
+  [OPTIONS.NORTH]: (currentStaff) => {
+    const location = currentStaff.location;
+    return location === "Oldham";
+  },
+
+  // Only staff members with the location set to Abingdon
+  [OPTIONS.SOUTH]: (currentStaff) => {
+    const location = currentStaff.location;
+    return location === "Abingdon";
+  },
+};
