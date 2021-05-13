4ed91f8ea93c0782225b9aa6224017024976137a
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-13 10:41:25 +0100

    Scaffold story highglight

--- pages/stories/index.js
+++ pages/stories/index.js
@@ -1,6 +1,9 @@
 import { Footer } from "../../components/footer/footer";
 import { Nav } from "../../components/nav/nav";
+import { PageTitle } from "../../components/page-title/page-title";
+import { StoryHighlight } from "../../components/story-highlight/story-highlight";
 import { defaultNavItems, SubNav } from "../../components/sub-nav/sub-nav";
+import { VideoWithCardCover } from "../../components/video-with-card-cover/video-with-card-cover";
 import { useCartCount } from "../../lib/cart/use-cart-count";
 import { useGlobalProps } from "../../lib/global-props/hook";
 import { withGlobalProps } from "../../lib/global-props/inject";
@@ -15,7 +18,19 @@ export default function StoriesLandingPage() {
         <Nav numberOfItemsInCart={cartCount} />
         <SubNav navItems={defaultNavItems} />
       </header>
-      <main></main>
+      <main>
+        <PageTitle
+          heading="People we support"
+          description="Its not the work that we do here at the AceCentre that's amazing, it's the people we work with"
+        />
+        <VideoWithCardCover>
+          <p>
+            An insight into Paul and Julie&apos;s lives and how they manage
+            their challenges
+          </p>
+        </VideoWithCardCover>
+        <StoryHighlight />
+      </main>
       <Footer currentYear={currentYear} />
     </>
   );
