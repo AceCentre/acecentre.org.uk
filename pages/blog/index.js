017ac36380b3a1c0b7101f3e25affb46ae70467b
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-14 11:01:11 +0100

    Add all categories

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -21,7 +21,7 @@ export default function Home() {
       <main>
         <BlogSearch />
         <BlogCategoryGrid />
-        <AllCategories />
+        <AllCategories blogCategories={blogCategories} />
       </main>
       <Footer currentYear={currentYear} />
     </>
@@ -31,3 +31,20 @@ export default function Home() {
 export const getStaticProps = withGlobalProps(async () => {
   return { props: {} };
 });
+
+const blogCategories = [
+  { href: "/blog/category/1", title: "Category One" },
+  { href: "/blog/category/2", title: "Category Two" },
+  { href: "/blog/category/3", title: "Category Three" },
+  { href: "/blog/category/4", title: "Category Four" },
+  { href: "/blog/category/5", title: "Category Five" },
+  { href: "/blog/category/6", title: "Category Six" },
+  { href: "/blog/category/7", title: "Category Seven" },
+  { href: "/blog/category/8", title: "Category Eight" },
+  { href: "/blog/category/9", title: "Category Nine" },
+  { href: "/blog/category/10", title: "Category Ten" },
+  { href: "/blog/category/11", title: "Category Eleven" },
+  { href: "/blog/category/12", title: "Category Twelve" },
+  { href: "/blog/category/13", title: "Category Three" },
+  { href: "/blog/category/14", title: "Category Fourteen" },
+];
