05f7c37c26e61bd587e9c03ed471c34ecbb236ff
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-14 14:48:08 +0100

    Use slug instead of href

--- components/all-categories/all-categories.stories.js
+++ components/all-categories/all-categories.stories.js
@@ -8,20 +8,20 @@ export default {
 const Template = (args) => <AllCategories {...args} />;
 
 const blogCategories = [
-  { href: "/blog/category/1", title: "Category One" },
-  { href: "/blog/category/2", title: "Category Two" },
-  { href: "/blog/category/3", title: "Category Three" },
-  { href: "/blog/category/4", title: "Category Four" },
-  { href: "/blog/category/5", title: "Category Five" },
-  { href: "/blog/category/6", title: "Category Six" },
-  { href: "/blog/category/7", title: "Category Seven" },
-  { href: "/blog/category/8", title: "Category Eight" },
-  { href: "/blog/category/9", title: "Category Nine" },
-  { href: "/blog/category/10", title: "Category Ten" },
-  { href: "/blog/category/11", title: "Category Eleven" },
-  { href: "/blog/category/12", title: "Category Twelve" },
-  { href: "/blog/category/13", title: "Category Three" },
-  { href: "/blog/category/14", title: "Category Fourteen" },
+  { slug: "1", title: "Category One" },
+  { slug: "2", title: "Category Two" },
+  { slug: "3", title: "Category Three" },
+  { slug: "4", title: "Category Four" },
+  { slug: "5", title: "Category Five" },
+  { slug: "6", title: "Category Six" },
+  { slug: "7", title: "Category Seven" },
+  { slug: "8", title: "Category Eight" },
+  { slug: "9", title: "Category Nine" },
+  { slug: "10", title: "Category Ten" },
+  { slug: "11", title: "Category Eleven" },
+  { slug: "12", title: "Category Twelve" },
+  { slug: "13", title: "Category Three" },
+  { slug: "14", title: "Category Fourteen" },
 ];
 
 export const Standard = Template.bind({});
