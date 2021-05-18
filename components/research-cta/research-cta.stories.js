1dcd35a2d5a1964b7a607e1992f2bd47bf226be9
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-18 11:42:53 +0100

    Scaffold research CTA

---
+++ components/research-cta/research-cta.stories.js
@@ -0,0 +1,11 @@
+import { ResearchCta } from "./research-cta";
+
+export default {
+  title: "Projects/ResearchCta",
+  component: ResearchCta,
+};
+
+const Template = (args) => <ResearchCta {...args} />;
+
+export const Standard = Template.bind({});
+Standard.args = {};
