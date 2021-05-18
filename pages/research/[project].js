6f8c28f3cedb64c13fb64465584675744895fba8
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-18 10:43:11 +0100

    Scaffold research pages

---
+++ pages/research/[project].js
@@ -0,0 +1,31 @@
+import { Footer } from "../../components/footer/footer";
+import { Nav } from "../../components/nav/nav";
+import { defaultNavItems, SubNav } from "../../components/sub-nav/sub-nav";
+import { useCartCount } from "../../lib/cart/use-cart-count";
+import { useGlobalProps } from "../../lib/global-props/hook";
+import { withGlobalProps } from "../../lib/global-props/inject";
+
+export default function CategoryPage() {
+  const cartCount = useCartCount();
+  const { currentYear } = useGlobalProps();
+
+  return (
+    <>
+      <header>
+        <Nav numberOfItemsInCart={cartCount} />
+        <SubNav navItems={defaultNavItems} />
+      </header>
+      <main></main>
+      <Footer currentYear={currentYear} />
+    </>
+  );
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: false,
+  };
+}
+
+export const getStaticProps = withGlobalProps();
