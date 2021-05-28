a5161efc8c3d7efdcc1cf8d24426724ea3d39048
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-28 12:10:59 +0100

    WIP

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-module.exports = {
+const withPlugins = require("next-compose-plugins");
+const withCSS = require("@zeit/next-css");
+const withTM = require("next-transpile-modules")([
+  "@adobe/react-spectrum",
+  "@spectrum-icons/.*",
+  "@react-spectrum/.*",
+]);
+
+module.exports = withPlugins([withCSS, withTM], {
   target: "serverless",
   images: {
     domains: ["acecentre.org.uk", "internal.acecentre.org.uk"],
   },
-};
+});
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,18 @@
 import "../styles/globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import { SSRProvider, Provider, defaultTheme } from "@adobe/react-spectrum";
 import { DefaultHead } from "../components/default-head";
 import { GlobalsContext } from "../lib/global-props/context";
 
 function MyApp({ Component, pageProps: { globalProps = {}, ...pageProps } }) {
   return (
     <GlobalsContext.Provider value={globalProps}>
+      <SSRProvider>
+        <Provider theme={defaultTheme}>
+          <Component {...pageProps} />
+        </Provider>
+      </SSRProvider>
       <DefaultHead />
-      <Component {...pageProps} />
     </GlobalsContext.Provider>
   );
 }
