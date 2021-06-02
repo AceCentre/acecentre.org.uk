fa8c7ad76095681652863674fa80f596bae3ae4d
Gavin Henderson <github@gavinhenderson.co.uk>
2021-06-02 13:56:10 +0100

    Add pagination

---
+++ components/pagination/pagination.stories.js
@@ -0,0 +1,11 @@
+import { Pagination } from "./pagination";
+
+export default {
+  title: "Resources/Pagination",
+  component: Pagination,
+};
+
+const Template = (args) => <Pagination {...args} />;
+
+export const Standard = Template.bind({});
+Standard.args = {};
