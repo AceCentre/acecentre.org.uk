831b71f0fb1cd66231e9e7e9792f6420723a30d5
Gavin Henderson <github@gavinhenderson.co.uk>
2021-06-03 15:39:41 +0100

    Add some tests

--- cypress/integration/resources-browse.spec.js
+++ cypress/integration/resources-browse.spec.js
@@ -1,17 +1,39 @@
 context("Resources Browse", () => {
   it("Completes a search and has the correct search text on the next page", () => {
+    const searchText = "test";
+
     cy.visit("/");
 
     // Click the resources link in the nav
     cy.findByTestId("subnav").findByRole("link", { name: "Resources" }).click();
 
     // Find the search box and then type 'a'
-    cy.findByRole("textbox", { name: "Search resources" }).type("a{enter}");
+    cy.findByRole("textbox", { name: "Search resources" }).type(
+      `${searchText}{enter}`
+    );
 
-    // const searchButton = cy.findByRole("button", { name: "Search" });
-    // searchButton.click();
+    cy.contains("You searched for").contains(searchText);
+  });
 
-    // const resultsElement = cy.findByRole("heading", { name: /Results/i });
-    // resultsElement.contains(searchText);
+  it("Make a search from the resources page", () => {
+    const searchText = "test";
+
+    cy.visit("/resources/all");
+
+    // Find the search box and then type 'a'
+    cy.get('input[name="searchText"]').type(`${searchText}{enter}`);
+
+    cy.contains("You searched for").contains(searchText);
+  });
+
+  it("Make a search from the resources page", () => {
+    const searchText = "test";
+
+    cy.visit("/resources/all");
+
+    // Find the search box and then type 'a'
+    cy.get('input[name="searchText"]').type(`${searchText}{enter}`);
+
+    cy.contains("You searched for").contains(searchText);
   });
 });
