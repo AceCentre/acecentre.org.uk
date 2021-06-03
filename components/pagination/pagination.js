e61247d267661ff8e72322fa12f8dfeb30ee8ef2
Gavin Henderson <github@gavinhenderson.co.uk>
2021-06-03 10:01:35 +0100

    Pagination now works

--- components/pagination/pagination.js
+++ components/pagination/pagination.js
@@ -1,27 +1,57 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import styles from "./pagination.module.css";
 
 export const Pagination = ({ pageCount, currentPage }) => {
+  const { query } = useRouter();
+
   let arrayOfNumbers = [];
   for (let i = 1; i < pageCount + 1; i++) {
     arrayOfNumbers.push(i);
   }
 
+  const changePageLink = (newPageNumber) => {
+    const newQuery = { ...query, page: newPageNumber };
+    const queryStringPairs = [];
+
+    for (const [key, value] of Object.entries(newQuery)) {
+      if (value === null) continue;
+      if (value === "") continue;
+
+      queryStringPairs.push(`${key}=${value}`);
+    }
+
+    const newQueryString = queryStringPairs.join("&");
+
+    return `/resources/all?${newQueryString}`;
+  };
+
+  const currentPageAsInt = parseInt(currentPage);
+
   return (
     <div className={styles.container}>
       <ul className={styles.list}>
-        {currentPage > 1 && <li className={styles.listItem}>Next</li>}
+        {currentPageAsInt > 1 && (
+          <li className={styles.listItem}>
+            <Link href={changePageLink(currentPageAsInt - 1)}>Previous</Link>
+          </li>
+        )}
         {arrayOfNumbers.map((currentPageNumber) => (
           <li
             className={`${styles.listItem} ${
-              currentPageNumber === currentPage ? styles.selectedItem : ""
+              currentPageNumber === currentPageAsInt ? styles.selectedItem : ""
             }`}
             key={`page-${currentPageNumber}`}
           >
-            {currentPageNumber}
+            <Link href={changePageLink(currentPageNumber)}>
+              <a>{currentPageNumber}</a>
+            </Link>
           </li>
         ))}
-        {currentPage < pageCount && (
-          <li className={styles.listItem}>Previous</li>
+        {currentPageAsInt < pageCount && (
+          <li className={styles.listItem}>
+            <Link href={changePageLink(currentPageAsInt + 1)}>Next</Link>
+          </li>
         )}
       </ul>
     </div>
