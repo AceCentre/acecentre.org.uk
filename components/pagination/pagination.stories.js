2699e6d78c4f33e5c941913317b4f1eb74dd5bc3
Gavin Henderson <github@gavinhenderson.co.uk>
2021-06-03 09:16:56 +0100

    Hide next and prev when you are at the end of the list

--- components/pagination/pagination.stories.js
+++ components/pagination/pagination.stories.js
@@ -9,3 +9,9 @@ const Template = (args) => <Pagination {...args} />;
 
 export const Standard = Template.bind({});
 Standard.args = { currentPage: 2, pageCount: 10 };
+
+export const FirstPage = Template.bind({});
+FirstPage.args = { currentPage: 1, pageCount: 10 };
+
+export const LastPage = Template.bind({});
+LastPage.args = { currentPage: 10, pageCount: 10 };
