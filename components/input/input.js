1b601b0c73a59e324a0a0495cbd6dd55842bdd16
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-06-14 16:00:02 +0100

    Style input

--- components/input/input.js
+++ components/input/input.js
@@ -1,5 +1,21 @@
 import styles from "./input.module.css";
+import {
+  Input as ChakraInput,
+  InputGroup,
+  InputRightElement,
+} from "@chakra-ui/react";
 
-export const Input = () => {
-  return <input />;
+export const Input = ({ maxWidth, children, placeholder }) => {
+  return (
+    <>
+      <InputGroup maxWidth={maxWidth}>
+        <ChakraInput
+          className={styles.input}
+          backgroundColor="#F5F5F5"
+          placeholder={placeholder}
+        />
+        <InputRightElement>{children}</InputRightElement>
+      </InputGroup>
+    </>
+  );
 };
