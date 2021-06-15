f9ae109180efa6b51a943ef493a28210df658c31
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-06-15 15:52:07 +0100

    Build nav CTA

---
+++ components/nav-cta/nav-cta.js
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import styles from "./nav-cta.module.css";
+import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
+import Avatar from "@material-ui/core/Avatar";
+
+export const NavCta = ({
+  primaryLine,
+  secondaryLine,
+  iconPath,
+  backgroundColour,
+  href,
+  iconColour,
+}) => {
+  console.log(iconColour);
+  return (
+    <Link href={href}>
+      <a className={styles.link}>
+        <div className={styles.container}>
+          <style jsx>{`
+            .colouredBackground {
+              background-color: ${backgroundColour};
+            }
+
+            img {
+              max-width: 240px;
+            }
+          `}</style>
+          <div className={`${styles.topSection} colouredBackground`}>
+            <img className={styles.icon} src={iconPath} />
+            <div className={`${styles.avatarContainer} avatarParent`}>
+              <Avatar
+                style={{ backgroundColor: iconColour }}
+                className={`${styles.avatarArrow}`}
+              >
+                <ArrowForwardIcon />
+              </Avatar>
+            </div>
+          </div>
+          <div className={styles.bottomSection}>
+            <p className={styles.primaryLine}>{primaryLine}</p>
+            <p className={styles.secondaryLine}>{secondaryLine}</p>
+          </div>
+        </div>
+      </a>
+    </Link>
+  );
+};
