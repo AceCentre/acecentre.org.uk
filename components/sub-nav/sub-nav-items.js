0e907951fc74979a2c5f4d0bb437e2dc9c5bfd25
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-06-15 15:52:32 +0100

    Shift nav items out of component

---
+++ components/sub-nav/sub-nav-items.js
@@ -0,0 +1,152 @@
+export const defaultNavItems = [
+  {
+    title: "About",
+    tagLine: "learn about us",
+    href: "/about",
+    cta: {
+      primaryLine: "Contact us",
+      secondaryLine: "to learn more about us",
+      iconPath: "/phone.svg",
+      backgroundColour: "#bfdded",
+      iconColour: "#84BADC",
+      href: "/contact",
+    },
+    subItems: [
+      // TODO Make these links right
+      { title: "About us", href: "/getting-started" },
+      { title: "Our people", href: "/supporting-language" },
+      { title: "Our trustees", href: "/supporting-access" },
+      { title: "Work with us", href: "/working-in-schools" },
+      { title: "People we support", href: "/family-and-friends" },
+      { title: "Blog", href: "/family-and-friends" },
+      { title: "Projects", href: "/family-and-friends" },
+    ],
+  },
+  {
+    title: "Getting started",
+    href: "/getting-started",
+    tagLine: "how to get started",
+    cta: {
+      primaryLine: "Contact us",
+      secondaryLine: "to get started",
+      iconPath: "/phone.svg",
+      backgroundColour: "#bfdded",
+      iconColour: "#84BADC",
+      href: "/contact",
+    },
+    subItems: [
+      // TODO Make these links right
+      { title: "Getting started with AAC and AT", href: "/getting-started" },
+      { title: "What is AAC / What is AT?", href: "/supporting-language" },
+      { title: "Accessing the computer", href: "/supporting-access" },
+      {
+        title: "Which is the right communication aid for me?",
+        href: "/working-in-schools",
+      },
+      { title: "Resources to get started", href: "/family-and-friends" },
+    ],
+  },
+  {
+    title: "Resources",
+    tagLine: "explore resources",
+    href: "/resources",
+    cta: {
+      primaryLine: "Contact us",
+      secondaryLine: "for help with resources",
+      iconPath: "/phone.svg",
+      backgroundColour: "#bfdded",
+      iconColour: "#84BADC",
+      href: "/contact",
+    },
+    subItems: [
+      // TODO Make these links right
+      { title: "All resources", href: "/getting-started" },
+      { title: "Getting started resources", href: "/supporting-language" },
+      { title: "Supporting language", href: "/supporting-access" },
+      {
+        title: "Supporting Access",
+        href: "/working-in-schools",
+      },
+      { title: "Working in schools", href: "/family-and-friends" },
+      { title: "Family and friends", href: "/family-and-friends" },
+      { title: "Software", href: "/family-and-friends" },
+      { title: "Alphabet resources", href: "/family-and-friends" },
+      { title: "Symbol resources", href: "/family-and-friends" },
+    ],
+  },
+  {
+    title: "Services",
+    tagLine: "what we do",
+    href: "/service",
+    cta: {
+      primaryLine: "Contact us",
+      secondaryLine: "to see how we can help",
+      iconPath: "/phone.svg",
+      backgroundColour: "#bfdded",
+      iconColour: "#84BADC",
+      href: "/contact",
+    },
+    subItems: [
+      // TODO Make these links right
+      { title: "All services", href: "/getting-started" },
+      { title: "NHS services", href: "/supporting-language" },
+      { title: "Assessments", href: "/supporting-access" },
+      {
+        title: "Engineering",
+        href: "/working-in-schools",
+      },
+      { title: "Advice and information", href: "/family-and-friends" },
+      { title: "Research", href: "/family-and-friends" },
+      { title: "Partnerships", href: "/family-and-friends" },
+      { title: "AceCentre Learning", href: "/family-and-friends" },
+    ],
+  },
+  {
+    title: "AceCentre learning",
+    tagLine: "start learning",
+    href: "/acecentre-learning",
+    cta: {
+      primaryLine: "Contact us",
+      secondaryLine: "about our training options",
+      iconPath: "/phone.svg",
+      backgroundColour: "#bfdded",
+      iconColour: "#84BADC",
+      href: "/contact",
+    },
+    subItems: [
+      // TODO Make these links right
+      { title: "AceCentre Learning", href: "/getting-started" },
+      { title: "All courses", href: "/supporting-language" },
+      { title: "Access courses", href: "/supporting-access" },
+      {
+        title: "Communication Courses",
+        href: "/working-in-schools",
+      },
+      { title: "Education courses", href: "/family-and-friends" },
+    ],
+  },
+  {
+    title: "Get involved",
+    tagLine: "get involved",
+    href: "/get-involved",
+    cta: {
+      primaryLine: "One-off donation",
+      secondaryLine: "makes a difference",
+      iconPath: "/just-giving.svg",
+      backgroundColour: "#EFB3F3",
+      iconColour: "#AA1FBD",
+      href:
+        "https://www.justgiving.com/onecheckout/donation/direct/charity/139889",
+    },
+    subItems: [
+      // TODO Make these links right
+      { title: "Why support ace?", href: "/getting-started" },
+      { title: "Donate", href: "/supporting-language" },
+      { title: "Fundraise", href: "/supporting-access" },
+      {
+        title: "Volunteer",
+        href: "/working-in-schools",
+      },
+    ],
+  },
+];
