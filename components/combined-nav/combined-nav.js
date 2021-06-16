c0cfffa0ce1d736f9478c5e37906cb63b55a8c89
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-06-16 10:50:33 +0100

    Start mobile nav

--- components/combined-nav/combined-nav.js
+++ components/combined-nav/combined-nav.js
@@ -1,6 +1,12 @@
+import { Image } from "../image";
 import { Nav } from "../nav/nav";
 import { SubNav } from "../sub-nav/sub-nav";
 import styles from "./combined-nav.module.css";
+import SvgIcon from "@material-ui/core/SvgIcon";
+import SearchIcon from "@material-ui/icons/Search";
+import MenuIcon from "@material-ui/icons/Menu";
+import { Button as ChakraButton } from "@chakra-ui/react";
+import Link from "next/link";
 
 export const CombinedNav = ({ cartCount, defaultNavItems }) => {
   return (
@@ -9,7 +15,35 @@ export const CombinedNav = ({ cartCount, defaultNavItems }) => {
         <Nav numberOfItemsInCart={cartCount} />
         <SubNav navItems={defaultNavItems} />
       </div>
-      <div className={styles.mobileContainer}>Mobile</div>
+      <div className={styles.mobileContainer}>
+        <Link name="home" href="/">
+          <a>
+            <Image
+              height={152}
+              width={290}
+              maxHeight={50}
+              src={"/nav-logo.png"}
+              alt="The AceCentre logo"
+            ></Image>
+          </a>
+        </Link>
+        <div className={styles.buttonContainer}>
+          <ChakraButton
+            className={`${styles.navButton} ${styles.menuButton}`}
+            variant="unstyled"
+          >
+            Menu
+            <SvgIcon className={styles.icon}>
+              <MenuIcon />
+            </SvgIcon>
+          </ChakraButton>
+          <ChakraButton className={styles.navButton} variant="unstyled">
+            <SvgIcon className={styles.icon}>
+              <SearchIcon />
+            </SvgIcon>
+          </ChakraButton>
+        </div>
+      </div>
     </>
   );
 };
