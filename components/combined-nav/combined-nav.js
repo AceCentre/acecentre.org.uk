78bc669826e51ee295f5fa5b68b073ac307aa845
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-06-16 09:57:39 +0100

    Show and hide nav

--- components/combined-nav/combined-nav.js
+++ components/combined-nav/combined-nav.js
@@ -1,11 +1,15 @@
 import { Nav } from "../nav/nav";
 import { SubNav } from "../sub-nav/sub-nav";
+import styles from "./combined-nav.module.css";
 
 export const CombinedNav = ({ cartCount, defaultNavItems }) => {
   return (
     <>
-      <Nav numberOfItemsInCart={cartCount} />
-      <SubNav navItems={defaultNavItems} />
+      <div className={styles.desktopContainer}>
+        <Nav numberOfItemsInCart={cartCount} />
+        <SubNav navItems={defaultNavItems} />
+      </div>
+      <div className={styles.mobileContainer}>Mobile</div>
     </>
   );
 };
