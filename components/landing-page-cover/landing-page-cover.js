dcef0901e0ac1c661b21a96bd5ffbe8d377d8e00
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-06-18 10:11:15 +0100

    WIP

--- components/landing-page-cover/landing-page-cover.js
+++ components/landing-page-cover/landing-page-cover.js
@@ -4,7 +4,14 @@ import styles from "./landing-page-cover.module.css";
 export const LandingPageCover = () => {
   return (
     <div>
-      <Image src="/landing-page-cover.png" width="100%" />
+      <div className={styles.imageContainer}>
+        <img
+          className={styles.image}
+          src="/landing-page-cover.png"
+          // width={1441}
+          // height={942}
+        />
+      </div>
     </div>
   );
 };
