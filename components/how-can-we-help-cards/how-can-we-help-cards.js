915c3cef89a42b26bcd300b4c6b57e2a9edc4116
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-06-18 14:26:03 +0100

    Build how can we help cards

--- components/how-can-we-help-cards/how-can-we-help-cards.js
+++ components/how-can-we-help-cards/how-can-we-help-cards.js
@@ -1,62 +1,83 @@
+import Link from "next/link";
 import styles from "./how-can-we-help-cards.module.css";
 
 export const HowCanWeHelpCards = () => {
   return (
     <div className={styles.container}>
-      <h1>How can we help</h1>
+      <p className={styles.handwriting}>We&apos;re here for you</p>
+      <h1>How We Help</h1>
       <div className={styles.allCards}>
         <div className={styles.gettingStarted}>
-          <img
-            alt="placeholder"
-            width="100%"
-            height="250px"
-            src="/placeholder.jpeg"
+          <HowWeHelpCard
+            src="/getting-started.svg"
+            background="#F1D1D0"
+            headline="Getting started with AAC and AT"
+            secondaryLine="New to AAC and AT? Learn more with our guide to getting started"
+            href="/getting-started"
           />
-          <h2>Getting started</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
         </div>
         <div className={styles.resources}>
-          <img
-            alt="placeholder"
-            width="100%"
-            height="250px"
-            src="/placeholder.jpeg"
+          <HowWeHelpCard
+            src="/resources.svg"
+            background="#F6EEC5"
+            headline="Resources"
+            secondaryLine="Resources to help you get started with AAC, free symbol & alphabet charts"
+            href="/resources"
           />
-          <h2>Resources</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
         </div>
 
         <div className={styles.aceCentreLearning}>
-          <img
-            alt="placeholder"
-            width="100%"
-            height="250px"
-            src="/placeholder.jpeg"
+          <HowWeHelpCard
+            src="/acecentre-learning.svg"
+            background="#B0E8DE"
+            headline="AceCentre Learning"
+            secondaryLine="Find the right training for your specific needs"
+            href="/acecentre-learning"
           />
-          <h2>Ace Centre Learning</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
         </div>
         <div className={styles.assessment}>
-          <img
-            alt="placeholder"
-            width="100%"
-            height="250px"
-            src="/placeholder.jpeg"
+          <HowWeHelpCard
+            src="/assessments.svg"
+            background="#F4D4A4"
+            headline="Assessments"
+            secondaryLine="We offer independent interdisciplinary assessments"
+            href="/assessments"
           />
-          <h2>Assessment</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
         </div>
         <div className={styles.helpline}>
-          <img
-            alt="placeholder"
-            width="100%"
-            height="250px"
-            src="/placeholder.jpeg"
+          <HowWeHelpCard
+            src="/advice-line.svg"
+            background="#BFDDED"
+            headline="Free Advice Line"
+            secondaryLine="Call us on 0800 048 7642
+            Office hours, Monday - Friday"
+            href="tel:08000487642"
           />
-          <h2>Helpline</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
         </div>
       </div>
     </div>
+  );
+};
+
+const HowWeHelpCard = ({ src, background, headline, secondaryLine, href }) => {
+  return (
+    <>
+      <style jsx>{`
+        .imageBackground {
+          background-color: ${background};
+        }
+      `}</style>
+      <Link href={href}>
+        <a className={styles.cardContainer}>
+          <div className="imageBackground">
+            <img src={src} />
+          </div>
+          <div className={styles.bottomSection}>
+            <p className={styles.cardHeadline}>{headline}</p>
+            <p>{secondaryLine}</p>
+          </div>
+        </a>
+      </Link>
+    </>
   );
 };
