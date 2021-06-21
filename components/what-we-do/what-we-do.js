059e06ba0702d9927bfc55e5c19435053d18fed0
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-06-21 12:01:27 +0100

    Finish what we do

--- components/what-we-do/what-we-do.js
+++ components/what-we-do/what-we-do.js
@@ -1,3 +1,5 @@
+import { Avatar } from "@material-ui/core";
+import { ArrowForward } from "@material-ui/icons";
 import Link from "next/link";
 import styles from "./what-we-do.module.css";
 
@@ -6,29 +8,80 @@ export const WhatWeDo = () => {
     <div className={styles.container}>
       <h1>What we do</h1>
       <ul className={styles.list}>
-        <li>
-          <Link href="/services/nhs">NHS Services</Link>
-        </li>
-        <li>
-          <Link href="/services/engineering">Engineering</Link>
-        </li>
-        <li>
-          <Link href="/services/research">Research</Link>
-        </li>
-        <li>
-          <Link href="/acecentre-learning">Ace Centre Learning</Link>
-        </li>
-        <li>
-          <Link href="/services/assessment">Assessment</Link>
-        </li>
-        <li>
-          <Link href="/services/advice-and-information">
-            Advice and Information
-          </Link>
-        </li>
-        <li>
-          <Link href="/services/partnerships">Partnerships</Link>
-        </li>
+        <div>
+          <li>
+            <Link href="/services/nhs">
+              <a className={styles.link}>
+                <Avatar className={styles.avatar}>
+                  <ArrowForward />
+                </Avatar>
+                NHS Services
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/services/engineering">
+              <a className={styles.link}>
+                <Avatar className={styles.avatar}>
+                  <ArrowForward />
+                </Avatar>
+                Engineering
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/services/research">
+              <a className={styles.link}>
+                <Avatar className={styles.avatar}>
+                  <ArrowForward />
+                </Avatar>
+                Research
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/acecentre-learning">
+              <a className={styles.link}>
+                <Avatar className={styles.avatar}>
+                  <ArrowForward />
+                </Avatar>
+                Ace Centre Learning
+              </a>
+            </Link>
+          </li>
+        </div>
+        <div>
+          <li>
+            <Link href="/services/assessment">
+              <a className={styles.link}>
+                <Avatar className={styles.avatar}>
+                  <ArrowForward />
+                </Avatar>
+                Assessment
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/services/advice-and-information">
+              <a className={styles.link}>
+                <Avatar className={styles.avatar}>
+                  <ArrowForward />
+                </Avatar>
+                Advice and Information
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/services/partnerships">
+              <a className={styles.link}>
+                <Avatar className={styles.avatar}>
+                  <ArrowForward />
+                </Avatar>
+                Partnerships
+              </a>
+            </Link>
+          </li>
+        </div>
       </ul>
     </div>
   );
