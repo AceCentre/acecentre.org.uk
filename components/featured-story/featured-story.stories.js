055cb5a6bee3eb5db8c47fa53d6f03fb5713dfa1
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-06-21 15:48:00 +0100

    Add image props to storybook

--- components/featured-story/featured-story.stories.js
+++ components/featured-story/featured-story.stories.js
@@ -11,6 +11,12 @@ export const Standard = Template.bind({});
 Standard.args = {
   youtubeVideo: "https://www.youtube.com/watch?v=3fxrVktVzn0",
   title: "How we supported Paul & Julie",
+  featuredImage: {
+    src:
+      "https://internal.acecentre.org.uk/wp-content/uploads/2021/03/Screenshot-2021-06-21-at-15.08.11.png",
+    width: 6016,
+    height: 3150,
+  },
   summary:
     "<p>An insight into Paul and Julie&#8217;s lives; how they manage the inescapable changes and challenges that are brought on by Motor Neurone Disease</p>\n",
 };
