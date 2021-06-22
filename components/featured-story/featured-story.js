e199eb7e1bcacf5942269397e1c04ae04135bd36
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-06-22 15:54:13 +0100

    Add youtube video to featured story

--- components/featured-story/featured-story.js
+++ components/featured-story/featured-story.js
@@ -5,43 +5,58 @@ import { Button } from "../button/button";
 
 import Link from "next/link";
 import { Avatar } from "@material-ui/core";
+import { VideoPopover } from "../video-popover/video-popover";
+import { useState } from "react";
 
 export const FeaturedStory = ({
   summary,
   title,
   slug,
+  youtubeUrl = "https://www.youtube.com/watch?v=TqhZQsR5vmQ",
   featuredImage = {
     src:
       "https://internal.acecentre.org.uk/wp-content/uploads/2021/03/Screenshot-2021-06-21-at-15.08.11.png",
   },
 }) => {
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+
   return (
-    <div>
-      <div className={styles.imageContainer}>
-        <div className={styles.overlay}>
-          <div className={styles.playButton}>
-            <Avatar className={styles.playAvatar}>
-              <PlayArrowIcon className={styles.playIcon} />
-            </Avatar>
+    <>
+      <div>
+        <div className={styles.imageContainer}>
+          <div className={styles.overlay}>
+            <button
+              className={styles.playButton}
+              onClick={() => setIsPopoverOpen(true)}
+            >
+              <Avatar className={styles.playAvatar}>
+                <PlayArrowIcon className={styles.playIcon} />
+              </Avatar>
+            </button>
+            <OverlayCard
+              className={styles.desktopCard}
+              slug={slug}
+              title={title}
+              summary={summary}
+            />
           </div>
-          <OverlayCard
-            className={styles.desktopCard}
-            slug={slug}
-            title={title}
-            summary={summary}
-          />
+          <div className={styles.gradientCover}></div>
+          <div className={styles.backgroundOverlay}></div>
+          <img className={styles.image} src={featuredImage.src} />
         </div>
-        <div className={styles.gradientCover}></div>
-        <div className={styles.backgroundOverlay}></div>
-        <img className={styles.image} src={featuredImage.src} />
+        <OverlayCard
+          className={styles.mobileCard}
+          slug={slug}
+          title={title}
+          summary={summary}
+        />
       </div>
-      <OverlayCard
-        className={styles.mobileCard}
-        slug={slug}
-        title={title}
-        summary={summary}
+      <VideoPopover
+        youtubeUrl={youtubeUrl}
+        isPopoverOpen={isPopoverOpen}
+        onClose={() => setIsPopoverOpen(false)}
       />
-    </div>
+    </>
   );
 };
 
