ae7a9ccee7547c19f2c18367263899801584d7c0
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-06-22 15:14:24 +0100

    Finish display of landing page

--- components/get-involved/get-involved.js
+++ components/get-involved/get-involved.js
@@ -1,24 +1,52 @@
 import Link from "next/link";
 import styles from "./get-involved.module.css";
+import Image from "next/image";
+import { Button } from "../button/button";
 
 export const GetInvolved = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.innerContainer}>
-        <h1>Get involved</h1>
-        <p>
-          Your support is vital to our ongoing work to ensure that children and
-          adults with the most challenging communication difficulties have free
-          access to the advice, information and support they need to fulfil
-          their potential
-        </p>
-        <div className={styles.buttonContainer}>
-          <div className={styles.getInvolved}>
-            <Link href="/get-involved">Ways to get involved</Link>
+      <Image
+        className={styles.backgroundImage}
+        src="/collage.png"
+        objectFit="cover"
+        layout="fill"
+      />
+      <div className={styles.blueCover}></div>
+      <div className={styles.cardContainer}>
+        <div className={styles.card}>
+          <h2 className={styles.title}>How to get involved</h2>
+          <p className={styles.handwriting}>Make a difference</p>
+          <p className={styles.paragraph}>
+            Your support is vital to our ongoing work to ensure that children
+            and adults with the most challenging communication difficulties.
+          </p>
+          <div className={styles.buttonContainer}>
+            <Button href="/get-involved">Ways to get involved</Button>
+            <Link href="/donation">
+              <a className={styles.donationLink}>Make a donation</a>
+            </Link>
           </div>
-          <Link href="/donate">Make a donation</Link>
         </div>
       </div>
     </div>
   );
 };
+
+// return (
+//   <div className={styles.container}>
+//     <div className={styles.innerContainer}>
+//       <h1>Get involved</h1>
+//       <p>
+//         Your support is vital to our ongoing work to ensure that children and
+//         adults with the most challenging communication difficulties have free
+//         access to the advice, information and support they need to fulfil
+//         their potential
+//       </p>
+//       <div className={styles.buttonContainer}>
+//         <div className={styles.getInvolved}>
+//           <Link href="/get-involved">Ways to get involved</Link>
+//         </div>
+//         <Link href="/donate">Make a donation</Link>
+//       </div>
+//     </div>
