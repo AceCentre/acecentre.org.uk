d8bab2f8565892e2d1a0be43f2c02e1ed821e0cc
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-06-22 09:18:14 +0100

    Finish featured story display

--- components/featured-story/featured-story.js
+++ components/featured-story/featured-story.js
@@ -1,35 +1,70 @@
 import styles from "./featured-story.module.css";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import { Button } from "../button/button";
 
-import LiteYouTubeEmbed from "react-lite-youtube-embed";
 import Link from "next/link";
+import { Avatar } from "@material-ui/core";
 
-export const FeaturedStory = ({ youtubeVideo, summary, title, slug }) => {
-  const videoId = urlToVideoId(youtubeVideo);
-
+export const FeaturedStory = ({
+  summary,
+  title,
+  slug,
+  featuredImage = {
+    src:
+      "https://internal.acecentre.org.uk/wp-content/uploads/2021/03/Screenshot-2021-06-21-at-15.08.11.png",
+  },
+}) => {
   return (
-    <div className={styles.container}>
-      <div className={styles.videoContainer}>
-        <LiteYouTubeEmbed
-          id={videoId}
-          title={`A youtube video about: ${title}`}
-          noCookie={true}
-        />
-      </div>
-      <div>
-        <h1>{title}</h1>
-        {/* We are dangerously setting inner HTML here because the summary comes from wordpress */}
-        <div dangerouslySetInnerHTML={{ __html: summary }}></div>
-        <div className={styles.buttonContainer}>
-          <div className={styles.findOutMore}>
-            <Link href={`/stories/${slug}`}>Full story</Link>
+    <div>
+      <div className={styles.imageContainer}>
+        <div className={styles.overlay}>
+          <div className={styles.playButton}>
+            <Avatar className={styles.playAvatar}>
+              <PlayArrowIcon className={styles.playIcon} />
+            </Avatar>
           </div>
-          <Link href="/stories">Read more stories</Link>
+          <OverlayCard
+            className={styles.desktopCard}
+            slug={slug}
+            title={title}
+            summary={summary}
+          />
         </div>
+        <div className={styles.gradientCover}></div>
+        <div className={styles.backgroundOverlay}></div>
+        <img className={styles.image} src={featuredImage.src} />
       </div>
+      <OverlayCard
+        className={styles.mobileCard}
+        slug={slug}
+        title={title}
+        summary={summary}
+      />
     </div>
   );
 };
 
-const urlToVideoId = (url) => {
-  return new URL(url).searchParams.get("v");
+const OverlayCard = ({ title, summary, slug, className }) => {
+  return (
+    <div className={className}>
+      <p className={styles.greenSpan}>Stories</p>
+      <h2 className={styles.cardTitle}>{title}</h2>
+      <div
+        className={styles.cardDescription}
+        dangerouslySetInnerHTML={{ __html: summary }}
+      ></div>
+      <div className={styles.cardLinkContainer}>
+        <Button className={styles.readFullStory} href={`/stories/${slug}`}>
+          Read full story
+        </Button>
+        <Link href="/stories">
+          <a className={styles.allStories}>
+            All Stories
+            <ChevronRightIcon />
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
 };
