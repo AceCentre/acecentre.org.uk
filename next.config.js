49f427ece13ff914989a7138d433a1b2782710ce
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-06-23 11:47:41 +0100

    Back to netlify images

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   target: "serverless",
   images: {
-    images: {
-      loader: "cloudinary",
-      path: "https://res.cloudinary.com/ace-cloud/image/upload/",
-    },
     domains: ["acecentre.org.uk", "internal.acecentre.org.uk"],
   },
 };
