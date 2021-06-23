eb9632dc1cf3890cafab989b1f22d2c3ac41793b
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-06-23 11:30:56 +0100

    Use cloudinary all the time

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   target: "serverless",
   images: {
+    images: {
+      loader: "cloudinary",
+      path: "https://res.cloudinary.com/ace-cloud/image/upload/",
+    },
     domains: ["acecentre.org.uk", "internal.acecentre.org.uk"],
   },
 };
