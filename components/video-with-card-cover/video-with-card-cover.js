5cfed625c518f42c40e5172f6d580ff10ada8db8
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-06-23 14:44:01 +0100

    Finish cover image

--- components/video-with-card-cover/video-with-card-cover.js
+++ components/video-with-card-cover/video-with-card-cover.js
@@ -1,11 +1,16 @@
 import styles from "./video-with-card-cover.module.css";
+import Image from "next/image";
 
-export const VideoWithCardCover = ({ children }) => {
+export const VideoWithCardCover = ({ children, src = "/about-cover.jpeg" }) => {
   return (
     <div className={styles.container}>
-      <div className={styles.cardContainer}>
-        <div className={styles.cardInnerContainer}>
-          <div className={styles.card}>{children}</div>
+      <div className={styles.coverImageContainer}>
+        <Image src={src} layout="fill" objectFit="cover" objectPosition="top" />
+        <div className={styles.backgroundGradient} />
+        <div className={styles.fullContainer}>
+          <div className={styles.innerContainer}>
+            <div className={styles.card}>{children}</div>
+          </div>
         </div>
       </div>
     </div>
