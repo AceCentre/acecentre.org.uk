fd84e0c00d318f2a5283739175593658e3c50828
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-06-28 09:17:39 +0100

    Working at Ace

--- components/working-at-ace/working-at-ace.js
+++ components/working-at-ace/working-at-ace.js
@@ -1,25 +1,40 @@
+import { ImageWithLoader as Image } from "../image";
 import styles from "./working-at-ace.module.css";
+
+import FormatQuoteIcon from "@material-ui/icons/FormatQuote";
+import { Avatar } from "@material-ui/core";
 
 export const WorkingAtAce = () => {
   return (
     <div className={styles.container}>
-      <h1>Working at AceCentre</h1>
+      <h2 className={styles.title}>Working at Ace Centre</h2>
       <div className={styles.innerContainer}>
-        <div>
-          <img
-            alt="placeholder"
-            width="100%"
-            height="100%"
-            src="/placeholder.jpeg"
+        <div className={styles.imageContainer}>
+          <Image
+            src="/careers.jpeg"
+            layout="fill"
+            objectFit="cover"
+            alt="Two people outside looking happy"
           />
+          <div className={styles.blueBackground}></div>
         </div>
-        <div>
-          <p>
-            &quot;Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Pellentesque gravida rutrum mattis. Aenean tincidunt neque id turpis
-            viverra pellentesque.&quot;
-          </p>
-          <p>- Will Wade AAC Consultant</p>
+        <div className={styles.quote}>
+          <Avatar className={styles.avatar}>
+            <FormatQuoteIcon className={styles.icon} />
+          </Avatar>
+          <div className={styles.quoteText}>
+            <p className={styles.quoteContent}>
+              &quot;Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Pellentesque gravida rutrum mattis. Aenean tincidunt neque id
+              turpis viverra pellentesque.&quot;
+            </p>
+            <div>
+              <p>
+                <strong>Anna Reeves DL</strong>
+              </p>
+              <p>CEO</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
