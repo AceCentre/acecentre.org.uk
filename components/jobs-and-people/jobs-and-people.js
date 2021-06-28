5dc9ecb590be0b489355ac8ab1c29a0300b9ed29
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-06-28 11:13:10 +0100

    Finish meet the team

--- components/jobs-and-people/jobs-and-people.js
+++ components/jobs-and-people/jobs-and-people.js
@@ -1,16 +1,20 @@
-import Link from "next/link";
 import styles from "./jobs-and-people.module.css";
+
+import { ImageWithLoader as Image } from "../image";
+import { Button } from "../button/button";
 
 export const JobsAndPeople = () => {
   return (
     <div className={styles.container}>
       <div className={styles.jobImage}>
-        <img
-          alt="placeholder"
-          width="100%"
-          height="100%"
-          src="/placeholder.jpeg"
+        <Image
+          src="/job-opportunities.jpeg"
+          layout="fill"
+          objectFit="cover"
+          objectPosition="top"
+          alt="Staff member on taking a phone call"
         />
+        <div className={styles.blueBackground}></div>
       </div>
       <div className={styles.jobDescription}>
         <h2>Job opportunities</h2>
@@ -19,24 +23,30 @@ export const JobsAndPeople = () => {
           gravida rutrum mattis. Aenean tincidunt neque id turpis viverra
           pellentesque.
         </p>
-        <Link href="/careers">View Jobs</Link>
+        <div className={styles.buttonContainer}>
+          <Button href="https://indeed.co.uk">View jobs on indeed.co.uk</Button>
+        </div>
       </div>
       <div className={styles.peopleImage}>
-        <img
-          alt="placeholder"
-          width="100%"
-          height="100%"
-          src="/placeholder.jpeg"
+        <Image
+          src="/meet-our-people.jpeg"
+          layout="fill"
+          objectFit="cover"
+          objectPosition="top"
+          alt="Two staff members standing having a conversation"
         />
+        <div className={styles.blueBackground}></div>
       </div>
       <div className={styles.peopleDescription}>
-        <h2>Our team</h2>
+        <h2>Meet the team</h2>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
           gravida rutrum mattis. Aenean tincidunt neque id turpis viverra
           pellentesque.
         </p>
-        <Link href="/about/staff">Meet the team</Link>
+        <div className={styles.buttonContainer}>
+          <Button href="/about/staff">Meet the team</Button>
+        </div>
       </div>
     </div>
   );
