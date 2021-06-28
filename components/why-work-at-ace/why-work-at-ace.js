83d4726c98979d767936c18f2c744c7d6b17883a
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-06-28 10:43:07 +0100

    Update working at acecentre

--- components/why-work-at-ace/why-work-at-ace.js
+++ components/why-work-at-ace/why-work-at-ace.js
@@ -3,31 +3,37 @@ import styles from "./why-work-at-ace.module.css";
 export const WhyWorkAtAce = () => {
   return (
     <div className={styles.container}>
-      <h2>Why work at AceCentre</h2>
       <div className={styles.innerContainer}>
-        <div>
-          <h3>Title goes here</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Pellentesque gravida rutrum mattis. Aenean tincidunt neque id turpis
-            viverra pellentesque.
-          </p>
-        </div>
-        <div>
-          <h3>Title goes here</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Pellentesque gravida rutrum mattis. Aenean tincidunt neque id turpis
-            viverra pellentesque.
-          </p>
-        </div>
-        <div>
-          <h3>Title goes here</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Pellentesque gravida rutrum mattis. Aenean tincidunt neque id turpis
-            viverra pellentesque.
-          </p>
+        <h2 className={styles.title}>
+          Working at <span className={styles.titleName}>Ace Centre</span>
+        </h2>
+        <div className={styles.proContainer}>
+          <div className={styles.proBlock}>
+            <h3>Benefits</h3>
+            <p>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt.
+            </p>
+          </div>
+          <div className={styles.proBlock}>
+            <h3>Experience</h3>
+            <p>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt.
+            </p>
+          </div>
+          <div className={styles.proBlock}>
+            <h3>Growth</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam.
+            </p>
+          </div>
         </div>
       </div>
     </div>
