cb82ca751b9bd315b496c3921e5f5cad0b09cbe0
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-06-29 09:51:27 +0100

    Finish blog landing page

--- components/blog-category-grid/blog-category-grid.js
+++ components/blog-category-grid/blog-category-grid.js
@@ -1,13 +1,39 @@
 import styles from "./blog-category-grid.module.css";
+import { ImageWithLoader as Image } from "../image";
+import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
+import Link from "next/link";
+import { Avatar } from "@material-ui/core";
 
 export const BlogCategoryGrid = ({ blogCategories }) => {
   return (
-    <div>
-      <h3>Browse articles by category</h3>
-      <ul>
+    <div className={styles.container}>
+      <h3 className={styles.heading}>Browse articles by category</h3>
+      <ul className={styles.list}>
         {blogCategories.map((category) => {
           return (
-            <li key={`browse-articles-${category.slug}`}>{category.title}</li>
+            <li
+              className={styles.listItem}
+              key={`browse-articles-${category.slug}`}
+            >
+              <Link href={`/blog/category/${category.slug}`}>
+                <a className={styles.link}>
+                  <Image
+                    src={category.featuredImage.src}
+                    layout="fill"
+                    objectFit="cover"
+                  />
+                  <div className={styles.blueBannerContainer}>
+                    <div className={styles.blueBanner}>
+                      <p>{category.title}</p>
+                      <Avatar className={styles.avatar}>
+                        <ArrowForwardIcon className={styles.icon} />
+                      </Avatar>
+                    </div>
+                  </div>
+                  {/* {category.title} */}
+                </a>
+              </Link>
+            </li>
           );
         })}
       </ul>
