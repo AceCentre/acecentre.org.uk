c658732d3dc4f9ed9cd0c9ab488e75831f5fc62c
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-06-30 16:28:00 +0100

    Copy featured story to story highlight

--- components/story-highlight/story-highlight.js
+++ components/story-highlight/story-highlight.js
@@ -1,31 +1,89 @@
 import Link from "next/link";
-
+import { useState } from "react";
 import styles from "./story-highlight.module.css";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import { Button } from "../button/button";
+import { Avatar } from "@material-ui/core";
+import { VideoPopover } from "../video-popover/video-popover";
+import { ImageWithLoader as Image } from "../image";
 
-export const StoryHighlight = () => {
+export const StoryHighlight = ({
+  summary,
+  title,
+  slug,
+  youtubeVideo,
+  featuredImage,
+}) => {
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+
   return (
-    <div className={styles.container}>
-      <h2>Words from Lisa</h2>
-      <div className={styles.innerContainer}>
-        <div>
-          <img
-            alt="placeholder"
-            width="100%"
-            height="100%"
-            src="/placeholder.jpeg"
-          />
+    <>
+      <div>
+        <div className={styles.imageContainer}>
+          <div className={styles.overlay}>
+            <button
+              className={styles.playButton}
+              onClick={() => setIsPopoverOpen(true)}
+              aria-label={`Play video about: ${title}`}
+            >
+              <Avatar className={styles.playAvatar}>
+                <PlayArrowIcon className={styles.playIcon} />
+              </Avatar>
+            </button>
+            <OverlayCard
+              className={styles.desktopCard}
+              slug={slug}
+              title={title}
+              summary={summary}
+            />
+          </div>
+          <div className={styles.gradientCover}></div>
+          <div className={styles.backgroundOverlay}></div>
+          <div className={styles.nextImageContainer}>
+            <Image
+              alt={`An image of: ${title}`}
+              src={featuredImage.src}
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
         </div>
-        <div>
-          <p>
-            &quot;Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Pellentesque gravida rutrum mattis. Aenean tincidunt neque id turpis
-            viverra pellentesque.&quot;
-          </p>
-          <p>- Lisa</p>
-          <Link href="/stories/lisa">
-            <a>Read her story</a>
-          </Link>
-        </div>
+        <OverlayCard
+          className={styles.mobileCard}
+          slug={slug}
+          title={title}
+          summary={summary}
+        />
+      </div>
+      <VideoPopover
+        youtubeUrl={youtubeVideo}
+        isPopoverOpen={isPopoverOpen}
+        onClose={() => setIsPopoverOpen(false)}
+      />
+    </>
+  );
+};
+
+const OverlayCard = ({ title, summary, slug, className }) => {
+  return (
+    <div className={className}>
+      <p className={styles.greenSpan}>Stories</p>
+      <h2 className={styles.cardTitle}>{title}</h2>
+      <div
+        className={styles.cardDescription}
+        dangerouslySetInnerHTML={{ __html: summary }}
+      ></div>
+      <div className={styles.cardLinkContainer}>
+        <Button className={styles.readFullStory} href={`/stories/${slug}`}>
+          Read full story
+        </Button>
+        <Link href="/stories">
+          <a className={styles.allStories}>
+            All Stories
+            <ChevronRightIcon />
+          </a>
+        </Link>
       </div>
     </div>
   );
