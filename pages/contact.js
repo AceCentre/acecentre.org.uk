438230dfacc43e91fa05e486abe56ebce4e5f02d
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-07-05 10:06:14 +0100

    Scaffold contact cards

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,7 @@
 import { CombinedNav } from "../components/combined-nav/combined-nav";
+import { ContactCards } from "../components/contact-cards/contact-cards";
 import { Footer } from "../components/footer/footer";
+import { PageTitle } from "../components/page-title/page-title";
 import { defaultNavItems } from "../components/sub-nav/sub-nav";
 import { useCartCount } from "../lib/cart/use-cart-count";
 import { useGlobalProps } from "../lib/global-props/hook";
@@ -14,7 +16,13 @@ export default function Contact() {
       <header>
         <CombinedNav cartCount={cartCount} defaultNavItems={defaultNavItems} />
       </header>
-      <main></main>
+      <main>
+        <PageTitle
+          heading="Contact us"
+          description="If you think Ace Centre can help you or someone you know please get in touch"
+        />
+        <ContactCards />
+      </main>
       <Footer currentYear={currentYear} />
     </>
   );
