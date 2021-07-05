10c6cd6e5e696c004f5ed5bbe3a895cf6deba46d
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-07-05 16:29:01 +0100

    Form submission

--- components/form/form.js
+++ components/form/form.js
@@ -1,16 +1,29 @@
 import styles from "./form.module.css";
-import { FormiumForm } from "@formium/react";
+import { FormiumForm, defaultComponents } from "@formium/react";
+import { useState } from "react";
+
+const myComponents = {
+  ...defaultComponents,
+};
 
 export const Form = ({ form, formium, slug }) => {
+  const [success, setSuccess] = useState(false);
+
   return (
     <div className={styles.container}>
-      <FormiumForm
-        data={form}
-        onSubmit={async (values) => {
-          // Send form values to Formium
-          await formium.submitForm(slug, values);
-        }}
-      />
+      {success ? (
+        <p>Success!</p>
+      ) : (
+        <FormiumForm
+          data={form}
+          myComponents={myComponents}
+          onSubmit={async (values) => {
+            // Send form values to Formium
+            await formium.submitForm(slug, values);
+            setSuccess(true);
+          }}
+        />
+      )}
     </div>
   );
 };
