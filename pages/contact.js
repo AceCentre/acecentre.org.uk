c2e7b2282b2b1e5047ccdc5e415fb3507f25969b
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-07-05 11:21:31 +0100

    Scaffold find our offices

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,6 @@
 import { CombinedNav } from "../components/combined-nav/combined-nav";
 import { ContactCards } from "../components/contact-cards/contact-cards";
+import { FindOurOffices } from "../components/find-our-offices/find-our-offices";
 import { Footer } from "../components/footer/footer";
 import { PageTitle } from "../components/page-title/page-title";
 import { defaultNavItems } from "../components/sub-nav/sub-nav";
@@ -22,6 +23,7 @@ export default function Contact() {
           description="If you think Ace Centre can help you or someone you know please get in touch"
         />
         <ContactCards />
+        <FindOurOffices />
       </main>
       <Footer currentYear={currentYear} />
     </>
