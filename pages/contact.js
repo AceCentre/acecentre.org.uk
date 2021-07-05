a949506d048d34407258770b909e9150866b1a43
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-07-05 16:54:00 +0100

    Add form

--- pages/contact.js
+++ pages/contact.js
@@ -2,13 +2,15 @@ import { CombinedNav } from "../components/combined-nav/combined-nav";
 import { ContactCards } from "../components/contact-cards/contact-cards";
 import { FindOurOffices } from "../components/find-our-offices/find-our-offices";
 import { Footer } from "../components/footer/footer";
+import { Form } from "../components/form/form";
 import { PageTitle } from "../components/page-title/page-title";
 import { defaultNavItems } from "../components/sub-nav/sub-nav";
 import { useCartCount } from "../lib/cart/use-cart-count";
 import { useGlobalProps } from "../lib/global-props/hook";
 import { withGlobalProps } from "../lib/global-props/inject";
+import { formium } from "../lib/formium";
 
-export default function Contact() {
+export default function Contact({ slug, form }) {
   const cartCount = useCartCount();
   const { currentYear } = useGlobalProps();
 
@@ -23,6 +25,7 @@ export default function Contact() {
           description="If you think Ace Centre can help you or someone you know please get in touch"
         />
         <ContactCards />
+        <Form form={form} slug={slug} formium={formium} />
         <FindOurOffices />
       </main>
       <Footer currentYear={currentYear} />
@@ -30,4 +33,9 @@ export default function Contact() {
   );
 }
 
-export const getStaticProps = withGlobalProps();
+export const getStaticProps = withGlobalProps(async () => {
+  const slug = "contact";
+  const form = await formium.getFormBySlug(slug);
+
+  return { props: { slug, form } };
+});
