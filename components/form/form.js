55d828abbc187546bc910030dedbc33e3fe7cb11
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-07-05 16:51:38 +0100

    Form over rides

--- components/form/form.js
+++ components/form/form.js
@@ -1,9 +1,80 @@
 import styles from "./form.module.css";
 import { FormiumForm, defaultComponents } from "@formium/react";
-import { useState } from "react";
+import { memo, useState } from "react";
+
+const TextInput = memo(function TextInput({ type, ...props }) {
+  if (type == "text") {
+    return <input type={type} {...props} />;
+  }
+
+  if (type == "email") {
+    return <input type={type} {...props} />;
+  }
+
+  if (type == "url") {
+    return <input type={type} {...props} />;
+  }
+
+  console.warn(
+    `<TextInput /> doesn't have an implementation for specified type of ${type}.`
+  );
+  return <input type={type} {...props} />;
+});
+
+const FormControl = memo(function FormControl({
+  children,
+  description,
+  error,
+  label,
+  labelFor,
+}) {
+  return (
+    <div>
+      {label && <label htmlFor={labelFor}>{label}</label>}
+      {description && <div>{description}</div>}
+      {children}
+      {error && <div>{error}</div>}
+    </div>
+  );
+});
+
+const Radio = ({ label, ...props }) => {
+  return (
+    <label>
+      <input type="radio" {...props} />
+      {label}
+    </label>
+  );
+};
+
+const Checkbox = ({ label, value, name, id, checked, onChange, ...props }) => {
+  return (
+    <label>
+      <input
+        type="checkbox"
+        id={id}
+        name={name}
+        checked={checked}
+        value={value}
+        onChange={onChange}
+        {...props}
+      />
+      {label}
+    </label>
+  );
+};
+
+const Textarea = ({ ...props }) => {
+  return <textarea {...props}></textarea>;
+};
 
 const myComponents = {
   ...defaultComponents,
+  TextInput,
+  FormControl,
+  Radio,
+  Checkbox,
+  Textarea,
 };
 
 export const Form = ({ form, formium, slug }) => {
@@ -16,7 +87,7 @@ export const Form = ({ form, formium, slug }) => {
       ) : (
         <FormiumForm
           data={form}
-          myComponents={myComponents}
+          components={myComponents}
           onSubmit={async (values) => {
             // Send form values to Formium
             await formium.submitForm(slug, values);
