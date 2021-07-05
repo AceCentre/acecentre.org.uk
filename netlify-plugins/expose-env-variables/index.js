22580699e6f01087bcec797eec49c1a98e85cd22
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-07-05 16:16:25 +0100

    Pass extra vars

--- netlify-plugins/expose-env-variables/index.js
+++ netlify-plugins/expose-env-variables/index.js
@@ -12,5 +12,13 @@ module.exports = {
     fs.appendFileSync(pathToEnv, `BUILD_ID=${process.env.BUILD_ID}\n`);
     fs.appendFileSync(pathToEnv, `DEPLOY_KEY=${process.env.DEPLOY_KEY}\n`);
     fs.appendFileSync(pathToEnv, `IMAGE_URL=${process.env.IMAGE_URL}\n`);
+    fs.appendFileSync(
+      pathToEnv,
+      `NEXT_PUBLIC_FORMIUM_PROJECTID=${process.env.NEXT_PUBLIC_FORMIUM_PROJECTID}\n`
+    );
+    fs.appendFileSync(
+      pathToEnv,
+      `FORMIUM_TOKEN=${process.env.FORMIUM_TOKEN}\n`
+    );
   },
 };
