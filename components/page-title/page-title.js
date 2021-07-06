1def883a829ebc3735c7d162c9cf0ad67f4eda1a
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-07-06 17:27:37 +0100

    Style header

--- components/page-title/page-title.js
+++ components/page-title/page-title.js
@@ -1,13 +1,26 @@
 import styles from "./page-title.module.css";
 
-export const PageTitle = ({ heading, description, className = "" }) => {
+export const PageTitle = ({
+  children,
+  heading,
+  description,
+  className = "",
+}) => {
   return (
     <div className={`${styles.container} ${className}`}>
-      <h1>
-        <img width="41px" height="48px" src="/dash.svg" alt="A brush stroke" />
-        {heading}
-      </h1>
-      <p>{description}</p>
+      <div>
+        <h1>
+          <img
+            width="41px"
+            height="48px"
+            src="/dash.svg"
+            alt="A brush stroke"
+          />
+          {heading}
+        </h1>
+        <p>{description}</p>
+      </div>
+      {children && <div className={styles.childContainer}>{children}</div>}
     </div>
   );
 };
