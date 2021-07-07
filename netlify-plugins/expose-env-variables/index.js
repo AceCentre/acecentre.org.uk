bb261f844f0491b8f47cd9c97c81e96ac33f4993
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-07-07 15:15:48 +0100

    Add prom and all the creds

--- netlify-plugins/expose-env-variables/index.js
+++ netlify-plugins/expose-env-variables/index.js
@@ -4,13 +4,13 @@ const path = require("path");
 module.exports = {
   onPreBuild: async () => {
     const pathToEnv = path.join(process.cwd(), "./.env");
-
     fs.appendFileSync(pathToEnv, "\nNETLIFY=true\n");
     fs.appendFileSync(pathToEnv, `CONTEXT=${process.env.CONTEXT}\n`);
     fs.appendFileSync(pathToEnv, `REDIS_URL=${process.env.REDIS_URL}\n`);
     fs.appendFileSync(pathToEnv, `GITHUB_SHA=${process.env.GITHUB_SHA}\n`);
     fs.appendFileSync(pathToEnv, `BUILD_ID=${process.env.BUILD_ID}\n`);
     fs.appendFileSync(pathToEnv, `DEPLOY_KEY=${process.env.DEPLOY_KEY}\n`);
+    fs.appendFileSync(pathToEnv, `PROM_KEY=${process.env.PROM_KEY}\n`);
     fs.appendFileSync(pathToEnv, `IMAGE_URL=${process.env.IMAGE_URL}\n`);
     fs.appendFileSync(
       pathToEnv,
