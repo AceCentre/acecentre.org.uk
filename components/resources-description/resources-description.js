da8f268bd011534e2b4371b992786e44df1709ea
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-07-08 16:11:34 +0100

    Handle all downloads and payments

--- components/resources-description/resources-description.js
+++ components/resources-description/resources-description.js
@@ -1,8 +1,6 @@
 import styles from "./resources-description.module.css";
 
 export const ResourcesDescription = ({ resource }) => {
-  console.log(resource);
-
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>{resource.name}</h1>
