5df06133744fd71d56b3e7abb135d43559ea491b
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-07-08 14:26:30 +0100

    Use short description

--- components/resources-description/resources-description.js
+++ components/resources-description/resources-description.js
@@ -1,12 +1,14 @@
 import styles from "./resources-description.module.css";
 
 export const ResourcesDescription = ({ resource }) => {
+  console.log(resource);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>{resource.name}</h1>
       <div
         className={styles.description}
-        dangerouslySetInnerHTML={{ __html: resource.description }}
+        dangerouslySetInnerHTML={{ __html: resource.shortDescription }}
       />
     </div>
   );
