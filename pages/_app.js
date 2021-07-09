d98a106df6cd37a688b3c771591a43c3a4463308
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-07-09 15:59:36 +0100

    Routing logs

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,30 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { SSRProvider } from "@react-aria/ssr";
 
 import { GlobalsContext } from "../lib/global-props/context";
+import { useRouter } from "next/dist/client/router";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps: { globalProps = {}, ...pageProps } }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url, { shallow }) => {
+      console.log(
+        `App is changing to ${url} ${
+          shallow ? "with" : "without"
+        } shallow routing`
+      );
+    };
+
+    router.events.on("routeChangeStart", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method:
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChange);
+    };
+  }, []);
+
   return (
     <GlobalsContext.Provider value={globalProps}>
       <DefaultHead />
