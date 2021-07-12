78dce53a75966574ec65d401de9a82610a416e3a
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-07-12 15:32:05 +0100

    Add bespoke training

---
+++ components/bespoke-training/bespoke-training.js
@@ -0,0 +1,34 @@
+import styles from "./bespoke-training.module.css";
+
+import { Button } from "../button/button";
+import { ImageWithLoader as Image } from "../image";
+
+export const BespokeTraining = () => {
+  return (
+    <div className={styles.container}>
+      <div>
+        <h2>Bespoke training solutions</h2>
+        <p>
+          Our expert Ace Centre team can help to create a bespoke training
+          solution for you. Click below to fill out our training enquiry form to
+          see how we can develop a suitable training programme for your
+          requirements.
+        </p>
+        <div className={styles.button}>
+          <Button href="/form/bespoke-training">Make an online enquiry</Button>
+        </div>
+        <p>
+          or call our advice line on <strong>0800 048 7642</strong>
+        </p>
+      </div>
+      <div className={styles.imageContainer}>
+        <Image
+          src={"./bespoke-training.jpeg"}
+          layout="fill"
+          objectFit="cover"
+        />
+        <div className={styles.background} />
+      </div>
+    </div>
+  );
+};
