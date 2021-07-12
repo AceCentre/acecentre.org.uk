96733e9bb0532f8382e0cd5e97701f697d144500
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-07-12 10:38:36 +0100

    Scaffold course list

---
+++ components/course-list/course-list.js
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import {
+  Card,
+  usePostsWithoutImageCounters,
+} from "../latest-from-blog/latest-from-blog";
+
+import styles from "./course-list.module.css";
+
+export const CourseList = ({
+  title,
+  viewAllLink,
+  viewAllText = "View all",
+  products,
+  className = "",
+}) => {
+  const productsWithoutImageCounters = usePostsWithoutImageCounters(products);
+
+  return (
+    <div className={`${styles.container} ${className}`}>
+      <div className={styles.titleContainer}>
+        {title && <h2 className={styles.title}>{title}</h2>}
+        {viewAllLink && (
+          <Link href={viewAllLink}>
+            <a className={styles.viewAllLink}>{viewAllText} &gt;</a>
+          </Link>
+        )}
+      </div>
+      <ul className={styles.postList}>
+        {productsWithoutImageCounters.map((product) => {
+          return (
+            <Card
+              className={styles.card}
+              imageContainerClassName={styles.imageContainer}
+              href={`/learning/${product.slug}`}
+              key={`${title}-card-${product.slug}`}
+              noImagePostCount={product.noImagePostCount}
+              subtitle={product.mainCategoryName}
+              featuredImage={product.featuredImage}
+              title={product.title}
+            >
+              <p className={styles.productTitle}>{product.title}</p>
+            </Card>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
