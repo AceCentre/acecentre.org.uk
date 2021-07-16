0578917178967df2f7502963a062a76718864d9c
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-07-16 09:30:47 +0100

    List all courses

--- pages/learning/search.js
+++ pages/learning/search.js
@@ -6,7 +6,10 @@ import { defaultNavItems } from "../../components/sub-nav/sub-nav";
 import { useGlobalProps } from "../../lib/global-props/hook";
 import { withGlobalProps } from "../../lib/global-props/inject";
 
-export default function LearningSearchPage() {
+import { CourseList } from "../../components/course-list/course-list";
+import { getAllCourses } from "../../lib/products/get-courses";
+
+export default function LearningSearchPage({ courses }) {
   const { currentYear } = useGlobalProps();
 
   return (
@@ -17,10 +20,15 @@ export default function LearningSearchPage() {
       <main>
         <BackToLink where="Ace Centre Learning" href="/learning" />
         <CourseFilter />
+        <CourseList products={courses} />
       </main>
       <Footer currentYear={currentYear} />
     </>
   );
 }
 
-export const getServerSideProps = withGlobalProps();
+export const getServerSideProps = withGlobalProps(async () => {
+  let courses = await getAllCourses();
+
+  return { props: { courses } };
+});
