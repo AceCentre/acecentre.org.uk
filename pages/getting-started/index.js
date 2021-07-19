c655949864043bfcb3e209aa81f96ff842b46265
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-07-19 09:09:16 +0100

    Add banner to getting-started

--- pages/getting-started/index.js
+++ pages/getting-started/index.js
@@ -1,8 +1,9 @@
-import { CombinedNav } from "../components/combined-nav/combined-nav";
-import { Footer } from "../components/footer/footer";
-import { defaultNavItems } from "../components/sub-nav/sub-nav";
-import { useGlobalProps } from "../lib/global-props/hook";
-import { withGlobalProps } from "../lib/global-props/inject";
+import { SearchBox } from "../../components/search-box/search-box";
+import { CombinedNav } from "../../components/combined-nav/combined-nav";
+import { Footer } from "../../components/footer/footer";
+import { defaultNavItems } from "../../components/sub-nav/sub-nav";
+import { useGlobalProps } from "../../lib/global-props/hook";
+import { withGlobalProps } from "../../lib/global-props/inject";
 
 export default function GettingStartedLanding() {
   const { currentYear } = useGlobalProps();
@@ -12,7 +13,16 @@ export default function GettingStartedLanding() {
       <header>
         <CombinedNav defaultNavItems={defaultNavItems} />
       </header>
-      <main></main>
+      <main>
+        <SearchBox
+          includeSearch={false}
+          title="Getting started with AAC and AT"
+          description="Learn about Augmentative and Alternative Communication (AAC) and how it can help"
+          backgroundImage="/pink-wave.svg"
+          backgroundColor="#F1D1D0"
+          textColor="#333333"
+        />
+      </main>
       <Footer currentYear={currentYear} />
     </>
   );
