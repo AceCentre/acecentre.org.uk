9d2f8e1780572a8d8e4cc883e84296a9dbcbc3e9
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-07-20 14:53:31 +0100

    Finish first pass at getting started detail page

--- components/project-highlight/project-highlight.js
+++ components/project-highlight/project-highlight.js
@@ -9,26 +9,46 @@ export const ProjectHighlight = ({ project }) => {
       <h2 className={styles.topTitle}>
         Learn about how we created this resource
       </h2>
-      <div className={styles.container}>
+      <CardHighlight
+        image={project.featuredImage}
+        title={project.title}
+        description={project.description}
+        href={`/projects/${project.slug}`}
+        viewText="View project"
+      />
+    </>
+  );
+};
+
+export const CardHighlight = ({
+  title,
+  image,
+  description,
+  href,
+  viewText,
+}) => {
+  return (
+    <div className={styles.container}>
+      {image && (
         <div className={styles.imageContainer}>
           <Image
-            src={project.featuredImage.src}
-            alt={project.featuredImage.alt}
+            src={image.src}
+            alt={image.alt}
             layout="fill"
             objectFit="cover"
           />
         </div>
-        <div className={styles.content}>
-          <h3 className={styles.title}>{project.title}</h3>
-          <div
-            className={styles.description}
-            dangerouslySetInnerHTML={{ __html: project.description }}
-          />
-          <Link href={`/projects/${project.slug}`}>
-            <a className={styles.viewAllLink}>View project &gt;</a>
-          </Link>
-        </div>
+      )}
+      <div className={styles.content}>
+        <h3 className={styles.title}>{title}</h3>
+        <div
+          className={styles.description}
+          dangerouslySetInnerHTML={{ __html: description }}
+        />
+        <Link href={href}>
+          <a className={styles.viewAllLink}>{viewText} &gt;</a>
+        </Link>
       </div>
-    </>
+    </div>
   );
 };
