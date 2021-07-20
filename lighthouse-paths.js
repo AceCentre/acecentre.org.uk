cc2bd5d6ff21485aed14f6a01e33b93244a4df72
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-07-20 15:22:07 +0100

    Add what is aac

--- lighthouse-paths.js
+++ lighthouse-paths.js
@@ -32,6 +32,7 @@ const PATHS_TO_TEST = [
   "/getting-started",
   "/getting-started/finding-the-right-aid",
   "/getting-started/how-can-i-access-my-computer",
+  "/getting-started/what-is-aac",
 ];
 
 module.exports = PATHS_TO_TEST;
