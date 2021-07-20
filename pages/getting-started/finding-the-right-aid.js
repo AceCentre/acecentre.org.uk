f470d2652b1cd1993638e2bf3f3c11e9a8d67d1c
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-07-20 11:06:26 +0100

    Add cover to finding the right aid

--- pages/getting-started/finding-the-right-aid.js
+++ pages/getting-started/finding-the-right-aid.js
@@ -1,8 +1,11 @@
 import { CombinedNav } from "../../components/combined-nav/combined-nav";
 import { Footer } from "../../components/footer/footer";
 import { defaultNavItems } from "../../components/sub-nav/sub-nav";
+import { VideoWithCardCover } from "../../components/video-with-card-cover/video-with-card-cover";
 import { useGlobalProps } from "../../lib/global-props/hook";
 import { withGlobalProps } from "../../lib/global-props/inject";
+
+import styles from "../../styles/getting-started.module.css";
 
 export default function GettingStartedLanding() {
   const { currentYear } = useGlobalProps();
@@ -12,7 +15,18 @@ export default function GettingStartedLanding() {
       <header>
         <CombinedNav defaultNavItems={defaultNavItems} />
       </header>
-      <main></main>
+      <main>
+        <VideoWithCardCover
+          src="/finding-the-right-aid-cover.jpg"
+          alt="An individual using a head tracking AAC device"
+        >
+          <h1>Which is the right communication aid for me?</h1>
+          <p className={styles.description}>
+            The problem is that there is no ‘one size fits all’ communication
+            aid solution.
+          </p>
+        </VideoWithCardCover>
+      </main>
       <Footer currentYear={currentYear} />
     </>
   );
