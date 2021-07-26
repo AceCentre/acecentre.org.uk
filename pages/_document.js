5eb67bc1147e2aeca2f06eabbc45cf9adecd0cb0
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-07-26 14:12:03 +0100

    Revert "Load timeline on prod"
    This reverts commit 1162675748cad914ca1203d99cce5888926bfb8c.

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import config from "../lib/config";
 
 class MyDocument extends Document {
   render() {
@@ -19,18 +18,6 @@ class MyDocument extends Document {
         <body style={{ color: "#333333" }}>
           <Main />
           <NextScript />
-          {config.environment == "production" && (
-            <div
-              dangerouslySetInnerHTML={{
-                __html: `
-          
-            <script>
-              !function(){var e=function(){var e,t=document.getElementsByTagName("body")[0];(e=document.createElement("script")).async=1,e.src="https://s3-us-west-2.amazonaws.com/o-t/vf/v.min.js",t.appendChild(e),(e=document.createElement("script")).async=1,e.src="https://s3-us-west-2.amazonaws.com/o-t/vf/c-3.0.4.min.js",t.appendChild(e)},t=function(e){window.jQuery?e():setTimeout(function(){t(e)},50)};window.onload=function(){window.teidvf=5451,window.tio_vf_eat="x7LgPCUN";var n=document.getElementsByTagName("head")[0],s=document.createElement("link");if(s.rel="stylesheet",s.type="text/css",s.href="https://s3-us-west-2.amazonaws.com/o-t/vf/s.min.css",n.appendChild(s),"undefined"==typeof jQuery){var a=document.createElement("script");a.async=1,a.src="https://s3-us-west-2.amazonaws.com/o-t/vf/j.min.js",n.appendChild(a)}t(e)}}();
-            </script>
-          `,
-              }}
-            ></div>
-          )}
         </body>
       </Html>
     );
