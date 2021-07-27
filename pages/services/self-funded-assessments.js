df9f0de1a508ed267d9daee121373192478ae7e5
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-07-27 10:17:27 +0100

    Add self funded assessments

--- pages/services/self-funded-assessments.js
+++ pages/services/self-funded-assessments.js
@@ -28,7 +28,7 @@ export default function EngineeringPage() {
           src="/services/engineering.jpg"
           alt="An engineering using a 3D printer"
         >
-          <h1 className={styles.cardTitle}>Assessments</h1>
+          <h1 className={styles.cardTitle}>Self-funded assessments</h1>
           <p className={styles.cardDescription}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do
             eiusmod tempor incididunt ut labore.
@@ -40,84 +40,65 @@ export default function EngineeringPage() {
         </VideoWithCardCover>
         <div className={styles.bottomContainer}>
           <div className={styles.leftContent}>
-            <h2>About our assessments</h2>
-            <p>
-              Our assessment and training team will work with the individual,
-              family and involved professionals to identify and achieve goals to
-              support the development of communication and learning, and
-              facilitate greater independence.
-            </p>
-            <h2>Type of assessments</h2>
+            <h2>Self-funded assessments</h2>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation.
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
             </p>
+            <h2>The referral form</h2>
             <p>
-              Dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna Lorem ipsum dolor sit amet,
-              consectetur adipiscing elit, sed do eiusmod
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
             </p>
-
-            <h2>Am I eligible for an NHS England assessment?</h2>
+            <h2>Video support</h2>
             <p>
-              To be eligible for the NHS England Specialised AAC Services, a
-              person must:
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
             </p>
-            <ul className={styles.list}>
-              <ListItem>Be resident in England</ListItem>
-              <ListItem>Be registered with a GP practice in England</ListItem>
-              <ListItem>
-                Have a severe/complex communication difficulty associated with a
-                range of physical, cognitive, learning, or sensory deficits
-              </ListItem>
-              <ListItem>
-                Have a clear discrepancy between their level of understanding
-                and their ability to speak
-              </ListItem>
-              <ListItem>
-                Be able to understand the purpose of a communication aid
-              </ListItem>
-              <ListItem>
-                Have developed beyond cause and effect understanding
-              </ListItem>
-              <ListItem>
-                Link ideas/ semantic categories and syntactic functions beyond
-                basic requests.
-              </ListItem>
-            </ul>
-
-            <div className={styles.inlineCard}>
-              <h2>
-                I am <span className={styles.normalWeight}>eligible for</span>{" "}
-                NHS England funded assessments
-              </h2>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt olor sit amet, consectetur adit labore
-                et dolore magna aliqua.
-              </p>
-              <Link href="/services/nhs/assessments">
-                <a className={styles.link}>View service &gt;</a>
-              </Link>
-            </div>
-            <div className={styles.inlineCard}>
-              <h2>
-                <span className={styles.normalWeight}>I’m</span> not{" "}
-                <span className={styles.normalWeight}>eligible for</span> NHS
-                England funded assessments
-              </h2>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt olor sit amet, consectetur adit labore
-                et dolore magna aliqua.
-              </p>
-              <Link href="/services/self-funded-assessments">
-                <a className={styles.link}>View service &gt;</a>
-              </Link>
-            </div>
+            <h2>Prior to the assessment</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <h2>Who should be involved?</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud.
+            </p>
+            <h2>The venue</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud.
+            </p>
+            <h2>Length of assessment</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud.
+            </p>
+            <h2>What happens at the assessment?</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud.
+            </p>
+            <h2>Following the assessment</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud.
+            </p>
           </div>
-
           <div>
             <div className={styles.quote}>
               <Avatar className={styles.avatar}>
@@ -144,22 +125,6 @@ export default function EngineeringPage() {
               </Avatar>
               <div className={styles.quoteText}>
                 <h3>Self-assessment referral form</h3>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt olor sit.
-                </p>
-                <div className={styles.downloadButtonContainer}>
-                  <Button>Download form</Button>
-                </div>
-              </div>
-            </div>
-
-            <div className={styles.quote}>
-              <Avatar className={styles.avatar}>
-                <FormatQuoteIcon className={styles.icon} />
-              </Avatar>
-              <div className={styles.quoteText}>
-                <h3>NHS England referral form</h3>
                 <p>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor incididunt olor sit.
