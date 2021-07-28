bddc4c90436d48ab2c386909b4ad9d5d607fe0e8
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-07-28 12:09:15 +0100

    Add vide with cover

--- pages/get-involved/index.js
+++ pages/get-involved/index.js
@@ -1,6 +1,7 @@
 import { CombinedNav } from "../../components/combined-nav/combined-nav";
 import { Footer } from "../../components/footer/footer";
 import { defaultNavItems } from "../../components/sub-nav/sub-nav";
+import { VideoWithCardCover } from "../../components/video-with-card-cover/video-with-card-cover";
 import { useGlobalProps } from "../../lib/global-props/hook";
 import { withGlobalProps } from "../../lib/global-props/inject";
 
@@ -12,7 +13,19 @@ export default function GetInvolved() {
       <header>
         <CombinedNav defaultNavItems={defaultNavItems} />
       </header>
-      <main></main>
+      <main>
+        <VideoWithCardCover
+          src="/get-involved.jpg"
+          alt="Client getting fitted with a new AAC device"
+        >
+          <h1>Get involved</h1>
+          <p>
+            We’re ensuring that people with the severest communication
+            challenges continue to benefit from professional assessment and
+            intervention
+          </p>
+        </VideoWithCardCover>
+      </main>
       <Footer currentYear={currentYear} />
     </>
   );
