eb5646806dfc4d70e9102b45de61e126713dd90f
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-07-28 12:25:27 +0100

    Add ways to get involved

---
+++ components/ways-to-get-involved/ways-to-get-involved.stories.js
@@ -0,0 +1,11 @@
+import { WaysToGetInvolved } from "./ways-to-get-involved";
+
+export default {
+  title: "/GetInvolved/WaysToGetInvovled",
+  component: WaysToGetInvolved,
+};
+
+const Template = (args) => <WaysToGetInvolved {...args} />;
+
+export const Standard = Template.bind({});
+Standard.args = {};
