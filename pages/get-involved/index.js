dbaca0e8ed64b8ac770958b97f74faa4cb20ab02
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-07-28 13:51:21 +0100

    Fix widths

--- pages/get-involved/index.js
+++ pages/get-involved/index.js
@@ -1,12 +1,14 @@
 import { CombinedNav } from "../../components/combined-nav/combined-nav";
+import { FeaturedStory } from "../../components/featured-story/featured-story";
 import { Footer } from "../../components/footer/footer";
 import { defaultNavItems } from "../../components/sub-nav/sub-nav";
 import { VideoWithCardCover } from "../../components/video-with-card-cover/video-with-card-cover";
 import { WaysToGetInvolved } from "../../components/ways-to-get-involved/ways-to-get-involved";
 import { useGlobalProps } from "../../lib/global-props/hook";
 import { withGlobalProps } from "../../lib/global-props/inject";
+import { getSimpleStory } from "../../lib/story/get-story";
 
-export default function GetInvolved() {
+export default function GetInvolved({ featuredStory }) {
   const { currentYear } = useGlobalProps();
 
   return (
@@ -27,10 +29,15 @@ export default function GetInvolved() {
           </p>
         </VideoWithCardCover>
         <WaysToGetInvolved />
+        <FeaturedStory {...featuredStory} />
       </main>
       <Footer currentYear={currentYear} />
     </>
   );
 }
 
-export const getStaticProps = withGlobalProps();
+export const getStaticProps = withGlobalProps(async () => {
+  const featuredStory = await getSimpleStory("paul");
+
+  return { props: { featuredStory } };
+});
