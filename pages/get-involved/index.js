2e9a4781561001e0aca9a752f4386000344c742f
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-07-28 14:52:28 +0100

    Finish get involved landing page

--- pages/get-involved/index.js
+++ pages/get-involved/index.js
@@ -1,4 +1,5 @@
 import { CombinedNav } from "../../components/combined-nav/combined-nav";
+import { Facts } from "../../components/facts/facts";
 import { FeaturedStory } from "../../components/featured-story/featured-story";
 import { Footer } from "../../components/footer/footer";
 import { defaultNavItems } from "../../components/sub-nav/sub-nav";
@@ -7,6 +8,8 @@ import { WaysToGetInvolved } from "../../components/ways-to-get-involved/ways-to
 import { useGlobalProps } from "../../lib/global-props/hook";
 import { withGlobalProps } from "../../lib/global-props/inject";
 import { getSimpleStory } from "../../lib/story/get-story";
+
+import styles from "../../styles/get-involved.module.css";
 
 export default function GetInvolved({ featuredStory }) {
   const { currentYear } = useGlobalProps();
@@ -28,6 +31,37 @@ export default function GetInvolved({ featuredStory }) {
             intervention
           </p>
         </VideoWithCardCover>
+        <div className={styles.columns}>
+          <div className={styles.left}>
+            <h2>Why support AceCentre?</h2>
+            <p>
+              Staffed by teams of teachers, therapists and technicians, Ace
+              Centre offers many years experience and expertise in Assistive
+              Technology (AT), particularly Augmentative and Alternative
+              Communication (AAC). The charity also provides a range of general
+              services to support children and adults with severe communication
+              difficulties.
+            </p>
+            <p>
+              We are grateful to sponsors, commissioners and funding bodies that
+              enable us to continue and develop our provision. As a relatively
+              small charity we depend on regular giving to be able to continue
+              to offer high quality services that are effective for those who
+              use them.
+            </p>
+            <p>
+              By supporting Ace Centre you help ensure that people with the
+              severest communication challenges continue to benefit from
+              professional assessment and intervention. Whilst we often make use
+              of low-tech gadgets and techniques we also need to be able to
+              provide expensive solutions for those who need them.
+            </p>
+            <p>This is why we hope you will support Ace Centre.</p>
+          </div>
+          <div className={styles.right}>
+            <Facts />
+          </div>
+        </div>
         <WaysToGetInvolved />
         <FeaturedStory {...featuredStory} />
       </main>
