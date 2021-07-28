463c9df8f678dabde0b83098d2a89b35d7bef58f
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-07-28 16:41:57 +0100

    Finish donate page

---
+++ components/other-ways-to-donate/other-ways-to-donate.js
@@ -0,0 +1,87 @@
+import styles from "./other-ways-to-donate.module.css";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import { Avatar } from "@material-ui/core";
+import { Button } from "../button/button";
+
+export const OtherWaysToDonate = () => {
+  return (
+    <div className={styles.outerContainer}>
+      <div className={styles.container}>
+        <h2 className={styles.title}>Other ways to donate</h2>
+        <div className={styles.grid}>
+          <div>
+            <h3>One off donations</h3>
+            <p>
+              You can donate here on our giving site or directly through bank
+              transfer.
+            </p>
+          </div>
+          <div>
+            <h3>Fundraise</h3>
+            <p>
+              You can sign up to give £1 a month or more here on our giving site
+              or directly through your bank by setting up a standing order. If
+              you do this directly through your bank using our account details
+              please let us know so we can look out for your donation. If you
+              prefer, you can complete this form and let us contact your bank.
+            </p>
+          </div>
+          <div>
+            <h3>Volunteer</h3>
+            <p>
+              Some presents really are priceless! Ask your family and friends to
+              donate to Ace Centre rather than give you another scented candle
+              (no offence to scented candle givers!) Or perhaps you could donate
+              in place of a present for someone else.
+            </p>
+          </div>
+          <div>
+            <h3>Donate in memory</h3>
+            <p>
+              Leaving a gift in memory of a loved one is a special way to help
+              others. Every gift really does make a difference – your donation
+              will help us to improve the quality of life for children and
+              adults with the most challenging communication needs by ensuring
+              they have prompt access to the advice and support they need so
+              they can achieve their goals.
+            </p>
+          </div>
+          <div>
+            <h3>Leave a gift in your will</h3>
+            <ul className={styles.list}>
+              <ListItem>What are the benefits of making a will?</ListItem>
+              <ListItem>How do I leave a gift in my will?</ListItem>
+              <ListItem>
+                I already have a will, do I need to make a new one, or can I
+                just amend it?
+              </ListItem>
+              <ListItem>What next?</ListItem>
+            </ul>
+          </div>
+          <div className={styles.inlineCard}>
+            <h3>Donate today</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna.
+            </p>
+            <Button>Get in touch online</Button>
+            <p>
+              or call us on <strong>0800 048 7642</strong>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const ListItem = ({ children }) => {
+  return (
+    <li className={styles.listItem}>
+      <Avatar className={styles.listAvatar}>
+        <ChevronRightIcon />
+      </Avatar>
+      {children}
+    </li>
+  );
+};
