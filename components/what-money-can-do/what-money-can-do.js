27070008927ae3def75ad2a7403417033d4a042e
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-07-30 09:21:59 +0100

    Fix a11y issues on donate page

--- components/what-money-can-do/what-money-can-do.js
+++ components/what-money-can-do/what-money-can-do.js
@@ -6,7 +6,12 @@ export const WhatMoneyCanDo = () => {
   return (
     <div className={styles.container}>
       <div className={styles.imageContainer}>
-        <Image src="/what-money-can-do.jpg" layout="fill" objectFit="cover" />
+        <Image
+          src="/what-money-can-do.jpg"
+          layout="fill"
+          objectFit="cover"
+          alt="Two people looking at a communication device"
+        />
         <div className={styles.imageOverlay}></div>
       </div>
       <div>
@@ -16,7 +21,7 @@ export const WhatMoneyCanDo = () => {
             Helps ensure a prompt telephone or email response to someone feeling
             desperate
           </WhatItCanDo>
-          <WhatItCanDo cost="25" color="#699CC6">
+          <WhatItCanDo cost="25" color="#6499C4">
             Can provide an alphabet or picture communication board for someone
             with little or no speech
           </WhatItCanDo>
@@ -24,7 +29,7 @@ export const WhatMoneyCanDo = () => {
             Helps us to provide an hour’s appointment so someone with severe
             communication challenges can start getting the right support at last
           </WhatItCanDo>
-          <WhatItCanDo cost="100" color="#E08800">
+          <WhatItCanDo cost="100" color="#D17D00">
             A full day’s training for families, with the opportunity to meet
             other parents who support children with challenging communication
             needs.
