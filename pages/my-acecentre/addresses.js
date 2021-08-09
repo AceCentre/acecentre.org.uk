1092dc4774a8c750d637f59aa32bb553d9f91107
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-08-09 15:22:19 +0100

    Scaffold addresses

---
+++ pages/my-acecentre/addresses.js
@@ -0,0 +1,37 @@
+import { CombinedNav } from "../../components/combined-nav/combined-nav";
+import { Footer } from "../../components/footer/footer";
+import { defaultNavItems } from "../../components/sub-nav/sub-nav-items";
+import withSession from "../../lib/auth/with-session";
+import { useGlobalProps } from "../../lib/global-props/hook";
+
+export default function Addresses() {
+  const { currentYear } = useGlobalProps();
+
+  return (
+    <>
+      <header>
+        <CombinedNav defaultNavItems={defaultNavItems} />
+      </header>
+      <main></main>
+      <Footer currentYear={currentYear} />
+    </>
+  );
+}
+
+// Redirect if you are signed in
+export const getServerSideProps = withSession(async function ({ req }) {
+  const user = req.session.get("user");
+
+  if (!user || !user.authToken) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+});
