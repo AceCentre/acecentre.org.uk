f6c5d4b47d2f40237d93844343f6fc414885e93f
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-08-10 10:27:25 +0100

    Finish front end for address change

--- components/address-field/address-field.js
+++ components/address-field/address-field.js
@@ -2,11 +2,110 @@ import styles from "./address-field.module.css";
 import { Input as ChakraInput, FormControl, FormLabel } from "@chakra-ui/react";
 import { Button } from "../button/button";
 import { Select } from "@chakra-ui/react";
+import { useState } from "react";
 
-export const AddressField = ({ title, details = {}, countries }) => {
+const useAddressSubmit = (addressType) => {
+  const [submitDisabled, setSubmitDisabled] = useState(false);
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+    setSubmitDisabled(true);
+
+    let address = {};
+    const elements = e.target.elements;
+
+    if (elements.firstName) {
+      address["firstName"] = elements.firstName.value || "";
+    }
+    if (elements.lastName) {
+      address["lastName"] = elements.lastName.value || "";
+    }
+    if (elements.company) {
+      address["company"] = elements.company.value || "";
+    }
+    if (elements.country) {
+      address["country"] = elements.country.value || "";
+    }
+    if (elements.addressLine1) {
+      address["addressLine1"] = elements.addressLine1.value || "";
+    }
+    if (elements.addressLine2) {
+      address["addressLine2"] = elements.addressLine2.value || "";
+    }
+    if (elements.city) {
+      address["city"] = elements.city.value || "";
+    }
+    if (elements.county) {
+      address["county"] = elements.county.value || "";
+    }
+    if (elements.postcode) {
+      address["postcode"] = elements.postcode.value || "";
+    }
+    if (elements.phone) {
+      address["phone"] = elements.phone.value || "";
+    }
+    if (elements.email) {
+      address["email"] = elements.email.value || "";
+    }
+
+    const submit = async () => {
+      try {
+        const response = await fetch("/api/auth/addresses", {
+          method: "POST",
+          body: JSON.stringify({ address, addressType }),
+        });
+
+        const parsed = await response.json();
+
+        if (parsed.success) {
+          setSubmitDisabled(false);
+          setErrorMessage(null);
+          setSuccessMessage("Successfully update details");
+          return;
+        }
+
+        if (parsed.validationError) {
+          setSubmitDisabled(false);
+          setErrorMessage(parsed.validationError);
+          setSuccessMessage(null);
+          return;
+        }
+
+        throw new Error("An error occurred");
+      } catch (error) {
+        console.warn(error);
+        setSubmitDisabled(false);
+        setErrorMessage("An error occurred");
+        setSuccessMessage(null);
+      }
+    };
+
+    submit();
+  };
+
+  return { formSubmit, submitDisabled, successMessage, errorMessage };
+};
+
+export const AddressField = ({
+  title,
+  details = {},
+  countries,
+  billing = false,
+}) => {
+  const {
+    formSubmit,
+    submitDisabled,
+    successMessage,
+    errorMessage,
+  } = useAddressSubmit(billing ? "billing" : "shipping");
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={formSubmit}>
       <h2 className={styles.title}>{title}</h2>
+      {successMessage && <p className={styles.success}>{successMessage}</p>}
+      {errorMessage && <p className={styles.error}>{errorMessage}</p>}
       <div className={styles.twoWide}>
         <Input
           maxWidth="100%"
@@ -33,28 +132,52 @@ export const AddressField = ({ title, details = {}, countries }) => {
         id="companyName"
         defaultValue={details.company}
       />
-
-      <FormControl className={styles.formControl} id="country">
-        <FormLabel>Country</FormLabel>
-        <Select
-          maxWidth={900}
-          width="100%"
-          borderRadius={4}
-          backgroundColor="#F5F5F5"
-          defaultValue={details.country}
-          name="country"
-          aria-label="Country"
-          placeholder="Select your country."
-        >
-          {countries.map((country) => {
-            return (
-              <option value={country.name} key={`country-${country.name}`}>
-                {country.description}
-              </option>
-            );
-          })}
-        </Select>
-      </FormControl>
+      {billing ? (
+        <FormControl className={styles.formControl} id="country">
+          <FormLabel>Country</FormLabel>
+          <Select
+            maxWidth={900}
+            width="100%"
+            borderRadius={4}
+            backgroundColor="#F5F5F5"
+            defaultValue={details.country}
+            name="country"
+            aria-label="Country"
+            placeholder="Select your country."
+          >
+            {countries.map((country) => {
+              return (
+                <option value={country.name} key={`country-${country.name}`}>
+                  {country.description}
+                </option>
+              );
+            })}
+          </Select>
+        </FormControl>
+      ) : (
+        <FormControl className={styles.formControl} id="country">
+          <FormLabel>Country</FormLabel>
+          <Select
+            maxWidth={900}
+            width="100%"
+            borderRadius={4}
+            backgroundColor="#F5F5F5"
+            name="country"
+            aria-label="Country"
+            placeholder="Select your country."
+            value="GB"
+            disabled
+          >
+            {countries.map((country) => {
+              return (
+                <option value={country.name} key={`country-${country.name}`}>
+                  {country.description}
+                </option>
+              );
+            })}
+          </Select>
+        </FormControl>
+      )}
 
       <Input
         maxWidth="100%"
@@ -96,24 +219,30 @@ export const AddressField = ({ title, details = {}, countries }) => {
         id="postcode"
         defaultValue={details.postcode}
       />
-      <Input
-        maxWidth="100%"
-        placeholder="07585634751"
-        name="phone"
-        ariaLabel="Phone"
-        id="phone"
-        defaultValue={details.phone}
-      />
-      <Input
-        maxWidth="100%"
-        placeholder="john@smith.com"
-        name="email"
-        ariaLabel="Email"
-        id="email"
-        defaultValue={details.email}
-      />
+      {billing && (
+        <>
+          <Input
+            maxWidth="100%"
+            placeholder="07585634751"
+            name="phone"
+            ariaLabel="Phone"
+            id="phone"
+            defaultValue={details.phone}
+          />
+          <Input
+            maxWidth="100%"
+            placeholder="john@smith.com"
+            name="email"
+            ariaLabel="Email"
+            id="email"
+            defaultValue={details.email}
+          />
+        </>
+      )}
       <div>
-        <Button type="submit">Save address</Button>
+        <Button disabled={submitDisabled} type="submit">
+          Save address
+        </Button>
       </div>
     </form>
   );
@@ -144,6 +273,7 @@ export const BillingDetails = ({ details, countries }) => {
       title="Billing address"
       countries={countries}
       details={details}
+      billing
     />
   );
 };
