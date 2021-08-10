ee9519b304d6db8391c02cd26840a1a38a16d2d2
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-08-10 15:00:25 +0100

    Make form functional

--- components/details-form/details-form.js
+++ components/details-form/details-form.js
@@ -2,10 +2,109 @@ import styles from "./details-form.module.css";
 import { Input as ChakraInput, FormControl, FormLabel } from "@chakra-ui/react";
 import Link from "next/link";
 import { Button } from "../button/button";
+import { useState } from "react";
 
-export const DetailsForm = () => {
+const useChangeDetails = () => {
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setButtonDisabled(true);
+
+    const elements = e.target.elements;
+    let details = {};
+
+    if (elements.firstName) {
+      details["firstName"] = elements.firstName.value || "";
+    }
+
+    if (elements.lastName) {
+      details["lastName"] = elements.lastName.value || "";
+    }
+
+    if (elements.displayName) {
+      details["name"] = elements.displayName.value || "";
+    }
+
+    if (elements.email) {
+      details["email"] = elements.email.value || "";
+    }
+
+    console.log(details);
+
+    if (!details.firstName) {
+      setButtonDisabled(false);
+      setErrorMessage("First name is a required field.");
+      setSuccessMessage(null);
+      return;
+    }
+
+    if (!details.lastName) {
+      setButtonDisabled(false);
+      setErrorMessage("Last name is a required field.");
+      setSuccessMessage(null);
+      return;
+    }
+
+    if (!details.name) {
+      setButtonDisabled(false);
+      setErrorMessage("Display name is a required field.");
+      setSuccessMessage(null);
+      return;
+    }
+
+    if (!details.email) {
+      setButtonDisabled(false);
+      setErrorMessage("Email is a required field.");
+      setSuccessMessage(null);
+      return;
+    }
+
+    const submit = async () => {
+      try {
+        const response = await fetch("/api/auth/details", {
+          method: "POST",
+          body: JSON.stringify({ details }),
+        });
+
+        const parsed = await response.json();
+
+        if (parsed.success) {
+          setButtonDisabled(false);
+          setErrorMessage(null);
+          setSuccessMessage("Successfully updated details");
+          return;
+        }
+
+        throw new Error("An error occurred");
+      } catch (error) {
+        console.warn(error);
+        setButtonDisabled(false);
+        setErrorMessage("An error occurred");
+        setSuccessMessage(null);
+      }
+    };
+
+    submit();
+  };
+
+  return { onSubmit, buttonDisabled, successMessage, errorMessage };
+};
+
+export const DetailsForm = ({ details }) => {
+  const {
+    onSubmit,
+    buttonDisabled,
+    successMessage,
+    errorMessage,
+  } = useChangeDetails();
+
   return (
-    <form className={styles.container}>
+    <form onSubmit={onSubmit} className={styles.container}>
+      {successMessage && <p className={styles.success}>{successMessage}</p>}
+      {errorMessage && <p className={styles.error}>{errorMessage}</p>}{" "}
       <div className={styles.split}>
         <Input
           maxWidth="100%"
@@ -13,6 +112,7 @@ export const DetailsForm = () => {
           name="firstName"
           ariaLabel="First name"
           id="firstName"
+          defaultValue={details.firstName}
         />
         <Input
           maxWidth="100%"
@@ -20,6 +120,7 @@ export const DetailsForm = () => {
           name="lastName"
           ariaLabel="Last name"
           id="lastName"
+          defaultValue={details.lastName}
         />
       </div>
       <Input
@@ -28,6 +129,7 @@ export const DetailsForm = () => {
         name="displayName"
         ariaLabel="Display Name"
         id="displayName"
+        defaultValue={details.displayName}
       />
       <Input
         maxWidth="100%"
@@ -35,12 +137,15 @@ export const DetailsForm = () => {
         name="email"
         ariaLabel="Email"
         id="email"
+        defaultValue={details.email}
       />
-      <Link href="forgot-password">
+      <Link href="/forgot-password">
         <a className={styles.link}>Send password reset email</a>
       </Link>
       <div>
-        <Button type="submit">Save details</Button>
+        <Button disabled={buttonDisabled} type="submit">
+          Save details
+        </Button>
       </div>
     </form>
   );
