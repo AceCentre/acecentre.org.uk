6f2c2d169f0a3c15209d4d3bbaeceb61b86fc1ef
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-08-10 12:20:49 +0100

    Switch to babel parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: "module",
   },
+  parser: "@babel/eslint-parser",
   plugins: ["react", "jest", "cypress"],
   rules: {
     indent: ["error", 2],
