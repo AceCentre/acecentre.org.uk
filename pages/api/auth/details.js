38575c57664d9b10f48cccaa3a080135827faf24
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-08-10 15:18:39 +0100

    Finish details function

---
+++ pages/api/auth/details.js
@@ -0,0 +1,34 @@
+import { gql } from "graphql-request";
+import withSession from "../../../lib/auth/with-session";
+import { clientRequest } from "../../../lib/client-request";
+
+const UPDATE_DETAILS = gql`
+  mutation UpdateDetails($input: UpdateUserInput!) {
+    updateUser(input: $input) {
+      user {
+        id
+      }
+    }
+  }
+`;
+
+async function handler(req, res) {
+  const body = JSON.parse(req.body);
+  const user = req.session.get("user");
+
+  await clientRequest(req, UPDATE_DETAILS, {
+    input: {
+      id: user.userId,
+      firstName: body.details.firstName,
+      lastName: body.details.lastName,
+      displayName: body.details.name,
+      email: body.details.email,
+    },
+  });
+
+  res.send({
+    success: true,
+  });
+}
+
+export default withSession(handler);
