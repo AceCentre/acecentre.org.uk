a9116f4216ebf40dca779685d6401c1a375b371a
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-08-11 15:34:51 +0100

    Scaffold basket

--- pages/basket.js
+++ pages/basket.js
@@ -1,19 +1,30 @@
+import { CombinedNav } from "../components/combined-nav/combined-nav";
+import { Footer } from "../components/footer/footer";
+import { defaultNavItems } from "../components/sub-nav/sub-nav-items";
 import withSession from "../lib/auth/with-session";
 import { getCart } from "../lib/cart/get";
+import { useGlobalProps } from "../lib/global-props/hook";
 
-export default function CartPage({ rawCart }) {
+export default function Basket({ rawBasket }) {
+  const { currentYear } = useGlobalProps();
+
   return (
-    <div>
-      <h1>Cart</h1>
-      <pre>{JSON.stringify(rawCart, null, 2)}</pre>
-    </div>
+    <>
+      <header>
+        <CombinedNav defaultNavItems={defaultNavItems} />
+      </header>
+      <main>
+        <pre>{JSON.stringify(rawBasket, null, 2)}</pre>
+      </main>
+      <Footer currentYear={currentYear} />
+    </>
   );
 }
 
 export const getServerSideProps = withSession(async function ({ req }) {
-  const rawCart = await getCart(req);
+  const rawBasket = await getCart(req);
 
   return {
-    props: { rawCart },
+    props: { rawBasket },
   };
 });
