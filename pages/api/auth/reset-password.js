7ef24b870bb4baf365a4b66b7ede052b149466bf
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-08-11 14:42:59 +0100

    Finish reset password

---
+++ pages/api/auth/reset-password.js
@@ -0,0 +1,15 @@
+import { resetPassword } from "../../../lib/auth/get-user";
+import withSession from "../../../lib/auth/with-session";
+
+async function handler(req, res) {
+  const body = JSON.parse(req.body);
+  const username = body.username;
+
+  await resetPassword(req, username);
+
+  res.send({
+    success: true,
+  });
+}
+
+export default withSession(handler);
