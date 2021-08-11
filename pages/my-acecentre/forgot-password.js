e7b35de7a59825b0353b0237bd4f5b289d0625b4
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-08-11 14:27:26 +0100

    Setup frontend for forgot password

---
+++ pages/my-acecentre/forgot-password.js
@@ -0,0 +1,27 @@
+import { CombinedNav } from "../../components/combined-nav/combined-nav";
+import { Footer } from "../../components/footer/footer";
+import { ResetPasswordForm } from "../../components/reset-password-form/reset-password-form";
+import { defaultNavItems } from "../../components/sub-nav/sub-nav-items";
+import withSession from "../../lib/auth/with-session";
+import { useGlobalProps } from "../../lib/global-props/hook";
+
+export default function ChangePassword() {
+  const { currentYear } = useGlobalProps();
+
+  return (
+    <>
+      <header>
+        <CombinedNav defaultNavItems={defaultNavItems} />
+      </header>
+      <main>
+        <ResetPasswordForm />
+      </main>
+      <Footer currentYear={currentYear} />
+    </>
+  );
+}
+
+// Redirect if you are signed in
+export const getServerSideProps = withSession(async () => {
+  return { props: {} };
+});
