81983b7118bcd093885fc574884bf2dbd33ffa46
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-08-11 12:15:10 +0100

    Add a change password page

---
+++ pages/my-acecentre/change-password.js
@@ -0,0 +1,50 @@
+import { SvgIcon } from "@material-ui/core";
+import { CombinedNav } from "../../components/combined-nav/combined-nav";
+import { Footer } from "../../components/footer/footer";
+import { defaultNavItems } from "../../components/sub-nav/sub-nav-items";
+import withSession from "../../lib/auth/with-session";
+import { useGlobalProps } from "../../lib/global-props/hook";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+
+import styles from "../../styles/my-acecentre.module.css";
+
+export default function ChangePassword() {
+  const { currentYear } = useGlobalProps();
+
+  return (
+    <>
+      <header>
+        <CombinedNav defaultNavItems={defaultNavItems} />
+      </header>
+      <main className={styles.changePasswordContainer}>
+        <SvgIcon className={styles.changePasswordIcon}>
+          <MailOutlineIcon />
+        </SvgIcon>
+        <p>
+          We have sent you a password reset email. Please check your spam inbox.
+        </p>
+      </main>
+      <Footer currentYear={currentYear} />
+    </>
+  );
+}
+
+// Redirect if you are signed in
+export const getServerSideProps = withSession(async ({ req }) => {
+  const user = req.session.get("user");
+
+  if (!user || !user.authToken) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+
+  console.log(user);
+
+  // await changePassword(req, user);
+
+  return { props: {} };
+});
