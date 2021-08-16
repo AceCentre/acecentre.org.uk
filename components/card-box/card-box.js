1c4d35d8eaf4ccd35ec447a4f9446ccba055db6d
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-08-16 11:24:14 +0100

    Style input element

--- components/card-box/card-box.js
+++ components/card-box/card-box.js
@@ -5,22 +5,23 @@ export const CardBox = () => {
   return (
     <div className={styles.container}>
       <h3>Pay with your credit card</h3>
-      <CardElement
-        options={{
-          style: {
-            base: {
-              fontSize: "16px",
-              color: "#424770",
-              "::placeholder": {
-                color: "#aab7c4",
+
+      <div className={styles.cardElementContainer}>
+        <CardElement
+          className={styles.cardElement}
+          options={{
+            style: {
+              base: {
+                iconColor: "#000000",
+                color: "#000000",
+                fontWeight: "500",
+                fontFamily: "Montserrat",
+                fontSize: "16px",
               },
             },
-            invalid: {
-              color: "red",
-            },
-          },
-        }}
-      />
+          }}
+        />
+      </div>
     </div>
   );
 };
