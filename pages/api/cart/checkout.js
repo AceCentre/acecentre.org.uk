d9c3322723778a5afd7de23277f30bbddbe48406
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-08-17 10:48:22 +0100

    Add to mailing list if button is checked

--- pages/api/cart/checkout.js
+++ pages/api/cart/checkout.js
@@ -1,11 +1,20 @@
 import withSession from "../../../lib/auth/with-session";
 import { checkout } from "../../../lib/cart/checkout";
+import { addToMailingList } from "../auth/register";
 
 async function handler(req, res) {
   const body = JSON.parse(req.body);
 
   try {
+    // Add to mailing list
+    if (body.addToMailingList) {
+      await addToMailingList(body.billingDetails.email);
+    }
+
     const result = await checkout(req, body);
+
+    // Delete cart
+
     res.send({ success: true, result });
     return;
   } catch (error) {
