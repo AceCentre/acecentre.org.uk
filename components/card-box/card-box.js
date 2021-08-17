4ed541a1c09ec7efbd4a46c76fa0056c20e9eb42
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-08-17 09:18:51 +0100

    Setup card collection info

--- components/card-box/card-box.js
+++ components/card-box/card-box.js
@@ -1,15 +1,18 @@
 import { CardElement } from "@stripe/react-stripe-js";
 import styles from "./card-box.module.css";
 
-export const CardBox = () => {
+export const CardBox = ({ cardError }) => {
   return (
     <div className={styles.container}>
       <h3>Pay with your credit card</h3>
+
+      {cardError && <p className={styles.error}>{cardError}</p>}
 
       <div className={styles.cardElementContainer}>
         <CardElement
           className={styles.cardElement}
           options={{
+            hidePostalCode: true,
             style: {
               base: {
                 iconColor: "#000000",
