8103c9f2f933d04ce73a5ee621c2680fecd0c599
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-08-18 09:24:47 +0100

    Load result from storage

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { CombinedNav } from "../../components/combined-nav/combined-nav";
 import { Footer } from "../../components/footer/footer";
 import { PageTitle } from "../../components/page-title/page-title";
@@ -5,11 +6,16 @@ import { defaultNavItems } from "../../components/sub-nav/sub-nav-items";
 import { OrderDetailTable } from "../../components/table/table";
 import withSession from "../../lib/auth/with-session";
 import { useGlobalProps } from "../../lib/global-props/hook";
-import { getOrder } from "../../lib/products/get-orders";
 import style from "../../styles/order.module.css";
 
-export default function OrderPage({ orderId, order }) {
+export default function OrderPage({ orderId }) {
   const { currentYear } = useGlobalProps();
+
+  const [order, setOrder] = useState(null);
+
+  useEffect(() => {
+    setOrder(JSON.parse(localStorage.getItem(`order-${orderId}`)));
+  }, []);
 
   return (
     <>
@@ -18,9 +24,11 @@ export default function OrderPage({ orderId, order }) {
       </header>
       <main>
         <PageTitle heading="Order details" description={`Order #${orderId}`} />
-        <div className={style.tableContainer}>
-          <OrderDetailTable order={order} onClose={() => {}} />
-        </div>
+        {order && (
+          <div className={style.tableContainer}>
+            <OrderDetailTable order={order} />
+          </div>
+        )}
       </main>
       <Footer currentYear={currentYear} />
     </>
@@ -28,11 +36,9 @@ export default function OrderPage({ orderId, order }) {
 }
 
 export const getServerSideProps = withSession(
-  async ({ params: { id: orderId }, req }) => {
-    const order = await getOrder(req, orderId);
-
+  async ({ params: { id: orderId } }) => {
     return {
-      props: { orderId, order },
+      props: { orderId },
     };
   }
 );
