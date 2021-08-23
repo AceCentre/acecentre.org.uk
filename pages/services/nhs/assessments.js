6bdad19bdf64dde513b4896954248b7def4f5de6
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-08-23 10:09:27 +0100

    Content for NHS page

--- pages/services/nhs/assessments.js
+++ pages/services/nhs/assessments.js
@@ -7,7 +7,6 @@ import { useGlobalProps } from "../../../lib/global-props/hook";
 import { withGlobalProps } from "../../../lib/global-props/inject";
 
 import ListAltIcon from "@material-ui/icons/ListAlt";
-import { CardHighlight } from "../../../components/project-highlight/project-highlight";
 
 import FormatQuoteIcon from "@material-ui/icons/FormatQuote";
 import { Avatar } from "@material-ui/core";
@@ -17,6 +16,7 @@ import styles from "../../../styles/nhs-assessment.module.css";
 import { getSimpleStory } from "../../../lib/story/get-story";
 import { FeaturedStory } from "../../../components/featured-story/featured-story";
 import { InformationDays } from "../../../components/information-days/information-days";
+import Link from "next/link";
 
 export default function NHSLanding({ featuredStory }) {
   const { currentYear } = useGlobalProps();
@@ -34,8 +34,8 @@ export default function NHSLanding({ featuredStory }) {
         >
           <h1 className={styles.cardTitle}>NHS England Assessment</h1>
           <p className={styles.cardDescription}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do
-            eiusmod tempor incididunt ut labore.
+            Specialist AAC Service supporting the North West and Thames Valley &
+            Wessex regions
           </p>
           <Button className={styles.cardButton}>Make an online enquiry</Button>
           <p className={styles.cardContact}>
@@ -46,68 +46,109 @@ export default function NHSLanding({ featuredStory }) {
           <div className={styles.leftContent}>
             <h2>NHS England assessments</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
+              If you meet criteria and your permanent place of residence is in
+              the North West or the Thames Valley & Wessex regions, you can
+              apply to Ace Centre for assessment, provision of equipment and
+              long-term maintenance support.
+            </p>
+            <p>
+              If you live outside the North West or Thames Valley & Wessex
+              regions, you will need to contact the NHS Specialised AAC Service
+              operating in your region.{" "}
+              <Link href="http://www.communicationmatters.org.uk/page/contacts-assessment-services-all">
+                Click here to find out which service you need to contact
+              </Link>
+            </p>
+            <p>
+              <Link href="/docs/NhsEligibilityAce.pdf">
+                Further information on the NHS England Eligibility criteria can
+                be found in this document
+              </Link>
             </p>
             <h2>The referral form</h2>
+            <ul>
+              <li>Please use the forms below to make a referral.</li>
+              <li>
+                Read all guidance documentation and complete all fields in order
+                to avoid delay
+              </li>
+              <li>
+                If possible, referral forms should be completed by a Speech and
+                Language Therapist or someone knowledgeable in the field of AAC.
+              </li>
+              <li>
+                Please note: any referral forms received without a valid NHS
+                Number and full GP details cannot be accepted.
+              </li>
+            </ul>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
+              <Link href="/docs/NHS-England-Specialised-AAC-Services-Referral-Form-v3.2-2021.docx">
+                Download our NHS England Specialised AAC Services Referral Form
+                (Word format)
+              </Link>
             </p>
-            <h2>Video support</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
+              <Link href="/docs/Ace-Centre-Guidance-Notes-V3-2019-with-appendices.pdf">
+                Please read the guidance document for the form.
+              </Link>
             </p>
-            <h2>Prior to the assessment</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
+              It is important to use the latest version of the NHS Referral Form
+              – this is currently v3.2 2021. The latest version of this form,
+              and the corresponding Guidance Notes, will always be accessible
+              from this page.
             </p>
-            <h2>Who should be involved?</h2>
+            <h2>Equipment Only Requests</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud.
+              <Link href="/docs/Equipment-Only-Request-Form-v-180118-3.docx">
+                Please complete an Equipment Only Request Form
+              </Link>{" "}
+              if you, or the team working with you, are confident you know the
+              best AAC system. This means you do not need a full assessment from
+              Ace Centre.
             </p>
-            <h2>The venue</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud.
+              <Link href="/docs/Equipment-Only-Request-Form-Guidance-Notes-v181217.pdf">
+                Please read the guidance document
+              </Link>{" "}
+              for the form. It is expected that you will have trialled at least
+              three systems and gained enough clinical information to enable you
+              to complete all relevant fields on the form.
             </p>
-            <h2>Length of assessment</h2>
+            <h2>More Information</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud.
+              Ace Centre will acknowledge all referrals in writing within 10
+              days of receipt.
             </p>
-            <h2>What happens at the assessment?</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud.
+              The acknowledgment will be sent to the main contact on the
+              submitted form and will advise on eligibility for the requested
+              service or notify the contact that further information is
+              required.
             </p>
-            <h2>Following the assessment</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud.
+              Please note: If someone does not meet the eligibility criteria
+              they should contact the Ace Centre to find out the many other ways
+              we can offer support.
             </p>
-            <CardHighlight
+            <p>
+              Environmental control and computer access needs should be met by
+              the relevant commissioned service.{" "}
+              <Link href="http://www.communicationmatters.org.uk/page/aac-commissioning-england">
+                For information on those services please see here.
+              </Link>
+            </p>
+            <p>
+              If you have questions or difficulty downloading any of these
+              documents, please contact our free helpline on 0800 080 3115
+            </p>
+            {/* Commenting this out because its broken */}
+            {/* <CardHighlight
               title="Check where our services are available"
               description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
               viewText="Launch service checker"
               href="https://servicefinder.acecentre.net/"
-            />
+            /> */}
           </div>
           <div>
             <div className={styles.serviceProvidedByContainer}>
@@ -126,15 +167,13 @@ export default function NHSLanding({ featuredStory }) {
               </Avatar>
               <div className={styles.quoteText}>
                 <p className={styles.quoteContent}>
-                  &quot;Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                  sed do eiusmod consectetur adipiscing elit, sed do
-                  eiusmodtempor incididunt &quot;
+                  &quot;You addressed so many of the issues and concerns. Thank
+                  you so much for your expert knowledge and advice.&quot;
                 </p>
                 <div>
                   <p>
-                    <strong>Anna Reeves DL</strong>
+                    <strong>Lois, SaLT, West Berkshire</strong>
                   </p>
-                  <p>CEO</p>
                 </div>
               </div>
             </div>
@@ -147,14 +186,11 @@ export default function NHSLanding({ featuredStory }) {
                 <p>
                   <strong>NHS England referral form</strong>
                 </p>
-                <p>
-                  Ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                  eiusmod tem por incididunt olor sit.
-                </p>
+                <p>Get the latest Specialised AAC Services Referral Form</p>
                 <div className={styles.downloadFormButtonContainer}>
                   <Button
                     className={styles.downloadFormButton}
-                    href="https://acecentre.org.uk/wp-content/uploads/2021/02/NHS-England-Specialised-AAC-Services-Referral-Form-v3.2-2021.docx"
+                    href="/docs/NHS-England-Specialised-AAC-Services-Referral-Form-v3.2-2021.docx"
                   >
                     Download form
                   </Button>
