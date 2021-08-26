4b296f9621ba9cfce2ad7159da7f00ddb9ec8cef
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-08-26 16:59:52 +0100

    Try a full lighthouse run

---
+++ scripts/sitemap-to-lighthouse-list.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const path = require("path");
+const parser = require("fast-xml-parser");
+const config = require("../lib/config");
+
+const script = () => {
+  const sitemapPath = path.join(__dirname, "../out/sitemap.xml");
+  const sitemapAsString = fs.readFileSync(sitemapPath, "utf8").toString();
+  const sitemapObject = parser.parse(sitemapAsString);
+
+  const siteUrl = config.baseUrl;
+  const urls = sitemapObject.urlset.url.map((url) => {
+    return url.loc.replace(siteUrl, "");
+  });
+
+  return urls;
+};
+
+module.exports = script;
