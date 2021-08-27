0af5ec7059574f68770183c99cb29c9917a743a4
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-08-27 16:13:55 +0100

    Add seo tags

--- pages/learning/[slug].js
+++ pages/learning/[slug].js
@@ -99,6 +99,11 @@ export const getStaticProps = withGlobalProps(async ({ params: { slug } }) => {
       course: currentCourse,
       reviews,
       relatedCourses,
+      seo: {
+        title: "Ace Centre Learning",
+        description:
+          "Our courses focus on the use of Assistive Technology to enable independence, access to education, learning and leisure activities, and communication.",
+      },
     },
   };
 });
