67f89694249120c80f9333a1237d475e1c678403
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-08-27 08:57:45 +0100

    Add ignore list

--- scripts/sitemap-to-lighthouse-list.js
+++ scripts/sitemap-to-lighthouse-list.js
@@ -2,15 +2,27 @@ const fs = require("fs");
 const path = require("path");
 const parser = require("fast-xml-parser");
 
+const IGNORE = ["my-acecentre"];
+
 const script = () => {
   const sitemapPath = path.join(__dirname, "../out/sitemap.xml");
   const sitemapAsString = fs.readFileSync(sitemapPath, "utf8").toString();
   const sitemapObject = parser.parse(sitemapAsString);
 
   const siteUrl = "https://acecentre.org.uk";
-  const urls = sitemapObject.urlset.url.map((url) => {
-    return url.loc.replace(siteUrl, "");
-  });
+  const urls = sitemapObject.urlset.url
+    .map((url) => {
+      return url.loc.replace(siteUrl, "");
+    })
+    .filter((url) => {
+      for (const current of IGNORE) {
+        // console.log(current, url, current.includes);
+        if (url.includes(current)) {
+          return false;
+        }
+      }
+      return true;
+    });
 
   return urls;
 };
