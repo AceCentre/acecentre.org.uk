fc427f04652a2ae438bfd31f08d20f39e00cca60
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-08-27 11:00:12 +0100

    Format getting started landing page

--- components/ways-to-get-involved/ways-to-get-involved.js
+++ components/ways-to-get-involved/ways-to-get-involved.js
@@ -7,29 +7,35 @@ export const WaysToGetInvolved = () => {
       <div className={styles.container}>
         <h2 className={styles.title}>Ways to get involved</h2>
         <div className={styles.grid}>
-          <div>
+          <div className={styles.element}>
             <h3>Donate</h3>
             <p>
               Help to ensure we can continue to offer free information and
               helpline services to everyone who needs them
             </p>
-            <Button>Find out more</Button>
+            <div className={styles.buttonContainer}>
+              <Button href="/get-involved/donate">Find out more</Button>
+            </div>
           </div>
-          <div>
+          <div className={styles.element}>
             <h3>Fundraise</h3>
             <p>
               As a charity we depend on donations for vital services, from
               baking caked to running marathons, find out ways you can help.
             </p>
-            <Button>Find out more</Button>
+            <div className={styles.buttonContainer}>
+              <Button href="/get-involved/fundraise">Find out more</Button>
+            </div>
           </div>
-          <div>
+          <div className={styles.element}>
             <h3>Sponsor</h3>
             <p>
               Corporate sponsorship should not be one way, our aim is to work
               with the business community to the benefit of both parties.
             </p>
-            <Button>Find out more</Button>
+            <div className={styles.buttonContainer}>
+              <Button href="/get-involved/sponsor">Find out more</Button>
+            </div>
           </div>
         </div>
       </div>
