6cda5e02a4e1f7b19fe12b951338fbdb0ee1bffb
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-08-27 08:50:23 +0100

    Hardcode url

--- scripts/sitemap-to-lighthouse-list.js
+++ scripts/sitemap-to-lighthouse-list.js
@@ -1,14 +1,13 @@
 const fs = require("fs");
 const path = require("path");
 const parser = require("fast-xml-parser");
-const config = require("../lib/config");
 
 const script = () => {
   const sitemapPath = path.join(__dirname, "../out/sitemap.xml");
   const sitemapAsString = fs.readFileSync(sitemapPath, "utf8").toString();
   const sitemapObject = parser.parse(sitemapAsString);
 
-  const siteUrl = config.baseUrl;
+  const siteUrl = "https://acecentre.org.uk";
   const urls = sitemapObject.urlset.url.map((url) => {
     return url.loc.replace(siteUrl, "");
   });
