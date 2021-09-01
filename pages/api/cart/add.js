b4532607d7a6e2ca410356a405f339dd3bafde84
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-09-01 11:18:06 +0100

    Add quanitity option to add to cart

--- pages/api/cart/add.js
+++ pages/api/cart/add.js
@@ -5,12 +5,13 @@ async function handler(req, res) {
   const body = JSON.parse(req.body);
   const productId = body.productId;
   const variationId = body.variationId || null;
+  const quantity = body.quantity || 1;
 
   if (!productId) {
     return res.send({ success: false, error: "No product ID supplied" });
   }
 
-  const result = await addToCart(req, { productId, variationId });
+  const result = await addToCart(req, { productId, variationId, quantity });
 
   res.send({ success: true, result });
 }
