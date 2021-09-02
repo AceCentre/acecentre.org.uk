a2da9a2a6c899d5ab27cafdf3ab6e0695b4ad6b5
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-09-02 16:10:43 +0100

    Log request

--- pages/api/cart/add-to-cohort-background.js
+++ pages/api/cart/add-to-cohort-background.js
@@ -3,6 +3,8 @@ import withSession from "../../../lib/auth/with-session";
 const wait = (s) => new Promise((resolve) => setTimeout(resolve, s * 1000));
 
 async function handler(req, res) {
+  console.log(JSON.stringify({ req, res }));
+
   await wait(60);
   await res.send({ success: true });
 }
