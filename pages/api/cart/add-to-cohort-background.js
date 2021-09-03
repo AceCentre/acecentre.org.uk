befc220b47886995e8b91612953e37b229c388f7
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-09-03 09:36:20 +0100

    Shift adding users to a different background endpoint

--- pages/api/cart/add-to-cohort-background.js
+++ pages/api/cart/add-to-cohort-background.js
@@ -1,12 +1,44 @@
+import { gql } from "graphql-request";
 import withSession from "../../../lib/auth/with-session";
+import { clientRequest } from "../../../lib/client-request";
 
-const wait = (s) => new Promise((resolve) => setTimeout(resolve, s * 1000));
+const ADD_USERS_TO_COHORT = gql`
+  mutation AddUsersToCohort($cohortName: String, $newUsers: [NewCohortUsers]) {
+    addUsersToCohort(
+      input: { input: { cohortName: $cohortName, newUsers: $newUsers } }
+    ) {
+      success
+    }
+  }
+`;
 
 async function handler(req, res) {
-  console.log(JSON.stringify({ req, res }));
+  const body = JSON.parse(req.body);
+  const cohortNames = body.cohortNames;
 
-  await wait(60);
+  for (let current of cohortNames) {
+    const addUserResult = await addUserToCohort(
+      req,
+      current.cohortName,
+      body.groupPurchaseEmails[current.productId]
+    );
+    console.log(addUserResult);
+  }
+
   await res.send({ success: true });
 }
+
+const addUserToCohort = async (req, cohortName, emails) => {
+  const result = await clientRequest(req, ADD_USERS_TO_COHORT, {
+    cohortName,
+    newUsers: emails.map((email) => ({
+      email,
+      firstName: email.split(/@/)[0],
+      lastName: email.split(/@/)[1],
+    })),
+  });
+
+  return result;
+};
 
 export default withSession(handler);
