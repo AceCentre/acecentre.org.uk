1b5fc14f96882a51c5dfd7c5592e77b90d461542
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-09-15 12:45:19 +0100

    Add more free resource texts

--- cypress/integration/free-resource.spec.js
+++ cypress/integration/free-resource.spec.js
@@ -1,8 +1,44 @@
+import path from "path";
+
 context("Free resources", () => {
   it("can navigate to a free resource", () => {
     cy.visit("");
     cy.findAllByRole("link", { name: "Resources" }).first().click();
     cy.findAllByRole("link", { name: "View all resources" }).first().click();
-    cy.findAllByRole("combobox", { name: "Price range" });
+    cy.findAllByRole("combobox", { name: "Price range" }).select("Free");
+    cy.url({ timeout: 10000 }).should("include", "pricerange=0");
+    cy.findByRole("main")
+      .findAllByRole("list")
+      .first()
+      .children()
+      .first()
+      .click();
+
+    cy.findByRole("main").should("contain", "Free");
+  });
+
+  it("can download a simple resource", () => {
+    cy.visit(
+      "/resources/high-contrast-listener-mediated-scan-charts-abc-frequency"
+    );
+
+    // Cant test download due to https://github.com/cypress-io/cypress/issues/14857
+    // So I just prevent default on clicks, its a hack but does the job
+
+    cy.window()
+      .document()
+      .then(function (doc) {
+        doc.addEventListener("click", (event) => {
+          event.preventDefault();
+        });
+
+        cy.findByRole("link", { name: "Free download" }).click();
+        cy.findByRole("heading", { name: "Free download complete" }).should(
+          "exist"
+        );
+        cy.findAllByRole("textbox", {
+          name: "Email address for mailing list",
+        }).should("exist");
+      });
   });
 });
