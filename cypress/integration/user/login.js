15cf80fcb74cabbaa097ff3e50c9046ef4dba216
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-09-15 10:21:16 +0100

    Lots of login tests

--- cypress/integration/user/login.js
+++ cypress/integration/user/login.js
@@ -1,5 +1,7 @@
 const NO_ACC_EMAIL = "thisisnotanaccount@gavin.com";
 const ACCOUNT_EXISTS = "ghenderson@acecentre.org.uk";
+const TEST_ACC = "cypress@test.com";
+const TEST_PASS = "testpassword";
 
 context("Login", () => {
   it("disables the login button by default", () => {
@@ -72,5 +74,29 @@ context("Login", () => {
     cy.findByTestId("login-error", { timeout: 10000 }).should((error) => {
       expect(error).to.contain("Your email address or password is incorrect");
     });
+  });
+
+  it("successfully logs in to a valid account", () => {
+    cy.visit("");
+    cy.findAllByRole("link", { name: "My Ace Centre" }).first().click();
+    cy.url({ timeout: 10000 }).should("include", "/login");
+
+    cy.findAllByRole("form", { name: "Login form" })
+      .findByRole("textbox", { name: "Username or email address" })
+      .type(TEST_ACC);
+
+    cy.findAllByRole("form", { name: "Login form" })
+      .findByLabelText("Password")
+      .type(TEST_PASS);
+
+    cy.findAllByRole("form", { name: "Login form" })
+      .findByRole("button", { name: "Log in" })
+      .click();
+
+    cy.url({ timeout: 10000 }).should("include", "/my-acecentre");
+
+    cy.findByRole("link", { name: "Manage details >" }).click();
+    cy.url({ timeout: 10000 }).should("include", "/details");
+    cy.findByRole("textbox", { name: "Email" }).should("have.value", TEST_ACC);
   });
 });
