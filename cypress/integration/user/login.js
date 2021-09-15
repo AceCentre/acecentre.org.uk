fba4e14abbb29f3081cea861c53fae94aa3cc66e
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-09-15 10:15:51 +0100

    Add more login tests

--- cypress/integration/user/login.js
+++ cypress/integration/user/login.js
@@ -1,3 +1,6 @@
+const NO_ACC_EMAIL = "thisisnotanaccount@gavin.com";
+const ACCOUNT_EXISTS = "ghenderson@acecentre.org.uk";
+
 context("Login", () => {
   it("disables the login button by default", () => {
     cy.visit("");
@@ -25,5 +28,49 @@ context("Login", () => {
     cy.findAllByRole("form", { name: "Login form" })
       .findByRole("button", { name: "Log in" })
       .should("not.be.disabled");
+  });
+
+  it("shows an error when no account exists", () => {
+    cy.visit("");
+    cy.findAllByRole("link", { name: "My Ace Centre" }).first().click();
+    cy.url({ timeout: 10000 }).should("include", "/login");
+
+    cy.findAllByRole("form", { name: "Login form" })
+      .findByRole("textbox", { name: "Username or email address" })
+      .type(NO_ACC_EMAIL);
+
+    cy.findAllByRole("form", { name: "Login form" })
+      .findByLabelText("Password")
+      .type("password");
+
+    cy.findAllByRole("form", { name: "Login form" })
+      .findByRole("button", { name: "Log in" })
+      .click();
+
+    cy.findByTestId("login-error", { timeout: 10000 }).should((error) => {
+      expect(error).to.contain("Your email address or password is incorrect");
+    });
+  });
+
+  it("shows an error when password is wrong", () => {
+    cy.visit("");
+    cy.findAllByRole("link", { name: "My Ace Centre" }).first().click();
+    cy.url({ timeout: 10000 }).should("include", "/login");
+
+    cy.findAllByRole("form", { name: "Login form" })
+      .findByRole("textbox", { name: "Username or email address" })
+      .type(ACCOUNT_EXISTS);
+
+    cy.findAllByRole("form", { name: "Login form" })
+      .findByLabelText("Password")
+      .type("password");
+
+    cy.findAllByRole("form", { name: "Login form" })
+      .findByRole("button", { name: "Log in" })
+      .click();
+
+    cy.findByTestId("login-error", { timeout: 10000 }).should((error) => {
+      expect(error).to.contain("Your email address or password is incorrect");
+    });
   });
 });
