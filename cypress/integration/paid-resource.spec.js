9a7d55da0050f2deb2a63a23564eb19327904a12
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-09-16 15:56:28 +0100

    Add test for failed 3d auth

--- cypress/integration/paid-resource.spec.js
+++ cypress/integration/paid-resource.spec.js
@@ -140,6 +140,75 @@ context("Paid resources", () => {
     cy.url({ timeout: 20000 }).should("include", "order");
   });
 
+  it("can add product to cart, and checkout without an account, decline 3D Auth", () => {
+    cy.visit("/resources/simple-charts-to-edit-in-word");
+    cy.findByRole("button", { name: "Add to cart" }).click();
+    cy.url({ timeout: 10000 }).should("include", "basket");
+    cy.findAllByRole("row", { name: /Total/g }).should("contain", "£10.00");
+    cy.findAllByRole("link", { name: "Checkout" }).last().click();
+    cy.url({ timeout: 10000 }).should("include", "checkout");
+
+    cy.findByRole("textbox", { name: "First name" }).type("John");
+    cy.findByRole("textbox", { name: "Last name" }).type("Smith");
+    cy.findByRole("combobox", { name: "Country" }).select(
+      "United Kingdom (UK)"
+    );
+    cy.findByRole("textbox", { name: "Address Line 1" }).type(
+      "80 Fake Address"
+    );
+    cy.findByRole("textbox", { name: "Town / City" }).type("Manchester");
+    cy.findByRole("textbox", { name: "Postcode" }).type("OL8 3QL");
+    cy.findByRole("textbox", { name: "Phone number" }).type("07545783496");
+    cy.findByRole("textbox", { name: "Email address" }).type(validEmail());
+
+    cy.get('iframe[title="Secure card payment input frame"]')
+      .its("0.contentDocument")
+      .then(cy.wrap)
+      .findAllByRole("textbox", { name: "Credit or debit card number" })
+      .type("4000002500003155");
+
+    cy.get('iframe[title="Secure card payment input frame"]')
+      .its("0.contentDocument")
+      .then(cy.wrap)
+      .findAllByRole("textbox", {
+        name: "Credit or debit card expiration date",
+      })
+      .type("1224");
+
+    cy.get('iframe[title="Secure card payment input frame"]')
+      .its("0.contentDocument")
+      .then(cy.wrap)
+      .findAllByRole("textbox", { name: "Credit or debit card CVC/CVV" })
+      .type("123");
+
+    cy.findByRole("checkbox", {
+      name: "I have read and agree to the website terms and conditions",
+    }).click({ force: true });
+
+    cy.findByRole("button", { name: "Place order" }).click();
+
+    cy.wait(15000);
+
+    // Huge hack to get into the many iframes for 3D Auth
+    // Any update to iframe names from stripe will break this
+    cy.get('iframe[name*="privateStripeFrame"]')
+      .its("0.contentDocument")
+      .then(cy.wrap)
+      .findByRole("document")
+      .find("#challengeFrame")
+      .its("0.contentDocument")
+      .then(cy.wrap)
+      .findByRole("document")
+      .find('iframe[name="acsFrame"]')
+      .its("0.contentDocument")
+      .then(cy.wrap)
+      .findByRole("button", { name: "Fail authentication" })
+      .click();
+
+    cy.url({ timeout: 20000 }).should("include", "order");
+    cy.contains("Order failed");
+  });
+
   it("can add product to cart, and checkout with an account", () => {
     cy.visit("/resources/simple-charts-to-edit-in-word");
     cy.findByRole("button", { name: "Add to cart" }).click();
