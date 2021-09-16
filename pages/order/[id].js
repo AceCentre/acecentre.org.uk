bd2d64e736c344dc5dd2eae15712d8ba51ccf5d2
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-09-16 15:33:24 +0100

    Add error

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -1,44 +1,75 @@
-import { useEffect, useState } from "react";
+import Link from "next/link";
 import { CombinedNav } from "../../components/combined-nav/combined-nav";
 import { Footer } from "../../components/footer/footer";
 import { PageTitle } from "../../components/page-title/page-title";
 import { defaultNavItems } from "../../components/sub-nav/sub-nav-items";
 import { OrderDetailTable } from "../../components/table/table";
 import withSession from "../../lib/auth/with-session";
+import { getOrder } from "../../lib/cart/order";
 import { useGlobalProps } from "../../lib/global-props/hook";
 import style from "../../styles/order.module.css";
 
-export default function OrderPage({ orderId }) {
+export default function OrderPage({ orderId, order }) {
   const { currentYear } = useGlobalProps();
-
-  const [order, setOrder] = useState(null);
-
-  useEffect(() => {
-    setOrder(JSON.parse(localStorage.getItem(`order-${orderId}`)));
-  }, []);
 
   return (
     <>
       <header>
         <CombinedNav defaultNavItems={defaultNavItems} />
       </header>
-      <main>
-        <PageTitle heading="Order details" description={`Order #${orderId}`} />
-        {order && (
+      {order.status.toLowerCase() === "completed" ? (
+        <main>
+          <PageTitle
+            heading="Order details"
+            description={`Order #${orderId}`}
+          />
           <div className={style.tableContainer}>
             <OrderDetailTable order={order} />
           </div>
-        )}
-      </main>
+        </main>
+      ) : (
+        <>
+          <style jsx>{`
+            span {
+              font-size: 100px;
+            }
+
+            main {
+              text-align: center;
+              width: 90%;
+              margin: 0 auto;
+              max-width: 1024px;
+              padding: 6rem 0;
+            }
+
+            h1 {
+              font-weight: normal;
+            }
+          `}</style>
+          <main>
+            <span>Order failed</span>
+            <h1>An error occurred</h1>
+            <p>
+              An error occurred while processing your order, this could be due
+              to insufficient funds. If you need help{" "}
+              <Link href="/contact">
+                <a>Contact Us</a>
+              </Link>
+            </p>
+          </main>
+        </>
+      )}
       <Footer currentYear={currentYear} />
     </>
   );
 }
 
 export const getServerSideProps = withSession(
-  async ({ params: { id: orderId } }) => {
+  async ({ req, params: { id: orderId } }) => {
+    const order = await getOrder(req, orderId);
+
     return {
-      props: { orderId },
+      props: { order, orderId },
     };
   }
 );
