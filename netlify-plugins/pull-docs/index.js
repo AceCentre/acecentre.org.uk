8ca4482f8bc5039745fa8457972fce80f26beedd
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-09-28 12:34:01 +0100

    Basic scaffolding for plugin

--- netlify-plugins/pull-docs/index.js
+++ netlify-plugins/pull-docs/index.js
@@ -1,5 +1,17 @@
+const { remoteDocs } = require("../../remote-docs.json");
+
 module.exports = {
-  onPreBuild: async () => {
-    console.log("pull the docs");
+  onPreBuild: async ({ utils: { run } }) => {
+    for (const currentDocs of remoteDocs) {
+      console.log(
+        `Attempting to pull ${currentDocs.owner}/${currentDocs.repo}`
+      );
+
+      await run.command(
+        `git clone https://github.com/${currentDocs.owner}/${currentDocs.repo} ~/temp-${currentDocs.repo}`
+      );
+
+      console.log("Successfully pulled to", `~/temp-${currentDocs.repo}`);
+    }
   },
 };
