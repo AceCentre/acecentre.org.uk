d332d9ee7fa5f38ab73299015b2304157ca863a0
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-09-28 14:05:48 +0100

    Try building pasco docs

--- netlify-plugins/pull-docs/index.js
+++ netlify-plugins/pull-docs/index.js
@@ -1,17 +1,58 @@
 const { remoteDocs } = require("../../remote-docs.json");
+const path = require("path");
 
 module.exports = {
   onPreBuild: async ({ utils: { run } }) => {
+    console.log("================");
     for (const currentDocs of remoteDocs) {
       console.log(
         `Attempting to pull ${currentDocs.owner}/${currentDocs.repo}`
       );
 
-      await run.command(
-        `git clone https://github.com/${currentDocs.owner}/${currentDocs.repo} ~/temp-${currentDocs.repo}`
+      const cloneTarget = `~/temp-${currentDocs.repo}`;
+
+      try {
+        await run.command(
+          `git clone https://github.com/${currentDocs.owner}/${currentDocs.repo} ${cloneTarget}`
+        );
+      } catch (error) {
+        if (
+          error.stderr ===
+          "fatal: destination path '~/temp-pasco' already exists and is not an empty directory."
+        ) {
+          console.log("Repo already cloned");
+          console.log("Attempting to pull latest changes");
+          await run.command(`git -C ${cloneTarget} pull`);
+          console.log("Pulled changes");
+        } else {
+          throw error;
+        }
+      }
+
+      console.log(
+        "Successfully got latest version of repo in",
+        `~/temp-${currentDocs.repo}`
       );
 
-      console.log("Successfully pulled to", `~/temp-${currentDocs.repo}`);
+      const source = `~/temp-${currentDocs.repo}/${currentDocs.folder}`;
+      const target = path.join(
+        __dirname,
+        "../../pages/product-docs",
+        `./${currentDocs.urlPath}`
+      );
+
+      console.log("Attempting the target directory");
+
+      await run.command(`mkdir -p ${target}`);
+
+      console.log("Created directory");
+
+      console.log(`Attempting to copy ${source} to ${target}`);
+
+      await run.command(`cp -R ${source}/. ${target}`);
+
+      console.log(`Successfully copy ${source} to ${target}`);
+      console.log("================");
     }
   },
 };
