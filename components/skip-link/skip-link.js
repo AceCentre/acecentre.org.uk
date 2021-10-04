294e39c609ce4ca4f47e706ee0b0f2a88274c28e
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-10-04 16:51:39 +0100

    Fix skip link

--- components/skip-link/skip-link.js
+++ components/skip-link/skip-link.js
@@ -3,7 +3,7 @@ import styles from "./skip-link.module.css";
 export const SkipLink = () => {
   return (
     <a className={styles.skipLink} href="#mainContent">
-      Skip to main content
+      Skip to content
     </a>
   );
 };
