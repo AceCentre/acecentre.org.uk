212b0945c382b4180362c49048da87140968a4a8
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-10-08 12:10:56 +0100

    Fix tests

--- cypress/integration/moodle.spec.js
+++ cypress/integration/moodle.spec.js
@@ -1,6 +1,46 @@
 context("Moodle Login", () => {
-  it(["post-deploy"], "loads the webpage without any issues", () => {
-    cy.visit("/");
-    cy.get("#__next").should("exist");
-  });
+  it(
+    ["post-deploy"],
+    "Logs into moodle, also logs you into AceCentre, then logs out of moodle and webpage",
+    () => {
+      // Ignore errors from theme boost on moodle because we dont control it
+      Cypress.on("uncaught:exception", (err) => {
+        if (err.message.includes("theme_boost")) return false;
+        return true;
+      });
+
+      // Go to login page
+      cy.visit("https://learning.acecentre.org.uk");
+      cy.findByRole("link", { name: "Log in" }).click();
+
+      // Entre login credentials
+      cy.findByRole("textbox", { name: "Username" }).type(
+        "fri08@gavinhenderson.co.uk"
+      );
+      cy.findByLabelText("Password").type("password");
+      cy.findByRole("button", { name: "Log in" }).click();
+
+      // Confirm we are logged in to moodle
+      cy.url({ timeout: 10000 }).should("include", "learning.acecentre.org.uk");
+      cy.findByRole("button", { name: "User menu" }).click();
+      cy.findByRole("menuitem", { name: "Dashboard" }).click();
+
+      // Check we are logged in to ace centre
+      cy.visit("https://acecentre.org.uk");
+      cy.findAllByRole("link", { name: "My Ace Centre" }).first().click();
+      cy.url({ timeout: 10000 }).should("include", "/my-acecentre");
+
+      // Go back to learning and logout and make sure we are logged out
+      cy.visit("https://learning.acecentre.org.uk");
+      cy.url({ timeout: 10000 }).should("include", "learning.acecentre.org.uk");
+      cy.findByRole("button", { name: "User menu" }).click();
+      cy.findByRole("menuitem", { name: "Log out" }).click();
+      cy.findByRole("link", { name: "Log in" }).should("exist");
+
+      // Check we are logged out of the AceCentre site
+      cy.visit("https://acecentre.org.uk");
+      cy.findAllByRole("link", { name: "My Ace Centre" }).first().click();
+      cy.url({ timeout: 10000 }).should("include", "/login");
+    }
+  );
 });
