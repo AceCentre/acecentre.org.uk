f8a5a1f5425e46c8d8af1c43064505c32c274a82
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-10-11 15:49:52 +0100

    Add another ignore

--- lighthouserc.js
+++ lighthouserc.js
@@ -106,6 +106,9 @@ module.exports = {
         "unminified-css": "off",
         "uses-text-compression": "off",
 
+        // Posthog does this
+        "no-unload-listeners": "off",
+
         ...getExtraAssertions(),
       },
     },
