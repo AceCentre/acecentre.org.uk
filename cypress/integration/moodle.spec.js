e80d67da52f653c444b7ac9310dad145857be48a
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-01-05 09:17:26 +0000

    Add a new test to check user is enrolled on a course

--- cypress/integration/moodle.spec.js
+++ cypress/integration/moodle.spec.js
@@ -1,4 +1,53 @@
-context("Moodle Login", () => {
+let couponCode = null;
+let couponId = null;
+
+before(async () => {
+  const response = await fetch(
+    "https://backend.acecentre.org.uk/wp-json/wc/v3/coupons",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Basic ",
+      },
+      body: JSON.stringify({
+        code: "cypress_testing_code",
+        discount_type: "percent",
+        amount: "100",
+        usage_limit: 1,
+        description: "Created automatically for Cypress tests",
+      }),
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to create coupon");
+  }
+
+  const result = await response.json();
+
+  couponCode = result.code;
+  couponId = result.id;
+});
+
+after(async () => {
+  const response = await fetch(
+    `https://backend.acecentre.org.uk/wp-json/wc/v3/coupons/${couponId}?force=true`,
+    {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Basic ",
+      },
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to delete coupon");
+  }
+});
+
+context("Moodle", () => {
   it(
     ["post-deploy"],
     "Logs into moodle, also logs you into AceCentre, then logs out of moodle and webpage",
@@ -39,6 +88,29 @@ context("Moodle Login", () => {
       cy.visit("https://acecentre.org.uk");
       cy.findAllByRole("link", { name: "My Ace Centre" }).first().click();
       cy.url({ timeout: 10000 }).should("include", "/login");
+    }
+  );
+
+  it.only(
+    ["post-deploy"],
+    "Buy a course for a new user, check they are enrolled on the course",
+    () => {
+      // Visit splash
+      cy.visit("https://acecentre.org.uk/learning/splash-training-i");
+
+      // Add to basket
+      cy.findByRole("button", { name: "Book this course" }).click();
+      cy.findAllByRole("button", { name: "Book this course" }).last().click();
+
+      // Check we are on the basket page
+      cy.url({ timeout: 10000 }).should("include", "/basket");
+
+      // Apply voucher
+      cy.findByRole("textbox", { name: "Voucher" }).type(couponCode);
+      cy.findByRole("button", { name: "Apply Voucher" }).click();
+
+      // Check we are on the basket page
+      cy.url({ timeout: 10000 }).should("include", "/basket");
     }
   );
 });
