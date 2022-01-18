15bb8d59d60c758600d44bcf2b300c272357638a
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-01-18 14:56:38 +0000

    Still have to export vars

--- .netlify/functions-internal/add-to-cohort.js
+++ .netlify/functions-internal/add-to-cohort.js
@@ -2,6 +2,9 @@ import { gql, GraphQLClient } from "graphql-request";
 import withSession from "../../lib/auth/with-session";
 import { App } from "@slack/bolt";
 import TurndownService from "turndown";
+import exportEnv from "../../envs";
+
+exportEnv();
 
 const ENDPOINT = "https://backend.acecentre.org.uk/graphql";
 
