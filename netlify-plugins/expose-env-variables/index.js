0a4e1edae141c3b42cd06862702a1b3c61994390
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-01-18 14:15:05 +0000

    Undo changes in expose vars

--- netlify-plugins/expose-env-variables/index.js
+++ netlify-plugins/expose-env-variables/index.js
@@ -18,20 +18,8 @@ module.exports = {
       "POSTHOG_KEY",
     ];
 
-    console.log("Exposing vars", varToExpose);
-
     const pathToEnv = path.join(process.cwd(), "./.env");
     const pathToJs = path.join(process.cwd(), "./envs.js");
-
-    const pathToBackgroundFunc = path.join(
-      process.cwd(),
-      "./.netlify/functions-internal/add-to-cohort.js"
-    );
-
-    const fileAsString = fs
-      .readFileSync(pathToBackgroundFunc, "utf8")
-      .toString();
-    let newBlock = "";
 
     fs.appendFileSync(pathToEnv, "\nNETLIFY=true\n");
     fs.appendFileSync(
@@ -39,26 +27,17 @@ module.exports = {
       "const exportEnvs = () => {\nprocess.env.NETLIFY=true\n"
     );
 
-    console.log({ pathToEnv, pathToJs, pathToBackgroundFunc, fileAsString });
-
     for (const varName of varToExpose) {
-      console.log(`Exposing ${varName} as ${process.env[varName]}`);
       fs.appendFileSync(pathToEnv, `${varName}=${process.env[varName]}\n`);
       fs.appendFileSync(
         pathToJs,
         `process.env.${varName}='${process.env[varName]}'\n`
       );
-
-      newBlock += `process.env.${varName}='${process.env[varName]}';\n`;
     }
 
     fs.appendFileSync(
       pathToJs,
       "\n}\nexportEnvs();\nexport default exportEnvs\n"
     );
-
-    console.log(newBlock + fileAsString);
-
-    fs.writeFileSync(pathToBackgroundFunc, newBlock + fileAsString);
   },
 };
