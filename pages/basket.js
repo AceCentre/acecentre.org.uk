87864c79bc5be5d7cc604a82250e3a9a0332ef66
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-01-18 14:34:56 +0000

    Skip basket

--- pages/basket.js
+++ pages/basket.js
@@ -4,39 +4,43 @@ import withSession from "../lib/auth/with-session"; // import { getCart } from "
 import { defaultNavItems } from "../components/sub-nav/sub-nav-items";
 import { getCart } from "../lib/cart/get";
 import { useGlobalProps } from "../lib/global-props/hook";
-// import { Button } from "../components/button/button";
-// import { CouponArea } from "../components/coupon-area/coupon-area";
-// import { PageTitle } from "../components/page-title/page-title";
-// import { BasketTable, TotalsTable } from "../components/table/table";
-// import { useCoupon } from "../lib/use-coupon";
-// import { useUpdateCart } from "../lib/use-update-cart";
+import { Button } from "../components/button/button";
+import { CouponArea } from "../components/coupon-area/coupon-area";
+import { PageTitle } from "../components/page-title/page-title";
+import { BasketTable, TotalsTable } from "../components/table/table";
+import { useCoupon } from "../lib/use-coupon";
+import { useUpdateCart } from "../lib/use-update-cart";
 
 import styles from "../styles/basket.module.css";
+import { useRouter } from "next/router";
 
-// lines,
-// subtotal,
-// shipping,
-// total,
-// discountTotal,
-// needsDelivered,
-// vat,
-// isLoggedIn,
-export default function Basket() {
+export default function Basket({
+  lines,
+  subtotal,
+  shipping,
+  total,
+  discountTotal,
+  needsDelivered,
+  vat,
+  isLoggedIn,
+}) {
   const { currentYear } = useGlobalProps();
 
-  // const {
-  //   onQuantityChange,
-  //   sendUpdate,
-  //   updateButtonDisabled,
-  //   error: updateCartError,
-  // } = useUpdateCart(lines);
+  const {
+    onQuantityChange,
+    sendUpdate,
+    updateButtonDisabled,
+    error: updateCartError,
+  } = useUpdateCart(lines);
 
-  // const {
-  //   applyCoupon,
-  //   onCouponChange,
-  //   isApplyVoucherDisabled,
-  //   error: couponError,
-  // } = useCoupon();
+  const {
+    applyCoupon,
+    onCouponChange,
+    isApplyVoucherDisabled,
+    error: couponError,
+  } = useCoupon();
+
+  const { query } = useRouter();
 
   return (
     <>
@@ -44,57 +48,64 @@ export default function Basket() {
         <CombinedNav defaultNavItems={defaultNavItems} />
       </header>
       <main id="mainContent">
-        <div className={styles.emptyCart}>
-          <h1>Shop is currently disabled</h1>
-          <p>
-            We have currently disabled our shop due to technical issues. Please
-            try again tomorrow.
-          </p>
-        </div>
-
-        {/* {lines.length > 0 ? (
+        {query.skip ? (
           <>
-            <PageTitle
-              heading="Checkout"
-              description="Here's a summary of your order"
-            />
-            <BasketTable onQuantityChange={onQuantityChange} lines={lines} />
-            <div className={styles.rightAlign}>
-              {updateCartError && (
-                <p className={styles.error}>{updateCartError}</p>
-              )}
-              <Button disabled={updateButtonDisabled} onClick={sendUpdate}>
-                Update quantities
-              </Button>
-            </div>
-            <TotalsTable
-              needsDelivered={needsDelivered}
-              discountTotal={discountTotal}
-              subtotal={subtotal}
-              total={total}
-              shipping={shipping}
-              vat={vat}
-            />
-            <CouponArea
-              isApplyVoucherDisabled={isApplyVoucherDisabled}
-              applyCoupon={applyCoupon}
-              onCouponChange={onCouponChange}
-              error={couponError}
-              isLoggedIn={isLoggedIn}
-            />
+            {lines.length > 0 ? (
+              <>
+                <PageTitle
+                  heading="Checkout"
+                  description="Here's a summary of your order"
+                />
+                <BasketTable
+                  onQuantityChange={onQuantityChange}
+                  lines={lines}
+                />
+                <div className={styles.rightAlign}>
+                  {updateCartError && (
+                    <p className={styles.error}>{updateCartError}</p>
+                  )}
+                  <Button disabled={updateButtonDisabled} onClick={sendUpdate}>
+                    Update quantities
+                  </Button>
+                </div>
+                <TotalsTable
+                  needsDelivered={needsDelivered}
+                  discountTotal={discountTotal}
+                  subtotal={subtotal}
+                  total={total}
+                  shipping={shipping}
+                  vat={vat}
+                />
+                <CouponArea
+                  isApplyVoucherDisabled={isApplyVoucherDisabled}
+                  applyCoupon={applyCoupon}
+                  onCouponChange={onCouponChange}
+                  error={couponError}
+                  isLoggedIn={isLoggedIn}
+                />
+              </>
+            ) : (
+              <div className={styles.emptyCart}>
+                <h1>Your basket is currently empty</h1>
+                <p>
+                  Add some resources or courses to your basket then come back to
+                  checkout
+                </p>
+                <div className={styles.allResourcesButton}>
+                  <Button href="/resources">See our resources</Button>
+                </div>
+              </div>
+            )}{" "}
           </>
         ) : (
           <div className={styles.emptyCart}>
-            <h1>Your basket is currently empty</h1>
+            <h1>Shop is currently disabled</h1>
             <p>
-              Add some resources or courses to your basket then come back to
-              checkout
+              We have currently disabled our shop due to technical issues.
+              Please try again tomorrow.
             </p>
-            <div className={styles.allResourcesButton}>
-              <Button href="/resources">See our resources</Button>
-            </div>
           </div>
-        )} */}
+        )}
       </main>
       <Footer currentYear={currentYear} />
     </>
