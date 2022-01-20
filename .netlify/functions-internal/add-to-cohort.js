5461ff63ec179735830a7a2cf5b40b8d9b5f5921
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-01-20 15:17:59 +0000

    Import request

--- .netlify/functions-internal/add-to-cohort.js
+++ .netlify/functions-internal/add-to-cohort.js
@@ -1,4 +1,4 @@
-import { gql, GraphQLClient } from "graphql-request";
+import { gql, GraphQLClient, request } from "graphql-request";
 import withSession from "../../lib/auth/with-session";
 import { App } from "@slack/bolt";
 import TurndownService from "turndown";
