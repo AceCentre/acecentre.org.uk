03f2be4a8f705a07536b637c9fe556717b8f3641
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-02-07 10:19:48 +0000

    Try some cloudinary magic

---
+++ scripts/calculate-cloudinary-cloud.js
@@ -0,0 +1,26 @@
+const got = require("got");
+
+const script = async () => {
+  const cloudOne = await got(
+    `https://${process.env.CLOUDINARY_ONE_KEY}:${process.env.CLOUDINARY_ONE_SECRET}@api.cloudinary.com/v1_1/ace-cloud/usage`
+  );
+  const cloudTwo = await got(
+    `https://${process.env.CLOUDINARY_TWO_KEY}:${process.env.CLOUDINARY_TWO_SECRET}@api.cloudinary.com/v1_1/ace-cloud-2/usage`
+  );
+
+  const cloudOneResult = JSON.parse(cloudOne.body);
+  const cloudTwoResult = JSON.parse(cloudTwo.body);
+
+  const cloudOneUsage = cloudOneResult.credits.used_percent;
+  const cloudTwoUsage = cloudTwoResult.credits.used_percent;
+
+  console.log({ cloudOneUsage, cloudTwoUsage });
+
+  if (cloudOneUsage < cloudTwoUsage) {
+    return "ace-cloud";
+  } else {
+    return "ace-cloud-2";
+  }
+};
+
+module.export = script;
