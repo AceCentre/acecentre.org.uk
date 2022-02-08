b6f638dfcf5b26f43479f8dbb69c597572b93e67
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-02-08 10:12:30 +0000

    Remove job opportunties

--- components/jobs-and-people/jobs-and-people.js
+++ components/jobs-and-people/jobs-and-people.js
@@ -6,28 +6,6 @@ import { Button } from "../button/button";
 export const JobsAndPeople = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.jobImage}>
-        <Image
-          src="/job-opportunities.jpeg"
-          layout="fill"
-          objectFit="cover"
-          objectPosition="top"
-          alt="Staff member on taking a phone call"
-        />
-        <div className={styles.blueBackground}></div>
-      </div>
-      <div className={styles.jobDescription}>
-        <h2>Job opportunities</h2>
-        <p>
-          If you are smart, interested in making a difference, and want to work
-          with an awesome team and just as amazing technology then get in touch
-        </p>
-        <div className={styles.buttonContainer}>
-          <Button href="https://uk.indeed.com/cmp/Ace-Centre-3">
-            View jobs on indeed.co.uk
-          </Button>
-        </div>
-      </div>
       <div className={styles.peopleImage}>
         <Image
           src="/meet-our-people.jpeg"
