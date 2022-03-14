8e8bfe7ffa571257f5f0878556dac15acfaeda47
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-03-14 12:25:15 +0000

    Make resource image a priority

--- components/resources-image/resources-image.js
+++ components/resources-image/resources-image.js
@@ -1,7 +1,7 @@
 import { ImageWithLoader as Image } from "../image";
 import styles from "./resources-image.module.css";
 
-export const ResourcesImage = ({ resource }) => {
+export const ResourcesImage = ({ resource, priority }) => {
   if (!resource.image) return null;
 
   const alt = resource?.image?.alt || `Featured image of: ${resource.name}`;
@@ -13,6 +13,7 @@ export const ResourcesImage = ({ resource }) => {
         alt={alt}
         layout="fill"
         objectFit={"contain"}
+        priority={priority}
       />
     </div>
   );
