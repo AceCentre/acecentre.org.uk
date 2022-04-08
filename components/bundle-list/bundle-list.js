bdb341a492bdc1471279fd1042ec47f7c72deace
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-04-08 15:28:34 +0100

    Add basic bundle page

---
+++ components/bundle-list/bundle-list.js
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import styles from "./bundle-list.module.css";
+
+export const BundleList = ({ bundle }) => {
+  console.log(bundle);
+
+  return (
+    <div className={styles.container}>
+      <h2>Included courses:</h2>
+      <ul className={styles.list}>
+        {bundle.courses.map((course) => (
+          <BundleItem key={course.slug} course={course}></BundleItem>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const BundleItem = ({ course }) => {
+  return (
+    <li className={styles.inlineCard}>
+      <h3>{course.title}</h3>
+      <p>{course.shortDescription}</p>
+      <Link href={`/learning/${course.slug}`}>
+        <a className={styles.link}>View course &gt;</a>
+      </Link>
+    </li>
+  );
+};
