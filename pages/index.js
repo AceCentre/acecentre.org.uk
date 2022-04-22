034fc2d5d4a48458f6698f56a17869464c8c7811
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-04-22 16:53:55 +0100

    Put olive on the homepage

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,7 @@ export default function Home({ featuredStory, landingPagePosts }) {
 }
 
 export const getStaticProps = withGlobalProps(async () => {
-  const featuredStory = await getSimpleStory("paul-pickford");
+  const featuredStory = await getSimpleStory("olive");
 
   if (!featuredStory) throw new Error("Could not fetch story for landing page");
 
