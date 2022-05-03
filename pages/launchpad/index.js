21a67d396ea747cd330bb79b774e853ccc8b48e4
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-05-03 17:03:41 +0100

    Setup launchpad landing page

--- pages/launchpad/index.js
+++ pages/launchpad/index.js
@@ -1,10 +1,14 @@
+import Link from "next/link";
 import { CombinedNav } from "../../components/combined-nav/combined-nav";
 import { Footer } from "../../components/footer/footer";
 import { defaultNavItems } from "../../components/sub-nav/sub-nav";
 import { useGlobalProps } from "../../lib/global-props/hook";
 import { withGlobalProps } from "../../lib/global-props/inject";
+import { getTemplates } from "../../lib/launchpad";
 
-export default function Resources() {
+import styles from "../../styles/launchpad.module.css";
+
+export default function Resources({ templates }) {
   const { currentYear } = useGlobalProps();
 
   return (
@@ -12,15 +16,33 @@ export default function Resources() {
       <header>
         <CombinedNav defaultNavItems={defaultNavItems} />
       </header>
-      <main id="mainContent"></main>
+      <main className={styles.main} id="mainContent">
+        <h1>AAC Launchpad</h1>
+        <p>This is an early prototype of AAC Launchpad.</p>
+        <p>Here are the available dynamic templates:</p>
+        <ul>
+          {templates.map((template) => {
+            return (
+              <li key={template.templateId}>
+                <Link href={`/launchpad/${template.templateId}`}>
+                  {template.templateName}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </main>
       <Footer currentYear={currentYear} />
     </>
   );
 }
 
 export const getStaticProps = withGlobalProps(async () => {
+  const templates = await getTemplates();
+
   return {
     props: {
+      templates,
       seo: {
         dontIndex: true,
       },
