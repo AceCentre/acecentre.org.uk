2f87ee8559d453706fac3f5ecae6082648900468
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-05-04 11:14:20 +0100

    Put together basic launchpad page

--- pages/launchpad/[slug].js
+++ pages/launchpad/[slug].js
@@ -4,18 +4,45 @@ import { useGlobalProps } from "../../lib/global-props/hook";
 import { withGlobalProps } from "../../lib/global-props/inject";
 import { CombinedNav } from "../../components/combined-nav/combined-nav";
 import { getTemplates } from "../../lib/launchpad";
+import { LaunchpadList } from "../../components/resource-list/resource-list";
 
-export default function LaunchpadDetail({ template }) {
+import styles from "../../styles/launchpad.module.css";
+import { BackToLink } from "../../components/back-to-link/back-to-link";
+import { ResourcesImage } from "../../components/resources-image/resources-image";
+import { ResourcesDescription } from "../../components/resources-description/resources-description";
+import { ResourcesShare } from "../../components/resources-share/resources-share";
+
+export default function LaunchpadDetail({ template, templates }) {
   const { currentYear } = useGlobalProps();
-
-  console.log(template);
 
   return (
     <>
       <header>
         <CombinedNav defaultNavItems={defaultNavItems} />
       </header>
-      <main id="mainContent"></main>
+      <main id="mainContent">
+        <BackToLink where="launchpad" href="/launchpad" />
+        <div className={styles.topArea}>
+          <div className={styles.leftTopArea}>
+            <ResourcesImage
+              resource={{ image: { src: template.templateImageUrl } }}
+              priority
+            />
+          </div>
+          <div className={styles.rightTopArea}>
+            <ResourcesDescription
+              resource={{
+                name: template.templateName,
+                shortDescription: template.templateDescription,
+              }}
+            />
+          </div>
+        </div>
+        <div className={styles.share}>
+          <ResourcesShare />
+        </div>
+        <LaunchpadList title="Other templates" templates={templates} />
+      </main>
       <Footer currentYear={currentYear} />
     </>
   );
@@ -38,9 +65,12 @@ export const getStaticProps = withGlobalProps(async ({ params: { slug } }) => {
   const templates = await getTemplates();
   const template = templates.find((template) => template.templateId === slug);
 
+  const otherTemplates = templates.filter((x) => x.templateId !== slug);
+
   return {
     props: {
       template,
+      templates: otherTemplates,
       seo: {
         noIndex: true,
       },
