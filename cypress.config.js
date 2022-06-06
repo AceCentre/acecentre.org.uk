3260aab29a5459ec0e87443648687286972f0930
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-06-06 09:34:15 +0100

    Fix linting

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  screenshotsFolder: './artifacts/screenshots',
-  videosFolder: './artifacts/videos',
-  projectId: '2da7p6',
+  screenshotsFolder: "./artifacts/screenshots",
+  videosFolder: "./artifacts/videos",
+  projectId: "2da7p6",
   retries: 3,
   chromeWebSecurity: false,
   pageLoadTimeout: 60000,
@@ -14,9 +14,9 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return require("./cypress/plugins/index.js")(on, config);
     },
-    baseUrl: 'http://localhost:3000',
-    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    baseUrl: "http://localhost:3000",
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
   },
-})
+});
