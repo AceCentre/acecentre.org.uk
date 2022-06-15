9b0d226a141e84fee74527c482e31cb1e9fab4f9
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-06-15 16:51:19 +0100

    Delete account

--- cypress/e2e/checkout.spec.js
+++ cypress/e2e/checkout.spec.js
@@ -1,17 +1,29 @@
+import { deleteUser } from "../support/delete-user";
+import { validEmail } from "../support/valid-email";
+
 context("Checkout", () => {
+  let newEmail;
+
+  beforeEach(() => {
+    newEmail = null;
+  });
+
+  afterEach(async () => {
+    await deleteUser(newEmail, "internal");
+  });
+
   it(
     ["pre-deploy"],
     "Buys a single paid virtual product, on an existing account",
     () => {
       // Register
-      const randomNumber = Math.floor(Math.random() * 9999999);
-      const email = `${randomNumber}@test.com`;
+      newEmail = validEmail();
       const password = "password";
       cy.visit("/login");
 
       cy.wait(20000);
 
-      cy.get("#email").type(email);
+      cy.get("#email").type(newEmail);
       cy.get("#register-password").type(password);
       cy.findByRole("button", { name: "Register" }).click();
       cy.url({ timeout: 30000 }).should("include", "my-acecentre");
