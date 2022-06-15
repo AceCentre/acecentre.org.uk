2d1f5255eed236bd726cfde4bfd795ede98db31b
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-06-15 14:25:07 +0100

    Move coupon code

---
+++ cypress/support/coupon.js
@@ -0,0 +1,68 @@
+export const deleteCoupon = async (couponId) => {
+  const response = await fetch(
+    `https://backend.acecentre.org.uk/wp-json/wc/v3/coupons/${couponId}?force=true`,
+    {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Basic ${Cypress.env("WORDPRESS_AUTH")}`,
+      },
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to delete coupon");
+  }
+};
+
+export const createCoupon = async () => {
+  const allCouponsResponse = await fetch(
+    "https://backend.acecentre.org.uk/wp-json/wc/v3/coupons?code=cypress_testing_code",
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Basic ${Cypress.env("WORDPRESS_AUTH")}`,
+      },
+    }
+  );
+
+  if (!allCouponsResponse.ok) {
+    throw new Error("Failed to create coupon");
+  }
+
+  const allCouponsResult = await allCouponsResponse.json();
+
+  if (allCouponsResult.length === 1) {
+    await deleteCoupon(allCouponsResult[0].id);
+  }
+
+  const response = await fetch(
+    "https://backend.acecentre.org.uk/wp-json/wc/v3/coupons",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Basic ${Cypress.env("WORDPRESS_AUTH")}`,
+      },
+      body: JSON.stringify({
+        code: "cypress_testing_code",
+        discount_type: "percent",
+        amount: "100",
+        usage_limit: 1,
+        description: "Created automatically for Cypress tests",
+      }),
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to create coupon");
+  }
+
+  const result = await response.json();
+
+  const couponCode = result.code;
+  const couponId = result.id;
+
+  return { couponCode, couponId };
+};
