f5d9e539d1f9c024d994642ada8f28ddacfcf5d1
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-06-15 14:32:49 +0100

    Move change password

--- cypress/e2e/bundle-courses.spec.js
+++ cypress/e2e/bundle-courses.spec.js
@@ -1,76 +1,9 @@
+import { changePassword } from "../support/change-password";
 import { createCoupon, deleteCoupon } from "../support/coupon";
 import { deleteUser } from "../support/delete-user";
 import { validEmail } from "../support/valid-email";
 
 const VALID_PASSWORD = "securepassword";
-
-const changePassword = async (email, password) => {
-  const getUserResponse = await fetch(
-    "https://backend.acecentre.org.uk/index.php?graphql",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Basic ${Cypress.env("WORDPRESS_AUTH")}`,
-      },
-      body: JSON.stringify({
-        query: "query($email:ID!) {user(id: $email,idType:EMAIL) {id}}",
-        variables: { email },
-      }),
-    }
-  );
-
-  if (!getUserResponse.ok) {
-    throw new Error("Failed to get user");
-  }
-
-  const getUserResult = await getUserResponse.json();
-
-  if (
-    !getUserResult ||
-    !getUserResult.data ||
-    !getUserResult.data.user ||
-    !getUserResult.data.user.id
-  ) {
-    throw new Error("Failed to get user");
-  }
-
-  const id = getUserResult.data.user.id;
-
-  const getChangePasswordResponse = await fetch(
-    "https://backend.acecentre.org.uk/index.php?graphql",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Basic ${Cypress.env("WORDPRESS_AUTH")}`,
-      },
-      body: JSON.stringify({
-        query:
-          "mutation changePassword($id: ID!, $password: String) { updateUser(input: {id: $id, password: $password}) { user { id } } }",
-        variables: { id, password },
-      }),
-    }
-  );
-
-  if (!getChangePasswordResponse.ok) {
-    throw new Error("Failed to change password");
-  }
-
-  const changePasswordResult = await getChangePasswordResponse.json();
-
-  if (
-    !changePasswordResult ||
-    !changePasswordResult.data ||
-    !changePasswordResult.data.updateUser ||
-    !changePasswordResult.data.updateUser.user ||
-    !changePasswordResult.data.updateUser.user.id
-  ) {
-    throw new Error("Failed to change password");
-  }
-
-  return null;
-};
 
 Cypress.Commands.add("changePassword", async (username, password) => {
   await changePassword(username, password);
