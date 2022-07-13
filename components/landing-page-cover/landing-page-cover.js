8af3e47b9f2c4e01e03bedfcc76781d8fb5c2a16
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-07-13 15:44:16 +0100

    Switch to video in banner

--- components/landing-page-cover/landing-page-cover.js
+++ components/landing-page-cover/landing-page-cover.js
@@ -4,9 +4,24 @@ import PlayArrowIcon from "@material-ui/icons/PlayArrow";
 import { useState } from "react";
 import { VideoPopover } from "../video-popover/video-popover";
 import styles from "./landing-page-cover.module.css";
-import { ImageWithLoader as Image } from "../image";
+import { imageLoaders } from "../../lib/config";
+
+import {
+  cloudinaryLoader,
+  imageKitLoader,
+  keyLoader,
+  rawLoader,
+} from "../../lib/image-loader";
+
+const loaders = {
+  cloudinaryLoader,
+  imageKitLoader,
+  keyLoader,
+  rawLoader,
+};
 
 const VIDEO_URL = "https://www.youtube.com/watch?v=cSLZUBqlB04";
+const normalLoader = loaders[imageLoaders.normal];
 
 export const LandingPageCover = () => {
   const [isPopoverOpen, setIsPopoverOpen] = useState(false);
@@ -14,36 +29,38 @@ export const LandingPageCover = () => {
   return (
     <>
       <div>
-        <div className={styles.imageContainer}>
-          <Image
-            className={styles.image}
-            objectFit="cover"
-            layout="fill"
-            src="/landing-page-cover.jpeg"
-            alt="Ace Centre engineer fitting an AAC device"
-          />
+        <div>
+          <div className={styles.buttonContainer}>
+            <Button
+              variant="unstyled"
+              onClick={() => setIsPopoverOpen(true)}
+              className={styles.ctaButton}
+              aria-label="Play video about Ace Centre"
+            >
+              <Avatar className={styles.ctaAvatar}>
+                <PlayArrowIcon className={styles.ctaIcon} />
+              </Avatar>
+            </Button>
+          </div>
+          <div className={styles.imageContainer}>
+            <video
+              className={styles.image}
+              src="./banner-video.mp4"
+              autoPlay
+              muted
+              loop
+              poster={normalLoader({ src: "./video-placeholder.jpeg" })}
+              title="Ace Centre promotional video"
+            ></video>
+          </div>
         </div>
+
         <div className={styles.card}>
           <p className={styles.tagLine}>
             We work with people of all ages who use or need{" "}
             <strong>Augmentative and Alternative Communication </strong>
             (AAC) and <strong>Assistive Technology</strong> (AT) to communicate
           </p>
-          {VIDEO_URL && (
-            <>
-              <p className={styles.ctaText}>Watch our video</p>
-              <Button
-                variant="unstyled"
-                onClick={() => setIsPopoverOpen(true)}
-                className={styles.ctaButton}
-                aria-label="Play video about Ace Centre"
-              >
-                <Avatar className={styles.ctaAvatar}>
-                  <PlayArrowIcon className={styles.ctaIcon} />
-                </Avatar>
-              </Button>
-            </>
-          )}
         </div>
       </div>
       {VIDEO_URL && (
