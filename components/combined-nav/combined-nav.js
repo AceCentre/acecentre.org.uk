676cd8f16ef18a989b686a5ebee738d18c0f5cc3
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-09-14 09:25:55 +0100

    Add queen banner

--- components/combined-nav/combined-nav.js
+++ components/combined-nav/combined-nav.js
@@ -18,6 +18,7 @@ import { Button } from "../button/button";
 import { Input } from "../input/input";
 import { useGlobalProps } from "../../lib/global-props/hook";
 import { OldBrowserBanner } from "../old-browser-banner/old-browser-banner";
+import { TopBanner } from "../top-banner/top-banner";
 
 const useMobileNav = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -80,6 +81,7 @@ export const CombinedNav = ({
 
   return (
     <>
+      <TopBanner />
       <OldBrowserBanner />
       <div
         className={`${styles.desktopContainer} ${
---
+++ components/top-banner/top-banner.js
@@ -0,0 +1,13 @@
+import styles from "./top-banner.module.css";
+
+export const TopBanner = () => {
+  return (
+    <div className={styles.container}>
+      <p>
+        As a mark of respect for the State Funeral for Her Majesty Queen
+        Elizabeth II, we have taken the decision to close on Monday 19
+        September.
+      </p>
+    </div>
+  );
+};
