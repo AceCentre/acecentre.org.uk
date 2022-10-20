74dc67d2eca2b2847243c95a373f5ead5b416c51
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-10-20 11:15:42 +0100

    Logging

--- cypress/integration/smoke.spec.js
+++ cypress/integration/smoke.spec.js
@@ -1,10 +1,15 @@
 context("Smoke", () => {
   before(() => {
+    console.log("======== RECORDING ===========");
+    console.log(Cypress.env("HARS_FOLDERS"));
     cy.recordHar();
+    console.log("======== RECORDING STARTED ===========");
   });
 
   after(() => {
+    console.log("======== SAVING ===========");
     cy.saveHar();
+    console.log("======== SAVED ===========");
   });
 
   it(["pre-deploy"], "loads the webpage without any issues", () => {
