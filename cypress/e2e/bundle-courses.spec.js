d0c97616cd5b7bc1efd1c1b3a170364fcd19ebb4
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-11-07 09:38:38 +0000

    Fix tests

--- cypress/e2e/bundle-courses.spec.js
+++ cypress/e2e/bundle-courses.spec.js
@@ -131,7 +131,7 @@ context("Moodle", () => {
       );
 
       cy.findByRole("checkbox", {
-        name: "I have read and agree to the website terms and conditions",
+        name: "I have read and agree to the website terms and conditions and privacy policy",
       }).click({ force: true });
       cy.findByRole("button", { name: "Place order" }).click();
       cy.url({ timeout: 40000 }).should("include", "order");
