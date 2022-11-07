40508cef3a1a756d2bbfc4cd02d67f47239271d2
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-11-07 16:53:12 +0000

    Add redirect

--- pages/page/[slug].js
+++ pages/page/[slug].js
@@ -30,38 +30,41 @@ export default function GenericPage({ page }) {
 const ALL_PAGES = [
   {
     slug: "cookies",
+    altSlugs: [],
     description:
       "Ace Centre is a registered national charity (no. 1089313) dedicated to the support of individuals with complex communication and physical difficulties, and we have always been committed to protecting the privacy and security of the personal data of our clients.",
   },
-  {
-    slug: "privacy",
-    description:
-      "Ace Centre is a registered national charity (no. 1089313) dedicated to the support of individuals with complex communication and physical difficulties, and we have always been committed to protecting the privacy and security of the personal data of our clients.",
-  },
+
   {
     slug: "ace-centre-privacy-policy-3",
+    altSlugs: ["privacy"],
     description:
       "Ace Centre is a registered national charity (no. 1089313) dedicated to the support of individuals with complex communication and physical difficulties, and we have always been committed to protecting the privacy and security of the personal data of our service users.",
   },
   {
     slug: "safeguarding-policies",
+    altSlugs: [],
     description:
       "At Ace Centre, we regard safeguarding of our clients a top priority.",
   },
   {
     slug: "purchase-terms-and-conditions",
+    altSlugs: [],
     description: "Terms and Conditions for all purchases made",
   },
   {
     slug: "copyright-and-licence-terms-for-non-profit-items",
+    altSlugs: ["copyright"],
     description: "Copyright and Licence terms for Non-Profit items",
   },
   {
     slug: "meet-paul-transcript",
+    altSlugs: [],
     description: "Transcript of Paul's case study",
   },
   {
     slug: "pasco-support",
+    altSlugs: [],
     description: "Support for the Pasco app",
   },
 ];
@@ -69,8 +72,12 @@ const ALL_PAGES = [
 export async function getStaticPaths() {
   const paths = ALL_PAGES.map((page) => ({ params: { slug: page.slug } }));
 
+  const altPaths = ALL_PAGES.flatMap((page) => page.altSlugs).map((slug) => ({
+    params: { slug },
+  }));
+
   return {
-    paths,
+    paths: [...paths, ...altPaths],
     // Currently this is ignored by Netlify so we have to use `notFound`
     // Ref: https://github.com/netlify/netlify-plugin-nextjs/issues/1179
     fallback: false,
@@ -78,6 +85,17 @@ export async function getStaticPaths() {
 }
 
 export const getStaticProps = withGlobalProps(async ({ params: { slug } }) => {
+  for (const page of ALL_PAGES) {
+    if (page.altSlugs.includes(slug.toLowerCase())) {
+      return {
+        redirect: {
+          destination: `/page/${page.slug}`,
+          permanent: true,
+        },
+      };
+    }
+  }
+
   const page = await getPage(slug);
 
   if (!page) {
