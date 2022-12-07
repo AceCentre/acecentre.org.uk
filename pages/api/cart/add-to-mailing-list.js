568d1d629c36ffab96b8e085777e0cfe551edcef
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-12-07 16:00:04 +0000

    Deprecate add-to-mailing-list-endpoint

--- pages/api/cart/add-to-mailing-list.js
+++ pages/api/cart/add-to-mailing-list.js
@@ -1,30 +1,31 @@
 import withSession from "../../../lib/auth/with-session";
-import { addToMailingList } from "../auth/register";
 
 async function handler(req, res) {
-  const body = JSON.parse(req.body);
+  res.send({ success: false });
 
-  let startTime = Date.now();
-  let endTime = Date.now();
-  console.log({ startTime, endTime });
-  try {
-    startTime = Date.now();
-    await addToMailingList(body.email);
-    endTime = Date.now();
-    console.log("Added to the mailing list", endTime - startTime);
-    res.send({ success: true });
-  } catch (error) {
-    const errors = error?.response?.errors || [];
-    const mainError = errors[0] || null;
-    const errorMessage = mainError?.message || "An error has occurred";
+  // const body = JSON.parse(req.body);
 
-    console.log({ error, mainError, errorMessage });
+  // let startTime = Date.now();
+  // let endTime = Date.now();
+  // console.log({ startTime, endTime });
+  // try {
+  //   startTime = Date.now();
+  //   await addToMailingList(body.email, { location: "checkout" });
+  //   endTime = Date.now();
+  //   console.log("Added to the mailing list", endTime - startTime);
+  //   res.send({ success: true });
+  // } catch (error) {
+  //   const errors = error?.response?.errors || [];
+  //   const mainError = errors[0] || null;
+  //   const errorMessage = mainError?.message || "An error has occurred";
 
-    // If the cart is already empty then lets just continue
-    console.log("-------");
-    res.send({ success: false, error: errorMessage });
-    return;
-  }
+  //   console.log({ error, mainError, errorMessage });
+
+  //   // If the cart is already empty then lets just continue
+  //   console.log("-------");
+  //   res.send({ success: false, error: errorMessage });
+  //   return;
+  // }
 }
 
 export default withSession(handler);
