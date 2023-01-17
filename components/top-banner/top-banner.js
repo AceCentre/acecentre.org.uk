4b78db35e78f1faecda12e5f8e2b54068c314524
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2023-01-17 14:10:58 +0000

    Update comm works banner

--- components/top-banner/top-banner.js
+++ components/top-banner/top-banner.js
@@ -1,56 +1,14 @@
 import styles from "./top-banner.module.css";
-import { Modal, ModalBody, ModalContent, ModalOverlay } from "@chakra-ui/modal";
-import { useState } from "react";
+
+import Link from "next/link";
 
 export const TopBanner = () => {
-  const [modelOpen, setModelOpen] = useState(false);
-
-  const onClose = () => setModelOpen(false);
-
   return (
-    <>
-      <Modal
-        scrollBehavior="inside"
-        size="3xl"
-        isCentered
-        isOpen={modelOpen}
-        onClose={onClose}
-      >
-        <ModalOverlay />
-        <ModalContent>
-          <ModalBody style={{ padding: "2rem" }}>
-            <div className={styles.topSection}>
-              <h2>Christmas Hours</h2>
-              <div className={styles.listSpacing}>
-                <p>26th Dec: closed</p>
-                <p>27th Dec: closed</p>
-                <p>28th Dec: 9:00 - 17:00</p>
-                <p>29th Dec: 9:00 - 17:00</p>
-                <p>30th Dec: 9:00 - 17:00</p>
-                <p>2nd Jan: closed</p>
-              </div>
-            </div>
-
-            <div className={styles.bottomContainer}>
-              <button className={styles.closeButton} onClick={onClose}>
-                Close window
-              </button>
-            </div>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
-      <div className={styles.container}>
-        <p>
-          🎅 Our opening hours will change over the Christmas period.{" "}
-          <a
-            href="#"
-            onClick={() => setModelOpen(true)}
-            className={styles.modalLink}
-          >
-            Click here to see more.
-          </a>
-        </p>
-      </div>
-    </>
+    <div className={styles.container}>
+      <p>
+        🎉 Communication Works: live AT events coming in May 2023.{" "}
+        <Link href="/communication-works">Click here to find out more!</Link>
+      </p>
+    </div>
   );
 };
