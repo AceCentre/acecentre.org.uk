580f659e3357f9687ee61e2ae887139ad6c9c356
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2023-01-26 14:23:14 +0000

    Fix redirect error

--- pages/resources/[slug].js
+++ pages/resources/[slug].js
@@ -87,7 +87,7 @@ const ResourceListSwitch = ({
 };
 
 export async function getStaticPaths() {
-  const allProducts = await getAllProducts(true);
+  const allProducts = await getAllProducts(true, false);
 
   if (!allProducts) throw new Error("Could not get all the products");
 
@@ -99,7 +99,7 @@ export async function getStaticPaths() {
     })),
     // Currently this is ignored by Netlify so we have to use `notFound`
     // Ref: https://github.com/netlify/netlify-plugin-nextjs/issues/1179
-    fallback: false,
+    fallback: true,
   };
 }
 
