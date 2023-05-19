cd4ffa2cb08d73beb6aa1ed7dde527dd8c2fe77d
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2023-05-19 11:17:36 +0100

    Fix tests, delete cohorts

--- cypress/support/delete-moodle-user.js
+++ cypress/support/delete-moodle-user.js
@@ -39,3 +39,58 @@ export const deleteMoodleUser = async (email) => {
 
   console.log(deletedUserResult);
 };
+
+export const deleteCohorts = async () => {
+  const client = await moodleClient.init({
+    wwwroot: "https://learning.acecentre.org.uk",
+    token: "b0694f465f97c730298884eb715d2f7e",
+    service: "automated",
+    strictSSL: false,
+  });
+
+  const cohortResults = await client.call({
+    wsfunction: "core_cohort_get_cohorts",
+    method: "POST",
+  });
+
+  const isTestName = (name) => {
+    return RegExp("test-.*@acecentre.org.uk", "g").test(name);
+  };
+
+  const testingCohorts = cohortResults.filter((x) => isTestName(x.name));
+
+  const cohortids = testingCohorts.map((x) => x.id);
+
+  const fullCohorts = await client.call({
+    wsfunction: "core_cohort_get_cohort_members",
+    method: "POST",
+    args: {
+      cohortids,
+    },
+  });
+
+  const emptyCohorts = fullCohorts.filter((x) => x.userids.length === 0);
+
+  console.log("Number of cohorts to delete: ", emptyCohorts.length);
+
+  if (emptyCohorts.length > 0) {
+    const cohortsToDelete = emptyCohorts.map((x) => x.cohortid);
+
+    cohortsToDelete.forEach((x) => {
+      const temp = testingCohorts.find((z) => z.id == x);
+
+      console.log("Deleting", temp.name);
+    });
+
+    // console.log(cohortsToDelete);
+    const deletionResult = await client.call({
+      wsfunction: "core_cohort_delete_cohorts",
+      method: "POST",
+      args: {
+        cohortids: cohortsToDelete,
+      },
+    });
+
+    console.log(deletionResult);
+  }
+};
