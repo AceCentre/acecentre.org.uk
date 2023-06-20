2d6a865bacdbd81c660c3e662368afc6d89e4e96
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2023-06-20 12:26:47 +0100

    Fix map box

--- components/service-finder-map/service-finder-map.js
+++ components/service-finder-map/service-finder-map.js
@@ -25,7 +25,7 @@ export const ServiceFinderMap = ({ services }) => {
       const map = L.map("map").setView([54.2511, -4.4631], 6);
 
       L.tileLayer(
-        "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
+        "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ2hlbmRlcnNvbjUiLCJhIjoiY2xqNDc3b3hpMWw4YjNucDk0enJtZjlyciJ9.vy3y2eap6esYOMmsoQXTQw",
         {
           maxZoom: 18,
           attribution:
