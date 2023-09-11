806f8a5b8cc1edf730643baabfde87e908382a0b
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2023-09-11 15:24:56 +0100

    Implement spinner

--- pages/language-library/all.js
+++ pages/language-library/all.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { CombinedNav } from "../../components/combined-nav/combined-nav";
 import { Footer } from "../../components/footer/footer";
 import { LanguageLibrarySearchForm } from "../../components/language-library-search-form/language-library-search-form";
@@ -5,9 +6,25 @@ import { PageTitle } from "../../components/page-title/page-title";
 import { defaultNavItems } from "../../components/sub-nav/sub-nav";
 import { useGlobalProps } from "../../lib/global-props/hook";
 import { withGlobalPropsNoRevalidate } from "../../lib/global-props/inject";
+import { getLandingPageSearchResultsProps } from "../../lib/language-library";
+import { useRouter } from "next/router";
+import { FullPageSpinner } from "../../components/full-page-spinner/full-page-spinner";
 
-export default function LanguageLibrary() {
+const useRouterReady = () => {
+  const [isReady, setIsReady] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    setIsReady(router.isReady);
+  }, [router.isReady]);
+
+  return isReady;
+};
+
+export default function LanguageLibrary({ resources }) {
   const { currentYear } = useGlobalProps();
+
+  const isReady = useRouterReady();
 
   return (
     <>
@@ -16,7 +33,11 @@ export default function LanguageLibrary() {
       </header>
       <main id="mainContent">
         <PageTitle heading="Explore" description="Language Library" />
-        <LanguageLibrarySearchForm />
+        {isReady ? (
+          <LanguageLibrarySearchForm resources={resources} />
+        ) : (
+          <FullPageSpinner />
+        )}
       </main>
       <Footer currentYear={currentYear} />
     </>
@@ -24,10 +45,11 @@ export default function LanguageLibrary() {
 }
 
 export const getServerSideProps = withGlobalPropsNoRevalidate(async () => {
-  // const props = await getLanguageLibraryLandingPageData();
+  const props = await getLandingPageSearchResultsProps();
 
   return {
     props: {
+      ...props,
       seo: { dontIndex: true, title: "Language Library" },
     },
   };
