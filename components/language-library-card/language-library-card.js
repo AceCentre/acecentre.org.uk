ba8991ed304d130816ad89c6c2c36a96b8b0c16a
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2023-09-11 15:25:13 +0100

    Init card

---
+++ components/language-library-card/language-library-card.js
@@ -0,0 +1,33 @@
+import styles from "./language-library-card.module.css";
+import { Image } from "../image";
+
+const listOfBackgrounds = [
+  styles.blueGradient,
+  styles.yellowGradient,
+  styles.redGradient,
+  styles.orangeGradient,
+  styles.redGradient,
+  styles.orangeGradient,
+  styles.blueGradient,
+  styles.yellowGradient,
+];
+
+export const LanguageLibraryCard = ({ resource, index = 0 }) => {
+  const backgroundClass =
+    listOfBackgrounds[index == 0 ? 0 : index % listOfBackgrounds.length];
+
+  return (
+    <div className={styles.container}>
+      <div className={`${styles.imageContainer} ${backgroundClass}`}>
+        <Image
+          src={resource.featuredImage.node.mediaItemUrl}
+          width={200}
+          height={150}
+          className={`${styles.image}`}
+        />
+      </div>
+
+      {/* <h3>{resource.title}</h3> */}
+    </div>
+  );
+};
