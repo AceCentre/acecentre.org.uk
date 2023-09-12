d77ddbe8d660c3fce9a9207abe4eaa8ff65333dd
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2023-09-12 16:34:09 +0100

    Start resource page

--- pages/language-library/[slug].js
+++ pages/language-library/[slug].js
@@ -1,5 +1,6 @@
 import { CombinedNav } from "../../components/combined-nav/combined-nav";
 import { Footer } from "../../components/footer/footer";
+import { LanguageLibraryResourcePage } from "../../components/language-library-resource-page/language-library-resource-page";
 import { defaultNavItems } from "../../components/sub-nav/sub-nav";
 import { useGlobalProps } from "../../lib/global-props/hook";
 import { withGlobalPropsNoRevalidate } from "../../lib/global-props/inject";
@@ -8,15 +9,13 @@ import { getLanguageLibraryResource } from "../../lib/language-library";
 export default function LanguageLibrary({ resource }) {
   const { currentYear } = useGlobalProps();
 
-  console.log(resource);
-
   return (
     <>
       <header>
         <CombinedNav defaultNavItems={defaultNavItems} />
       </header>
       <main id="mainContent">
-        <pre>{JSON.stringify(resource, null, 2)}</pre>
+        <LanguageLibraryResourcePage resource={resource} />
       </main>
       <Footer currentYear={currentYear} />
     </>
