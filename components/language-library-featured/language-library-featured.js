a1b983117cebddb443ca0b647efc7c841bcb68a7
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2023-09-12 09:57:56 +0100

    Add view all resources

--- components/language-library-featured/language-library-featured.js
+++ components/language-library-featured/language-library-featured.js
@@ -1,19 +1,21 @@
+import Link from "next/link";
 import { LanguageLibraryCard } from "../language-library-card/language-library-card";
 import styles from "./language-library-featured.module.css";
 
 export const LanguageLibraryFeatured = ({ resources }) => {
   return (
     <div className={styles.container}>
-      <h2>Featured Resources</h2>
+      <div className={styles.header}>
+        <h2>Featured Resources</h2>
+        <Link href="/language-library/all">View All Resources &gt;</Link>
+      </div>
       <div className={styles.list}>
         {resources.map((current, index) => (
-          <>
-            <LanguageLibraryCard
-              index={index}
-              key={current.slug}
-              resource={current}
-            />
-          </>
+          <LanguageLibraryCard
+            index={index}
+            key={current.slug}
+            resource={current}
+          />
         ))}
       </div>
     </div>
