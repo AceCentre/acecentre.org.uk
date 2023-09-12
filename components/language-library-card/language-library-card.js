935ee7414cef77ae2df267e8fbae59b458a05fde
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2023-09-12 11:10:49 +0100

    Snazz up language lirary

--- components/language-library-card/language-library-card.js
+++ components/language-library-card/language-library-card.js
@@ -1,5 +1,6 @@
 import styles from "./language-library-card.module.css";
 import { Image } from "../image";
+import Link from "next/link";
 
 const listOfBackgrounds = [
   styles.blueGradient,
@@ -17,18 +18,25 @@ export const LanguageLibraryCard = ({ resource, index = 0 }) => {
     listOfBackgrounds[index == 0 ? 0 : index % listOfBackgrounds.length];
 
   return (
-    <div className={styles.container}>
+    <Link
+      href={`/language-library/${resource.slug}`}
+      className={styles.container}
+      title={`Link to full resource: ${resource.title}`}
+    >
       <div className={`${styles.imageContainer} ${backgroundClass}`}>
         <Image
           src={resource.featuredImage.node.mediaItemUrl}
           width={200}
           height={150}
           className={`${styles.image}`}
+          alt={`Screenshot of resource: ${resource.title}`}
         />
       </div>
       <h3>{resource.title}</h3>
-      <p>Added by: {resource.authorUsername}</p>
-      <p>{resource.languages.nodes.map((x) => x.name).join(", ")}</p>
-    </div>
+      <p className={styles.addedBy}>Added by: {resource.authorUsername}</p>
+      {/* <p className={styles.languages}>
+        {resource.languages.nodes.map((x) => x.name).join(", ")}
+      </p> */}
+    </Link>
   );
 };
