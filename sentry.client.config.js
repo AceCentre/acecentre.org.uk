af9c282cd9424b8ce48f54ecdc81345e2e9281e2
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2023-11-15 14:20:52 +0000

    Update sentry

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -16,7 +16,9 @@ Sentry.init({
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps,
-  beforeSendTransaction: (event) => {
-    event;
-  },
+
+  ignoreErrors: [
+    // This error comes from Microsofts outlook crawler
+    "Non-Error promise rejection captured with value: Object Not Found Matching Id",
+  ],
 });
--- sentry.server.config.js
+++ sentry.server.config.js
@@ -16,4 +16,9 @@ Sentry.init({
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
+
+  ignoreErrors: [
+    // This error comes from Microsofts outlook crawler
+    "Non-Error promise rejection captured with value: Object Not Found Matching Id",
+  ],
 });
