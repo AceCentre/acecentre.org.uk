5f47997aeca6ac8779d3bdd98f9eadd8c3b0ec77
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2023-11-30 19:50:35 +0000

    Go back to old way

--- pages/resources/[slug].js
+++ pages/resources/[slug].js
@@ -17,8 +17,6 @@ import { getLaunchpadTemplate } from "../../lib/launchpad";
 import { LaunchpadPage } from "../../components/launchpad-generate/launchpad-generate";
 import { useRouter } from "next/router";
 import { ProductFaqs } from "../../components/product-faqs/product-faqs";
-import createApolloClient from "../../lib-v2/apollo-client";
-import { gql } from "@apollo/client";
 
 export default function ResourceDetail({
   resource,
@@ -111,24 +109,14 @@ const ResourceListSwitch = ({
 };
 
 export async function getStaticPaths() {
-  const client = createApolloClient();
-  const { data } = await client.query({
-    query: gql`
-      query getResourcePaths($tag: String) {
-        products(first: 999, where: { tagIn: [$tag] }) {
-          nodes {
-            slug
-          }
-        }
-      }
-    `,
-    variables: {
-      tag: "resource",
-    },
-  });
+  let allProducts = await getAllProducts(true);
+
+  allProducts = allProducts.filter((x) => x.slug !== "look2talk");
+
+  if (!allProducts) throw new Error("Could not get all the products");
 
   return {
-    paths: data.products.nodes.map((product) => ({
+    paths: allProducts.map((product) => ({
       params: {
         slug: product.slug,
       },
