e31de513d5a90958c5db80523649de4b8921dd6d
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2024-01-23 09:24:24 +0000

    Add banner to top of the page

--- components/combined-nav/combined-nav.js
+++ components/combined-nav/combined-nav.js
@@ -18,6 +18,7 @@ import { Button } from "../button/button";
 import { Input } from "../input/input";
 import { OldBrowserBanner } from "../old-browser-banner/old-browser-banner";
 import { useAuth } from "../../lib/auth-hook";
+import { TopBanner } from "../top-banner/top-banner";
 
 const useMobileNav = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -81,6 +82,7 @@ export const CombinedNav = ({
 
   return (
     <>
+      <TopBanner />
       <OldBrowserBanner />
 
       <div
--- components/top-banner/top-banner.js
+++ components/top-banner/top-banner.js
@@ -4,26 +4,14 @@ import styles from "./top-banner.module.css";
 import Link from "next/link";
 
 export const TopBanner = () => {
-  const currentDate = new Date();
-
-  const startDate = new Date("2023-05-15T13:00:00.000+01:00");
-  const endDate = new Date("2023-05-17T13:00:00.000+01:00");
-
   return (
     <div className={styles.container}>
-      {currentDate.getTime() > startDate.getTime() &&
-      currentDate.getTime() < endDate.getTime() ? (
-        <p>
-          Due to a staff training event our office will be{" "}
-          <span className={styles.closed}>CLOSED</span> from midday 15.5.23 to
-          17.5.23.
-        </p>
-      ) : (
-        <p>
-          🎉 Communication Works: live AT events coming in May 2023.{" "}
-          <Link href="/communication-works">Click here to find out more!</Link>
-        </p>
-      )}
+      <p>
+        🎉 Communication Works: live AT events coming in May 2024.{" "}
+        <Link href="/communication-works-2024">
+          Click here to find out more!
+        </Link>
+      </p>
     </div>
   );
 };
