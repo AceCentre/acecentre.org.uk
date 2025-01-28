16db4a774becf48a310b74e0fb011d892bd9f49a
SteveMuriithi1 <stevemuriithi10@gmail.com>
2025-01-28 11:19:09 +0000

    banner update

--- components/top-banner/top-banner.js
+++ components/top-banner/top-banner.js
@@ -7,7 +7,7 @@ export const TopBanner = () => {
   return (
     <div className={styles.container}>
       <p>
-      🎉 Join us for Communication Works North oon Friday 16th May!{" "}
+        🎉 Join us for Communication Works North on Friday 16th May!{" "}
         <Link href="/communication-works-2025">
         Find out more and register for your FREE ticket here
         </Link>
