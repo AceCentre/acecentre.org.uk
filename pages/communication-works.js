1a91659c597597bb04574a3271859e1f2418c2a1
SteveMuriithi1 <stevemuriithi10@gmail.com>
2025-03-03 10:34:28 +0000

    comm-works-google link update

--- pages/communication-works.js
+++ pages/communication-works.js
@@ -2,32 +2,36 @@ import { CombinedNav } from "../components/combined-nav/combined-nav";
 import { Footer } from "../components/footer/footer";
 import { defaultNavItems } from "../components/sub-nav/sub-nav";
 
-// import Avatar from "@mui/material/Avatar";
+import Avatar from "@mui/material/Avatar";
 
 import styles from "../styles/communication-works.module.css";
 
-// import { ImageWithLoader } from "../components/image";
+import { ImageWithLoader } from "../components/image";
 import { MailingList } from "../components/service-finder-mailing-list/service-finder-mailing-list";
-// import Link from "next/link";
-// import AssignmentIcon from "@mui/icons-material/Assignment";
-// import EventIcon from "@mui/icons-material/Event";
+import Link from "next/link";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import EventIcon from "@mui/icons-material/Event";
 import { getPage } from "../lib/generic-pages/get-page";
+import Head from "next/head";
 
 export default function CommunicationWorks({ page }) {
   return (
     <>
+      <Head>
+        <script defer data-domain="scecentre.org.uk/communication-works-2025" src="https://stats.scecentre.org.uk/js/script.js"></script>
+      </Head>
       <header>
         <CombinedNav defaultNavItems={defaultNavItems} />
       </header>
       <main id="mainContent">
-        {/* <div className={styles.imageContainer}>
+        <div className={styles.imageContainer}>
           <ImageWithLoader
-            // src="/comm-works-2024.png"
+            src="/comm-works-2025.png"
             alt="Communication works banner"
             width={6912}
             height={3456}
           />
-        </div> */}
+        </div>
         <div className={styles.bottomContainer}>
           <div className={styles.leftContent}>
             <div
@@ -39,14 +43,14 @@ export default function CommunicationWorks({ page }) {
               description="Sign up to our free newsletter to get emails about other Ace Centre events and news."
             />
           </div>
-          {/* <div>
+          <div>
             <div className={styles.quote}>
               <Avatar className={styles.avatar}>
                 <AssignmentIcon className={styles.icon} />
               </Avatar>
               <div>
                 <h3>Communication Works NORTH</h3>
-                <p>Thursday, 23 May 2024</p>
+                <p>Friday, 16 May 2025</p>
                 <p>New Bridge Academy, Oldham, OL8 3PH</p>
 
                 <p>
@@ -92,7 +96,7 @@ export default function CommunicationWorks({ page }) {
               </Avatar>
               <div>
                 <h3>Communication Works SOUTH</h3>
-                <p>Thursday, 6 June 2024</p>
+                <p>Thursday, 22 May 2025</p>
                 <p>Charlton Athletic Football Club, London SE7 8BL</p>
                 <p>
                   <Link href="https://www.eventbrite.co.uk/e/communication-works-2025-south-tickets-1026780567317">
@@ -101,8 +105,7 @@ export default function CommunicationWorks({ page }) {
                 </p>
               </div>
             </div>
-          </div> */}
-
+          </div>
         </div>
       </main>
       <Footer />
@@ -111,7 +114,7 @@ export default function CommunicationWorks({ page }) {
 }
 
 export const getStaticProps = async () => {
-  const page = await getPage("communication-works");
+  const page = await getPage("communication-works-2025");
 
   return {
     revalidate: 60,
@@ -121,7 +124,7 @@ export const getStaticProps = async () => {
       seo: {
         title: "Communication Works 2025",
         description:
-          "Communication Works 2024 is a free live event allowing attendees the opportunity to see first-hand a range of assistive and accessible technology, communication tools, digital strategies, and person-centred approaches.",
+          "Communication Works 2025 is a free live event allowing attendees the opportunity to see first-hand a range of assistive and accessible technology, communication tools, digital strategies, and person-centred approaches.",
       },
     },
   };
