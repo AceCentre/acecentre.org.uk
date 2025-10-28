ce9755a653939386423da1c5517b4fb784048c32
SteveMuriithi1 <stevemuriithi10@gmail.com>
2025-10-28 14:53:44 +0000

    more images

--- pages/services/nhs/I-have-a-device.js
+++ pages/services/nhs/I-have-a-device.js
@@ -221,7 +221,7 @@ export default function NHSLanding() {
       </header>
       <main id="mainContent">
         <VideoWithCardCover
-          src="/services/I-have-a-device-button.jpg"
+          src="/services/I-have-a-device1.jpg"
           alt="AAC devices on a shelf"
           nhs
           heightClass={styles.coverHeight}
