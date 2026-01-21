02f7af2eb4b867d604fdf8a26d92f6c172e6bd18
SteveMuriithi1 <stevemuriithi10@gmail.com>
2026-01-21 15:22:46 +0000

    CM banner date change

--- components/top-banner/top-banner.js
+++ components/top-banner/top-banner.js
@@ -7,9 +7,9 @@ export const TopBanner = () => {
   return (
     <div className={styles.container}>
       <p>
-        🎉 Join us for Communication Works North on Friday 16th May!{" "}
-        <Link href="/communication-works-2025">
-        Click here to find out more and see the seminar programme
+        🎉 Join us for Communication Works North on Friday 22nd May!{" "}
+        <Link href="/communication-works-2026">
+          Click here to find out more and see the seminar programme
         </Link>
       </p>
     </div>
