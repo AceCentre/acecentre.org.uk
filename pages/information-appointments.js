4490b2d00519e691864c95905d3160749c1443b2
SteveMuriithi1 <stevemuriithi10@gmail.com>
2026-06-19 13:04:29 +0100

    placing the booking in a button

--- pages/information-appointments.js
+++ pages/information-appointments.js
@@ -1,12 +1,18 @@
 import { CombinedNav } from "../components/combined-nav/combined-nav";
 import { Footer } from "../components/footer/footer";
-import { FormModal, INFORMATION_RESERVE } from "../components/ms-form";
 import { PageTitle } from "../components/page-title/page-title";
 import { defaultNavItems } from "../components/sub-nav/sub-nav";
+import { Button } from "../components/button/button";
 import Link from "next/link";
+import { useState } from "react";
 import styles from "../styles/information-appointments.module.css";
 
+const BOOKING_URL =
+  "https://outlook.office365.com/owa/calendar/AceCentre@acecentre.org.uk/bookings/s/Bki6azf24k--6merN0obQA2";
+
 export default function Contact() {
+  const [bookingOpen, setBookingOpen] = useState(false);
+
   return (
     <>
       <header>
@@ -19,48 +25,57 @@ export default function Contact() {
         />
         <div className={styles.container}>
           <p>
-            Our Information Appointments were developed on the belief that
-            information should be accessible to all, up-to-date, unbiased and,
-            ideally, free at source to people in need.
+            Our Information Appointments are built on a simple belief: everyone
+            should have access to information that is up-to-date, unbiased and,
+            wherever possible, free at the point of use.
           </p>
 
           <p>
-            This is why we offer online meetings with Ace Centre clinical staff
-            twice a month and free of charge. They are an opportunity to meet
-            informally with our team to discuss your needs.
+            That’s why we offer free online meetings with Ace Centre clinical
+            staff. These informal sessions give you the opportunity to talk
+            through your needs with members of our team in a supportive,
+            no-pressure environment.
           </p>
 
           <p>
-            Whether you are new to AAC/AT and trying to find a starting point,
-            or you are already using AAC/AT and want to know how to move on,
-            book in for a video-call session with our staff. We’re here to help!
+            Whether you’re new to AAC/AT and unsure where to start, or you’re
+            already using it and looking for next steps, you’re welcome to book
+            a video call with us—we’re here to help.
           </p>
-
+          <br />
+          <h2>Please note before booking: </h2>
           <p>
-            Please note, these sessions do not provide a full AT/AAC assessment.{" "}
-            <Link href="/services/assessments" legacyBehavior>
-              Click here for more information about assessments.
+            <b>
+              *For quicker support, call our Advice Helpline on 0800 080 3115
+              (option 2), available Monday, Wednesday and Friday, 1–5pm, to
+              speak directly with a team member.
+            </b>
+            <br />
+            As you may need to wait a couple of months for an Information
+            Appointment, take that time to invite the wider team around the
+            AAC/AT user to the appointment as this can be particularly valuable.
+          </p>
+          <p>
+            <b>
+              *You do not need to book an Information Appointment to decide
+              whether to refer for an NHS England assessment.{" "}
+            </b>
+            <br />
+            <Link href="/services/nhs/referral-process" legacyBehavior>
+              Our website has helpful guidance to support your decision-making
+              around referrals, available to access at any time.
             </Link>
           </p>
-
-          <h2>Booking an appointment</h2>
           <p>
-            To book your appointment complete the form below. Follow these
-            steps:
+            <b>*These sessions are not a full AAC/AT assessment.</b> We can
+            discuss your needs in general and signpost you to information and
+            options to explore independently.
+            <br />
+            <Link href="/services/assessments" legacyBehavior>
+              Ace Centre offers both NHS England and self-funded assessments.
+            </Link>
           </p>
-          <ol>
-            <li>
-              Select the month you want to have the appointment. Dates with
-              available appointments will be shown. Select the time that works
-              for you.
-            </li>
-            <li>Fill in your details so we can contact you.</li>
-            <li>
-              Answer all the questions about the appointment so we can come
-              prepared with all the information you might need
-            </li>
-          </ol>
-          <p>
+          {/* <p>
             If you have successfully booked an appointment but would prefer an
             earlier one,{" "}
             <FormModal form={INFORMATION_RESERVE}>
@@ -71,37 +86,34 @@ export default function Contact() {
               )}
             </FormModal>{" "}
             and we will add you to our cancellations waiting list.
-          </p>
-          <p>
-            If you prefer you can{" "}
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="https://outlook.office365.com/owa/calendar/AceCentre@acecentre.org.uk/bookings/s/Bki6azf24k--6merN0obQA2"
-            >
-              click this link to open the booking form in a new page
-            </a>
-          </p>
-          <>
-            <style jsx>{`
-              .info-frame {
-                height: 3100px;
-              }
+          </p> */}
 
-              @media (max-width: 1132px) {
-                .info-frame {
-                  height: 3700px;
-                }
-              }
-            `}</style>
-            <div
-              className={"info-frame"}
-              dangerouslySetInnerHTML={{
-                __html:
-                  "<iframe title='Book an information appointment' src='https://outlook.office365.com/owa/calendar/AceCentre@acecentre.org.uk/bookings/s/Bki6azf24k--6merN0obQA2' width='100%' height='100%' scrolling='yes' style='border:0'></iframe>",
-              }}
-            ></div>
-          </>
+          <div className={styles.bookingSection}>
+            {!bookingOpen ? (
+              <Button
+                className={styles.bookingButton}
+                onClick={() => setBookingOpen(true)}
+              >
+                I&apos;m ready to book
+              </Button>
+            ) : (
+              <div className={styles.bookingPanel}>
+                <p>
+                  If you prefer, you can{" "}
+                  <a target="_blank" rel="noreferrer" href={BOOKING_URL}>
+                    open the booking form in a new page
+                  </a>
+                  .
+                </p>
+                <div
+                  className={styles.infoFrame}
+                  dangerouslySetInnerHTML={{
+                    __html: `<iframe title='Book an information appointment' src='${BOOKING_URL}' width='100%' height='100%' scrolling='yes' style='border:0'></iframe>`,
+                  }}
+                />
+              </div>
+            )}
+          </div>
         </div>
       </main>
       <Footer />
